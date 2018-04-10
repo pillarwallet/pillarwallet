5f990a796ec671456d2441903ec4b5f973adb3df
Lewis Flude <lewisflude@Lewiss-MacBook-Pro.local>
2018-04-10 10:09:04 +0100

    Move Pincode screen to correct folder

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StackNavigator } from 'react-navigation';
 import Intro from './screens/Intro';
 import NewWallet from './screens/NewWallet';
 import Login from './screens/Login';
+import Pincode from "./screens/Pincode";
 
 const RootStack = StackNavigator(
   {
