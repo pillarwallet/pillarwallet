e0ac51ebbcd37844289d863864f8485bbc94d253
Franky Aguilar <franky@99centbrains.com>
2018-04-11 12:58:23 +0100

    External Stylesheets for Screens

--- src/components/Modals/PopModal.js
+++ src/components/Modals/PopModal.js
@@ -2,75 +2,12 @@ import React, { Component } from 'react';
 import {
   Animated,
   Easing,
-  StyleSheet,
   View,
   Text,
   TouchableHighlight,
 } from 'react-native';
-
+import styles from './styles';
 import OutlineButton from '../Buttons/OutlineButton';
-
-const styles = StyleSheet.create({
-  popOverContainer: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  popOverContainerBG: {
-    backgroundColor: 'black',
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    opacity: 0.5,
-  },
-
-  popOverBackground: {
-    width: '85%',
-    margin: 'auto',
-    height: 300,
-    backgroundColor: '#01bbff',
-    borderRadius: 10,
-    padding: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-  },
-
-  popOverHeader: {
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-
-  popOverHeaderText: {
-    color: 'white',
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-
-  popOverContentText: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-
-  popOverActions: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingBottom: 20,
-  },
-
-  popOverActionsText: {
-    color: 'white',
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-
-});
 
 export default class PopModal extends Component {
   //
--- src/screens/BackupPhrase/BackupPhrase.js
+++ src/screens/BackupPhrase/BackupPhrase.js
@@ -2,12 +2,12 @@
 // backup phrase for the wallet.
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
   View,
 } from 'react-native';
 
 import ethers from 'ethers';
+import styles from './styles';
 
 import DefaultButton from '../../components/Buttons/DefaultButton';
 import PopModal from '../../components/Modals/PopModal';
@@ -15,66 +15,6 @@ import PopModal from '../../components/Modals/PopModal';
 const mneumonicPhrase = ethers.HDNode.entropyToMnemonic(ethers.utils.randomBytes(16));
 const mneumonicList = mneumonicPhrase.split(' ');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-
-  textContainer: {
-    justifyContent: 'flex-start',
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-
-  header: {
-    fontSize: 32,
-    marginTop: 10,
-    alignItems: 'center',
-  },
-
-  paragraph: {
-    paddingTop: 10,
-    fontSize: 16,
-    color: 'grey',
-  },
-
-  paragraphSmall: {
-    fontSize: 12,
-    padding: 10,
-    color: 'grey',
-  },
-
-  mneumonicContainer: {
-    marginLeft: -10,
-    marginRight: -10,
-    marginTop: 20,
-    marginBottom: 20,
-    padding: 20,
-    backgroundColor: '#f2f2f2',
-    flexWrap: 'wrap',
-    flexDirection: 'row',
-
-    borderStyle: 'dashed',
-    borderWidth: 1,
-    borderColor: '#d6d7da',
-  },
-
-  listItem: {
-    fontWeight: 'bold',
-    marginRight: 10,
-    marginBottom: 2,
-    fontSize: 12,
-  },
-
-  confirmContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingBottom: 20,
-  },
-
-});
 
 export default class BackupPhrase extends Component {
   state = {
