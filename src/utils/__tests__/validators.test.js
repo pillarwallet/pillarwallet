b873603def80cba4883f461f14dc1c6f8689da27
Rostislav Chuklin <rostislavcn@gmail.com>
2018-04-16 13:53:18 +0100

    misspeled test file fix

--- src/utils/__tests__/validators.test.js
+++ src/utils/__tests__/validators.test.js
@@ -6,13 +6,13 @@ describe('Validators', () => {
     it('should validate the length of provided pin code', () => {
       const pin = '123456';
       const expectedErrorMessage = 'Invalid pin\'s length (should be 6 numbers)';
-      expect(validatePin(pin)).toBeEmpty();
+      expect(validatePin(pin)).toHaveLength(0);
       expect(validatePin('1')).toBe(expectedErrorMessage);
     });
 
     it('should allow only digits', () => {
       const expectedErrorMessage = 'Pin could contain numbers only';
-      expect(validatePin('1asd')).toBe(expectedErrorMessage);
+      expect(validatePin('1asdsd')).toBe(expectedErrorMessage);
     });
   });
 });
