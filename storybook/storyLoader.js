845d3eed67319d5c5a15a05842e6cfacfa4e5b39
Lewis Flude <lewis.flude@gmail.com>
2018-04-16 09:57:35 +0100

    Re-add "Feature/set up storybook (#27)" due to human error
    * Add helper for padding in storybook
    
    * Remove unused stories, add more stories for AssetCard
    
    * Add stories for all existing components
    
    * Fix wording
    
    * Add action logger for PopModal
    
    * Set home screen back to default
    
    * Clean up App.js and reset things to be mergable
    
    * No need to look for modules in storybook folder
    
    * Fix errors with linting and Jest

---
+++ storybook/storyLoader.js
@@ -0,0 +1,28 @@
+
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/AssetCard/AssetCard.stories');
+  require('../src/components/AssetCardList/AssetCardList.stories');
+  require('../src/components/Buttons/DefaultButton/DefaultButton.stories');
+  require('../src/components/Buttons/OutlineButton/OutlineButton.stories');
+  require('../src/components/Modals/PopModal/PopModal.stories');
+  
+}
+
+const stories = [
+  '../src/components/AssetCard/AssetCard.stories',
+  '../src/components/AssetCardList/AssetCardList.stories',
+  '../src/components/Buttons/DefaultButton/DefaultButton.stories',
+  '../src/components/Buttons/OutlineButton/OutlineButton.stories',
+  '../src/components/Modals/PopModal/PopModal.stories',
+  
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
