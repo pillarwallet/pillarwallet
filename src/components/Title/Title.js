d1e83c1c433a6ba7f87291ce44cbb7b98f86aa6c
Lewis Flude <lewis.flude@gmail.com>
2018-04-17 18:00:49 +0100

    Improve wrapper and Title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 
 const Title = styled.Text`
   font-size: 32;
-  margin: 20px;
+  margin: 20px 0;
   text-align: ${props => (props.center ? 'center' : 'left')};
 `;
 
