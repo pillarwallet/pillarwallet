4f09cb575edc2ce4f89c38226004585886ba24aa
Jegor Sidorenko <jegor@nfm.lt>
2018-04-24 17:19:40 +0100

    Refactor Mnemonic phrase component

--- src/components/MneumonicPhrase/MneumonicPhrase.js
+++ src/components/MneumonicPhrase/MneumonicPhrase.js
@@ -1,17 +1,42 @@
 // @flow
+import * as React from 'react';
 import styled from 'styled-components/native';
 
-const MneumonicPhrase = styled.View`
-margin-top: 20;
-margin-bottom: 20;
-padding: 20px;
-background-color: #f2f2f2;
-flex-wrap: wrap;
-flex-direction: row;
-border-color: #d6d7da;
-border-style: dashed;
-border-width: 1;
-border-radius: 4;
+const MneumonicPhraseWrapper = styled.View`
+  margin-top: 20;
+  margin-bottom: 20;
+  padding: 20px;
+  background-color: #f2f2f2;
+  flex-wrap: wrap;
+  flex-direction: row;
+  border-color: #d6d7da;
+  border-style: dashed;
+  border-width: 1;
+  border-radius: 4;
 `;
+
+const MneumonicPhraseItem = styled.Text`
+  font-weight: bold;
+  margin-right: 10;
+  margin-bottom: 2;
+  font-size: 12px;
+`;
+
+type Props = {
+  phrase: string,
+};
+
+const MneumonicPhrase = (props: Props) => {
+  const { phrase } = props;
+  const mnemonicList = phrase.split(' ');
+
+  return (
+    <MneumonicPhraseWrapper>
+      {
+        mnemonicList.map(word => <MneumonicPhraseItem key={word}>{word}</MneumonicPhraseItem>)
+      }
+    </MneumonicPhraseWrapper>
+  );
+};
 
 export default MneumonicPhrase;
--- src/components/MneumonicPhrase/MneumonicPhrase.stories.js
+++ src/components/MneumonicPhrase/MneumonicPhrase.stories.js
@@ -2,24 +2,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import MneumonicPhrase from 'components/MneumonicPhrase';
-import MneumonicPhraseItem from 'components/MneumonicPhraseItem';
 
+const phrase = 'fox the ipsum brown green dog jumps the red quick over lorem lazy';
 
 storiesOf('MneumonicPhrase', module)
   .add('Default', () => (
-    <MneumonicPhrase>
-      <MneumonicPhraseItem>fox</MneumonicPhraseItem>
-      <MneumonicPhraseItem>the</MneumonicPhraseItem>
-      <MneumonicPhraseItem>ipsum</MneumonicPhraseItem>
-      <MneumonicPhraseItem>brown</MneumonicPhraseItem>
-      <MneumonicPhraseItem>green</MneumonicPhraseItem>
-      <MneumonicPhraseItem>dog</MneumonicPhraseItem>
-      <MneumonicPhraseItem>jumps</MneumonicPhraseItem>
-      <MneumonicPhraseItem>the</MneumonicPhraseItem>
-      <MneumonicPhraseItem>red</MneumonicPhraseItem>
-      <MneumonicPhraseItem>quick</MneumonicPhraseItem>
-      <MneumonicPhraseItem>over</MneumonicPhraseItem>
-      <MneumonicPhraseItem>lorem</MneumonicPhraseItem>
-      <MneumonicPhraseItem>lazy</MneumonicPhraseItem>
-    </MneumonicPhrase>
+    <MneumonicPhrase phrase={phrase} />
   ));
