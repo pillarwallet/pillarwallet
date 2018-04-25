fb7a7cc5de7fa509f1c9fc9eb1e9c90876cadf3b
Jegor Sidorenko <jegor@nfm.lt>
2018-04-25 18:17:47 +0100

    Move PinDots component back to PinCode folder

--- src/components/PinCode/PinCode.js
+++ src/components/PinCode/PinCode.js
@@ -3,8 +3,8 @@ import * as React from 'react';
 import styled from 'styled-components/native/index';
 
 import KeyPad from 'components/KeyPad';
-import PinDots from 'components/PinDots';
 import { KEYPAD_BUTTON_DELETE, KEYPAD_BUTTON_FORGOT } from 'constants/keyPadButtonsConstants';
+import PinDots from './PinDots';
 
 const PASS_CODE_LENGTH = 6;
 
