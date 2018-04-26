3ec15000ee8f28552ddb60dc436e2d74ba1cd0c0
Rostislav Chuklin <rostislavcn@gmail.com>
2018-04-26 12:04:49 +0100

    SendModal component Implemented

---
+++ src/screens/Assets/SendModal.js
@@ -0,0 +1,156 @@
+// @flow
+import * as React from 'react';
+import { Share, Clipboard, View, Text } from 'react-native';
+import t from 'tcomb-form-native';
+import styled from 'styled-components/native';
+import SlideModal from 'components/Modals/SlideModal';
+import Button from 'components/Button';
+import TextInput from 'components/TextInput';
+import QRCodeScanner from 'components/QRCodeScanner';
+
+import { isValidETHAddress } from 'utils/validators';
+
+const { Form } = t.form;
+
+type Props = {
+  address: string,
+  isVisible: boolean,
+  onDismiss: Function,
+  formValues?: Object
+}
+
+type State = {
+  isScanning: boolean,
+  value: {
+    address: string | null,
+    amount: number | null
+  }
+}
+
+const Amount = t.refinement(t.Number, (amount) => {
+  return amount > 0;
+});
+
+Amount.getValidationErrorMessage = (amount) => {
+  return 'Amount should be specified.'
+};
+
+const Address = t.refinement(t.String, (address) => {
+  return address.length && isValidETHAddress(address);
+});
+
+Address.getValidationErrorMessage = (address) => {
+  if (!address) {
+    return 'Address must be provided.';
+  }
+  if (!isValidETHAddress(address)) {
+    return 'Invalid Ethereum Address.';
+  }
+};
+
+const TRANSACTION_TYPE = t.struct({
+  address: Address,
+  amount: Amount,
+});
+
+
+// EXTRACT TO FACTORY
+function AddressInputTemplate(locals) {
+  const { config: { onIconPress } } = locals;
+  const errorMessage = locals.error;
+  const inputProps = {
+    onChange: locals.onChange,
+    onBlur: locals.onBlur,
+    placeholder: 'Ethereum Address',
+    value: locals.value,
+    keyboardType: locals.keyboardType,
+    textAlign: 'right',
+  }
+  return <TextInput errorMessage={errorMessage} id="address" label={locals.label} icon="barcode" onIconPress={onIconPress} inputProps={inputProps} />
+}
+
+function AmountInputTemplate(locals) {
+  const errorMessage = locals.error;
+  const inputProps = {
+    onChange: locals.onChange,
+    onBlur: locals.onBlur,
+    placeholder: 'Specify the amount',
+    value: locals.value,
+    keyboardType: locals.keyboardType,
+    textAlign: 'right',
+    style: { paddingRight: 15 }
+  }
+  return <TextInput errorMessage={errorMessage} id="amount" label={locals.label} inputProps={inputProps} />
+}
+
+const gerenareteFormOptions = (config: Object) => ({
+  fields: {
+    amount: { template: AmountInputTemplate },
+    address: { template: AddressInputTemplate, config, label: 'To' },
+  },
+  order: ['amount', 'address']
+});
+
+const Container = styled.View`
+  justifyContent: flex-start;
+  paddingTop: 20px;
+  flex: 1;
+  alignSelf: stretch;
+`;
+
+export default class SendModal extends React.Component<Props, State> {
+
+  _form: t.form
+
+  state = {
+    isScanning: false,
+    value: {
+      address: '',
+      amount: 0,
+    }
+  }
+
+  handleChange = (value: Object) => {
+    this.setState({ value });
+  };
+
+  handleFormSubmit = () => {
+    const value = this._form.getValue();
+    if (!value) return;
+  };
+
+  handleToggleQRScanningState = () => {
+    this.setState({
+      isScanning: !this.state.isScanning
+    })
+  };
+
+  handleQRRead = (address: string) => {
+    this.setState({ value: { ...this.state.value, address }, isScanning: false });
+  };
+
+
+  render() {
+    const { address, isVisible, onDismiss } = this.props;
+    const { value, isScanning } = this.state;
+    const formOptions = gerenareteFormOptions({ onIconPress: this.handleToggleQRScanningState });
+    const qrScannnerComponent = <QRCodeScanner validator={isValidETHAddress} isActive={isScanning} onDismiss={this.handleToggleQRScanningState} onRead={this.handleQRRead} />
+    return (
+      <SlideModal title="send." isVisible={isVisible} onDismiss={onDismiss} fullScreenComponent={qrScannnerComponent}>
+        <Container>
+          <Form
+            ref={node => { this._form = node }}
+            type={TRANSACTION_TYPE}
+            options={formOptions}
+            value={value}
+            onChange={this.handleChange}
+          />
+          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', marginTop: 15, padding: 5 }}>
+            <Text>Fee: <Text style={{ fontWeight: 'bold', color: '#000' }}>0.0004ETH</Text></Text>
+            <Text onPress={this.handleFormSubmit} style={{ color: '#2077FD', fontSize: 16, fontWeight: 'bold' }}>Send</Text>
+          </View>
+        </Container>
+      </SlideModal>
+    );
+  }
+}
