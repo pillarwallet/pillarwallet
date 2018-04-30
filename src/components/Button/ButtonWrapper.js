01bab7af477181c5c4f4ce727ade291123fd49a1
Rostislav Chuklin <rostislavcn@gmail.com>
2018-04-30 17:35:57 +0100

    minore button UI tweak

--- src/components/Button/ButtonWrapper.js
+++ src/components/Button/ButtonWrapper.js
@@ -7,7 +7,7 @@ const ButtonWrapper = styled.TouchableHighlight`
   background: ${props => props.backgroundColor};
   margin-bottom: ${props => (props.marginBottom)};
   border-radius: 20;
-  box-shadow: ${props => `0px .5px 1px ${props.backgroundColor}`};
+  box-shadow: ${props => `0px .5px .5px ${props.backgroundColor}`};
   width: ${props => (props.width || '80%')};
 `;
 
