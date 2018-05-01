cb654dd8d7f7488e5e7ee30763da3713aba61c76
Rostislav Chuklin <rostislavcn@gmail.com>
2018-05-01 17:14:50 +0100

    confirmOTPAction saves OTP state

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,12 +1,17 @@
 // @flow
 import { NavigationActions } from 'react-navigation';
 import { ONBOARDING_FLOW } from 'constants/navigationConstants';
+import Storage from 'services/storage';
+
+const storage = Storage.getInstance('db');
 
 export const confirmOTPAction = (code: string) => {
   return async (dispatch: Function) => {
     // VALIDATE OTP
     if (code !== '1111') return;
+    await storage.save('app_settings', { OTP: true });
     dispatch(NavigationActions.navigate({ routeName: ONBOARDING_FLOW }));
   };
 };
+
 // TODO: SEND OTP
