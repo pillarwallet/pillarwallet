7fae9af1da8d5d6ad6a53fdc2097190d6be271bb
Jegor Sidorenko <jegor@nfm.lt>
2018-05-01 11:16:36 +0100

    Small refactoring

--- src/components/HyperLink/HyperLink.js
+++ src/components/HyperLink/HyperLink.js
@@ -4,25 +4,20 @@ import { Linking } from 'react-native';
 import styled from 'styled-components/native';
 
 type Props = {
-  children: any,
-  url: string
+  children: React.Node,
+  url: string,
 }
 
 const HyperLinkText = styled.Text`
   color: rgb(32,119,253);
 `;
 
-export default class HyperLink extends React.Component<Props> {
-  openLink(url: string) {
-    Linking.openURL(url);
-  }
+const HyperLink = (props: Props) => {
+  return (
+    <HyperLinkText onPress={() => Linking.openURL(props.url)}>
+      {props.children}
+    </HyperLinkText>
+  );
+};
 
-  render() {
-    return (
-      <HyperLinkText onPress={() => this.openLink(this.props.url)}>
-        {this.props.children}
-      </HyperLinkText>
-    );
-  }
-}
-
+export default HyperLink;
