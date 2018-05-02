4239f759ae36968309c28f94380cbaf31f754783
Lewis Flude <lewis.flude@gmail.com>
2018-05-02 14:39:52 +0100

    Use legacy way of importing styled-components to fix validation error

--- src/screens/BackupPhraseValidate/BackupPhraseValidate.js
+++ src/screens/BackupPhraseValidate/BackupPhraseValidate.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import type { NavigationScreenProp } from 'react-navigation';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import { Container } from 'components/Layout';
 import Wrapper from 'components/Wrapper';
 import { Title, Body, Label } from 'components/Typography';
