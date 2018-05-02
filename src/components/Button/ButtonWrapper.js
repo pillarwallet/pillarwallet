d7d487ba28d179d17a27e121a0e8f91ff4272f8b
Lewis Flude <lewis.flude@gmail.com>
2018-05-02 15:24:20 +0100

    Improve button styling

--- src/components/Button/ButtonWrapper.js
+++ src/components/Button/ButtonWrapper.js
@@ -3,13 +3,13 @@ import styled from 'styled-components/native';
 
 const ButtonWrapper = styled.TouchableHighlight`
   align-items: center;
-  padding: ${props => (props.small ? '5px' : '12px 5px')};
+  padding: ${props => (props.small ? '5px 20px' : '10px 80px')};
   background: ${props => props.backgroundColor};
   margin-top: ${props => (props.marginTop)};
   margin-bottom: ${props => (props.marginBottom)};
   border-radius: 40;
   box-shadow: ${props => `0px .5px .5px ${props.backgroundColor}`};
-  width: ${props => (props.width || '80%')};
+  width: ${props => (props.width || 'auto')};
 `;
 
 export default ButtonWrapper;
