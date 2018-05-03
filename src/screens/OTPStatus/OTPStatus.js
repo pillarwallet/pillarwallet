291b79172f3bcc1c3ce86c96ab4f906ad40414e4
Franky Aguilar <franky@99centbrains.com>
2018-05-03 12:28:59 +0100

    Adding SIgnup Screens to OPT

---
+++ src/screens/OTPStatus/OTPStatus.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from 'react';
+import type { NavigationScreenProp } from 'react-navigation';
+import { OTP } from 'constants/navigationConstants';
+import { Container } from 'components/Layout';
+import Wrapper from 'components/Wrapper';
+import HyperLink from 'components/HyperLink';
+import { Title, Body } from 'components/Typography';
+
+type Props = {
+  navigation: NavigationScreenProp<*>,
+}
+class OTPStatus extends React.Component<Props> {
+  loginAction = () => {
+    this.props.navigation.navigate(OTP);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Wrapper padding>
+          <Title>thanks</Title>
+          <Body>Congratulations!</Body>
+          <Body>You have been placed on our waiting list. We will notify you once your access has been approved.</Body>
+          <Body>
+            You can also check <HyperLink url="http://pillarproject.io/">Latest Updates</HyperLink> to stay informed about product updates and releases.
+          </Body>
+        </Wrapper>
+      </Container>
+    );
+  }
+}
+
+export default OTPStatus;
---
+++ src/screens/Signup/Signup.js
@@ -0,0 +1,148 @@
+// @flow
+import * as React from 'react';
+import type { NavigationScreenProp } from 'react-navigation';
+import { OTP_STATUS } from 'constants/navigationConstants';
+import styled from 'styled-components/native';
+import { Container, Footer, Center } from 'components/Layout';
+import Wrapper from 'components/Wrapper';
+import Button from 'components/Button';
+import HyperLink from 'components/HyperLink';
+import { Title, Body, Label } from 'components/Typography';
+import { Form, Picker, Icon, Input } from 'native-base';
+import countries from 'utils/countries.json';
+
+type Props = {
+  navigation: NavigationScreenProp<*>,
+}
+
+type State = {
+  selectedCountry: string,
+  selectedCountryFlag: string,
+  selectedCountryCallingCode: string,
+}
+
+const LoginForm = styled(Form)`
+  margin: 10px 0 40px;
+`;
+
+const InputField = styled(Input)`
+  border-bottom-width: 1px;
+  border-color: rgb(151,151,151);
+  font-size: 24px;
+  margin-bottom: 10px;
+
+`;
+
+const PhoneInput = styled(Input)`
+  border-bottom-width: 1px;
+  border-color: rgb(151,151,151);
+  font-size: 24px;
+`;
+
+const Emoji = styled.Text`
+  flex: 0 0 40px;
+  font-size: 36px;
+  line-height: 50px;
+`;
+
+const CountryPicker = styled(Picker)`
+  flex: 1;
+  align-self: flex-end;
+`;
+
+const CountryPickerWrapper = styled.View`
+  width: 100%;
+  margin-bottom: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom-width: 1px;
+  border-color: rgb(151,151,151);
+`;
+
+const FooterText = styled(Label)`
+  text-align: center;
+  max-width: 300px;
+`;
+
+class Signup extends React.Component<Props, State> {
+  state = {
+    selectedCountry: 'GB',
+    selectedCountryFlag: '🇬🇧',
+    selectedCountryCallingCode: '44',
+  };
+
+  generateCountryListPickerItems() {
+    return Object.keys(countries)
+      .map((key) => countries[key])
+      .map((country) => (
+        <Picker.Item
+          label={country.name.common}
+          flag={country.flag}
+          value={country.cca2}
+          key={country.cca2}
+        />
+      ));
+  }
+
+  handleValueChange(value: string) {
+    const country = countries.find(({ cca2 }) => cca2 === value);
+    this.setState({
+      selectedCountry: value,
+      selectedCountryFlag: country.flag || '🌍',
+      selectedCountryCallingCode: country.callingCode || '00',
+    });
+  }
+
+  loginAction = () => {
+    this.props.navigation.navigate(OTP_STATUS);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Wrapper padding>
+          <Title>signup</Title>
+          <Body>Securely manage your digital assets and participate in exlusive Initial Coin Offerings.</Body>
+          <LoginForm>
+            <Label>Firstname</Label>
+            <InputField
+              isFocused
+            />
+            <Label>Surname</Label>
+            <InputField />
+            <Label>E-mail</Label>
+            <InputField />
+            <Label>Country</Label>
+            <CountryPickerWrapper>
+              <Emoji>{this.state.selectedCountryFlag}</Emoji>
+              <CountryPicker
+                iosHeader="Select Country"
+                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                mode="dropdown"
+                selectedValue={this.state.selectedCountry}
+                handleValueChange={this.handleValueChange}
+              >
+                {this.generateCountryListPickerItems()}
+              </CountryPicker>
+            </CountryPickerWrapper>
+            <Label>Phone</Label>
+            <PhoneInput
+              defaultValue={`+${this.state.selectedCountryCallingCode}`}
+              keyboardType="phone-pad"
+            />
+          </LoginForm>
+        </Wrapper>
+        <Footer>
+          <Button block onPress={this.loginAction} title="Next" marginBottom="10px" />
+          <Center>
+            <FooterText>
+              By signing into Pillar Wallet you are agreeing to our <HyperLink url="http://pillarproject.io/">Terms</HyperLink> and <HyperLink url="http://pillarproject.io/">Privacy policy</HyperLink>
+            </FooterText>
+          </Center>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+export default Signup;
