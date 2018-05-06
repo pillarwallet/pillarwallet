01dd3b0ebd73032107b0d033592176b5d1a1b3cf
Franky Aguilar <franky@99centbrains.com>
2018-05-06 12:44:38 +0100

    Blue Dot Title Component.

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,11 +1,39 @@
 // @flow
+import * as React from 'react';
 import styled from 'styled-components/native';
 
-const Title = styled.Text`
-  font-size: 32;
-  margin: 20px 0;
-  padding: ${props => (props.padding ? '0 20px' : '0')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
+type Props = {
+  title: string,
+};
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  margin: 20px 0px;
 `;
+
+const Text = styled.Text`
+  font-size: 24;
+  font-weight: 700;
+  padding: ${props => (props.padding ? '0 20px' : '0')};
+  text-align: ${props => (props.align || 'left')};
+`;
+
+const BlueDot = styled.View`
+width: 4;
+height: 4;
+background-color: #25A8E5;
+margin-left: 1px;
+margin-top: 20px
+`;
+
+
+const Title = (props: Props) => {
+  return (
+    <Wrapper>
+      <Text>{props.title}</Text>
+      <BlueDot />
+    </Wrapper>
+  );
+};
 
 export default Title;
