c74084e2e3caa9f4d28da06285ca596b6f825efd
Jegor Sidorenko <jegor@nfm.lt>
2018-05-08 15:09:56 +0100

    Fix Title component's props

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,32 +4,32 @@ import styled from 'styled-components/native';
 
 type Props = {
   title: string,
+  align?: string,
 };
 
 const Wrapper = styled.View`
   flex-direction: row;
   margin: 20px 0px;
+  justify-content: ${props => (props.align || 'flex-start')};
 `;
 
 const Text = styled.Text`
   font-size: 24;
   font-weight: 700;
-  padding: ${props => (props.padding ? '0 20px' : '0')};
-  text-align: ${props => (props.align || 'left')};
 `;
 
 const BlueDot = styled.View`
-width: 4;
-height: 4;
-background-color: #25A8E5;
-margin-left: 1px;
-margin-top: 20px
+  width: 4;
+  height: 4;
+  background-color: #25A8E5;
+  margin-left: 1px;
+  margin-top: 20px;
 `;
 
 
 const Title = (props: Props) => {
   return (
-    <Wrapper>
+    <Wrapper {...props}>
       <Text>{props.title}</Text>
       <BlueDot />
     </Wrapper>
