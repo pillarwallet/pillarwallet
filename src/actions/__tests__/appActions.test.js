af582456a3070e433228c50262fa10c808db2d22
Jegor Sidorenko <jegor@nfm.lt>
2018-05-09 11:39:11 +0100

    Forgot to rename the test

--- src/actions/__tests__/appActions.test.js
+++ src/actions/__tests__/appActions.test.js
@@ -6,7 +6,7 @@ import { initAppAndRedirectAction } from '../appActions';
 
 const mockStore = configureMockStore([thunk]);
 const store = mockStore({});
-describe('App settings actions', () => {
+describe('App actions', () => {
   it('should trigger the app settings updated with any redirection due to the empty storage', () => {
     const expectedActions = [
       { type: UPDATE_APP_SETTINGS, payload: {} },
