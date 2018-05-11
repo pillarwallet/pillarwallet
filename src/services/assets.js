3cb53cddadff3c1c8abedba759ea0392c80dda9b
Rostislav Chuklin <rostislavcn@gmail.com>
2018-05-11 15:43:36 +0100

    fix to test:coverage

--- src/services/assets.js
+++ src/services/assets.js
@@ -2,6 +2,8 @@
 import { Contract, utils, providers } from 'ethers';
 import { NETWORK_PROVIDER } from 'react-native-dotenv';
 
+const PROVIDER = NETWORK_PROVIDER;
+
 const CONTRACT_ABI = [{
   constant: true,
   inputs: [
@@ -62,7 +64,7 @@ export async function transferERC20(options: ERC20TransferOptions) {
     amount,
     wallet,
   } = options;
-  wallet.provider = providers.getDefaultProvider(NETWORK_PROVIDER);
+  wallet.provider = providers.getDefaultProvider(PROVIDER);
   const contract = new Contract(contractAddress, CONTRACT_ABI, wallet);
   const numberOfDecimals = 18;
   const transaction = await contract.transfer(to, utils.parseUnits(amount.toString(), numberOfDecimals));
@@ -83,7 +85,7 @@ export async function transferETH(options: ETHTransferOptions) {
     value: utils.parseEther(amount.toString()),
     to,
   };
-  wallet.provider = providers.getDefaultProvider(NETWORK_PROVIDER);
+  wallet.provider = providers.getDefaultProvider(PROVIDER);
   const transaction = await wallet.sendTransaction(trx);
   return transaction;
 }
@@ -91,13 +93,13 @@ export async function transferETH(options: ETHTransferOptions) {
 // Fetch methods are temporary until the BCX API provided
 
 export async function fetchETHBalance(walletAddress: Address) {
-  const provider = providers.getDefaultProvider(NETWORK_PROVIDER);
+  const provider = providers.getDefaultProvider(PROVIDER);
   const balance = await provider.getBalance(walletAddress).then(utils.formatEther);
   return balance;
 }
 
 export async function fetchERC20Balance(walletAddress: Address, contractAddress: Address) {
-  const provider = providers.getDefaultProvider(NETWORK_PROVIDER);
+  const provider = providers.getDefaultProvider(PROVIDER);
   const contract = new Contract(contractAddress, CONTRACT_ABI, provider);
   const balance = await contract.balanceOf(walletAddress).then(utils.formatEther);
   return balance;
