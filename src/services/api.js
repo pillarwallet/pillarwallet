5325d63e14293983d4d87d1852fd03da756d4988
Rostislav Chuklin <rostislavcn@gmail.com>
2018-05-30 12:37:59 +0100

    fetch initial assets sdk fix

--- src/services/api.js
+++ src/services/api.js
@@ -5,22 +5,22 @@ import { SDK_PROVIDER } from 'react-native-dotenv';
 
 // temporary here
 export default function SDKWrapper() {
-  this.pillarSdk = null;
+  this.pillarWalletSdk = null;
 }
 
 SDKWrapper.prototype.init = function (privateKey: string) {
-  this.pillarSdk = new PillarSdk({
+  this.pillarWalletSdk = new PillarSdk({
     privateKey: privateKey.slice(2),
     apiUrl: SDK_PROVIDER,
   });
 };
 
 SDKWrapper.prototype.registerOnBackend = function (fcm: string) {
-  return this.pillarSdk.wallet.register({ fcmToken: fcm }).catch(() => ({}));
+  return this.pillarWalletSdk.wallet.register({ fcmToken: fcm }).catch(() => ({}));
 };
 
 SDKWrapper.prototype.getInitialAssets = function (walletId: string) {
-  return this.pillarSdk.asset.defaults({ walletId })
+  return this.pillarWalletSdk.asset.defaults({ walletId })
     .catch(() => [])
     .then(transformAssetsToObject);
 };
