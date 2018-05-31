4d9171ef6338009cd4ff54d332b26a24e3a3cb62
Rostislav Chuklin <RostislavCn@gmail.com>
2018-05-31 17:06:13 +0100

    Feature/username (#170)
    * update user details via sdk and save in local storage
    
    * import wallet minor js fix
    
    * connect Profile page to Redux
    
    * onboardingActions test fix
    
    * dev code

--- src/screens/NewProfile/NewProfile.js
+++ src/screens/NewProfile/NewProfile.js
@@ -1,30 +1,124 @@
 // @flow
 import * as React from 'react';
+import styled from 'styled-components/native';
+import t from 'tcomb-form-native';
+import { connect } from 'react-redux';
 import { KeyboardAvoidingView } from 'react-native';
 import type { NavigationScreenProp } from 'react-navigation';
 import { Container, ScrollWrapper } from 'components/Layout';
 import { LEGAL_TERMS } from 'constants/navigationConstants';
 import HeaderLink from 'components/HeaderLink';
-import { Paragraph, Label } from 'components/Typography';
+import TextInput from 'components/TextInput';
+import { Paragraph } from 'components/Typography';
 import Title from 'components/Title';
-import { LoginForm, InputField } from 'components/Form';
+import { updateLocalUserAction } from 'actions/userActions';
+
+const { Form } = t.form;
+
+const LoginForm = styled(Form)`
+  margin: 10px 0 40px;
+`;
 
 type Props = {
   navigation: NavigationScreenProp<*>,
+  updateUser: Function,
 }
 
-class NewProfile extends React.Component<Props> {
-  static navigationOptions = ({ navigation }: { navigation: NavigationScreenProp<*> }) => ({
-    headerRight: (
-      <HeaderLink
-        onPress={() => navigation.navigate(LEGAL_TERMS)}
-      >
-      Next
-      </HeaderLink>
-    ),
-  });
+type State = {
+  value: ?{
+    username: ?string,
+    fullName: ?string,
+  },
+}
+
+function InputTemplate(locals) {
+  const errorMessage = locals.error;
+  const inputProps = {
+    onChange: locals.onChange,
+    onBlur: locals.onBlur,
+    value: locals.value,
+    keyboardType: locals.keyboardType,
+    autoCapitalize: 'none',
+    style: {
+      fontSize: 24,
+      lineHeight: 0,
+    },
+  };
+
+  return (
+    <TextInput
+      errorMessage={errorMessage}
+      id={locals.label}
+      label={locals.label}
+      inputProps={inputProps}
+    />
+  );
+}
+
+const formStructure = t.struct({
+  username: t.String,
+  fullName: t.String,
+});
+
+const formOptions = {
+  fields: {
+    username: {
+      template: InputTemplate,
+      error: 'Please specify username',
+    },
+    fullName: {
+      template: InputTemplate,
+      error: 'Please provide full name',
+    },
+  },
+};
+
+class NewProfile extends React.Component<Props, State> {
+  _form: t.form;
+
+  state = {
+    value: null,
+  }
+
+  static navigationOptions = ({ navigation }: { navigation: NavigationScreenProp<*> }) => {
+    const { params = {} } = navigation.state;
+    return {
+      headerRight: (
+        <HeaderLink onPress={params.handleSubmit}>
+          Next
+        </HeaderLink>
+      ),
+    };
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    navigation.setParams({
+      handleSubmit: this.handleSubmit,
+    });
+  }
+
+  handleChange = (value: Object) => {
+    this.setState({
+      value,
+    });
+  }
+
+  handleSubmit = () => {
+    const { navigation, updateUser } = this.props;
+    const value = this._form.getValue();
+    if (!value) return;
+    const [firstName, lastName] = value.fullName.split(' ');
+    updateUser({
+      username: value.username,
+      firstName,
+      lastName,
+    });
+    navigation.navigate(LEGAL_TERMS);
+  }
 
   render() {
+    const { value } = this.state;
     return (
       <KeyboardAvoidingView
         behavior="position"
@@ -35,15 +129,13 @@ class NewProfile extends React.Component<Props> {
           <ScrollWrapper padding>
             <Title title="create profile" />
             <Paragraph>Fill our your profile.</Paragraph>
-            <LoginForm>
-              <Label>Username</Label>
-              <InputField
-                isFocused
-                placeholder="@"
-              />
-              <Label>Full Name</Label>
-              <InputField />
-            </LoginForm>
+            <LoginForm
+              innerRef={node => { this._form = node; }}
+              type={formStructure}
+              options={formOptions}
+              value={value}
+              onChange={this.handleChange}
+            />
           </ScrollWrapper>
         </Container>
       </KeyboardAvoidingView>
@@ -51,4 +143,8 @@ class NewProfile extends React.Component<Props> {
   }
 }
 
-export default NewProfile;
+const mapDispatchToProps = (dispatch) => ({
+  updateUser: (user: Object) => dispatch(updateLocalUserAction(user)),
+});
+
+export default connect(null, mapDispatchToProps)(NewProfile);
