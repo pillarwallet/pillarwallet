68eceb7abafd42bd0566a84769829c38a5140b4e
Jegor Sidorenko <jegor@nfm.lt>
2018-06-08 09:18:29 +0100

    Show push notifications from BCX

--- src/actions/notificationsActions.js
+++ src/actions/notificationsActions.js
@@ -1,11 +1,55 @@
 // @flow
 
 import firebase from 'react-native-firebase';
+import { Toast } from 'native-base';
+import { validBcxTransaction } from 'utils/validators';
+import { formatMoney } from 'utils/common';
 
 let notificationsListener = null;
 
+const parseNotification = (notificationBody: string) => {
+  let messageObj;
+  try {
+    messageObj = JSON.parse(notificationBody);
+  } catch (e) {
+    console.log(e); // eslint-disable-line
+  }
+  return messageObj;
+};
+
+const processNotification = (notification: Object, myEthAddress) => {
+  let messageText = '';
+  if (notification.type === 'BCX') {
+    // transaction notification
+    const transaction = parseNotification(notification.msg);
+    if (!transaction || !validBcxTransaction(transaction)) return;
+
+    const sender = transaction.from.toUpperCase();
+    const receiver = transaction.to.toUpperCase();
+    const transactionValue = formatMoney(transaction.value, 4);
+
+    if (receiver === myEthAddress && transaction.status === 'pending') {
+      messageText = `New incoming transaction (${transactionValue} ${transaction.asset})`;
+    } else if (receiver === myEthAddress && transaction.status === 'confirmed') {
+      messageText = `Transaction of ${transactionValue} ${transaction.asset} confirmed`;
+    } else if (sender === myEthAddress && transaction.status === 'pending') {
+      messageText = 'Transaction sent';
+    } else if (sender === myEthAddress && transaction.status === 'confirmed') {
+      messageText = `Your transaction of ${transactionValue} ${transaction.asset} was received`;
+    }
+  }
+
+  if (!messageText) return;
+
+  Toast.show({
+    text: messageText,
+    buttonText: '',
+  });
+};
+
 export const startListeningNotificationsAction = () => {
-  return async (dispatch: Function) => { // eslint-disable-line
+  return async (dispatch: Function, getState: Function) => { // eslint-disable-line
+    const { wallet: { data: wallet } } = getState();
     // check if permissions enabled
     const enabled = await firebase.messaging().hasPermission();
     if (!enabled) {
@@ -17,7 +61,8 @@ export const startListeningNotificationsAction = () => {
     await firebase.messaging().getToken();
     if (notificationsListener) return;
     notificationsListener = firebase.messaging().onMessage((message) => {
-      console.log(message);
+      if (!message._data || !Object.keys(message._data).length) return;
+      processNotification(message._data, wallet.address.toUpperCase());
     });
   };
 };
