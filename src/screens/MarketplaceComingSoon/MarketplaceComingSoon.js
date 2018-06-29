26585a248e6faea8b9eeace10d58472f7d3b0609
Lewis Flude <lewis.flude@gmail.com>
2018-06-29 16:30:11 +0100

    Add new Marketplace coming soon screen

---
+++ src/screens/MarketplaceComingSoon/MarketplaceComingSoon.js
@@ -0,0 +1,49 @@
+// @flow
+import * as React from 'react';
+import styled from 'styled-components/native';
+import { Container, Wrapper } from 'components/Layout';
+import { Paragraph } from 'components/Typography';
+import Title from 'components/Title';
+import { baseColors, fontSizes, fontWeights } from 'utils/variables';
+
+const ComingSoonBanner = styled.View`
+  padding: 16px;
+  margin-bottom: 20px;
+  background: ${baseColors.sunYellow};
+`;
+
+const ComingSoonBannerText = styled.Text`
+  font-size: ${fontSizes.extraLarge};
+  font-weight: ${fontWeights.bold};
+`;
+
+
+const MarketplaceComingSoon = () => {
+  return (
+    <Container>
+      <Wrapper regularPadding>
+        <Title title="marketplace" />
+      </Wrapper>
+      <ComingSoonBanner>
+        <ComingSoonBannerText>Coming Soon</ComingSoonBannerText>
+      </ComingSoonBanner>
+      <Wrapper regularPadding>
+
+        <Paragraph>
+          Guarenteed trusted ICOs
+        </Paragraph>
+        <Paragraph>
+          Store purchased tokens in wallet
+        </Paragraph>
+        <Paragraph>
+          Sell tokens when you like it
+        </Paragraph>
+        <Paragraph>
+          Safer connections with others
+        </Paragraph>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default MarketplaceComingSoon;
