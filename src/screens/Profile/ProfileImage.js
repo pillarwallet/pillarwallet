886d71283b509b30accd9989a30c9654371948bf
Vytaute Vilutyte <v.vilutytei@gmail.com>
2018-06-29 13:32:01 +0300

    Feature/profile-settings-screen (#215)
    * Add Profile/Settings screen front-end and update PortfolioBalance component
    
    * Update the value passed to the switch element
    
    * Add modals for Profile settings
    
    * Add modal for currency selector
    
    * Update TabBar front-end
    
    * Cleanup
    
    * Apply small fixes
    
    * Merge in RequestPinForTransaction
    
    * Update yarn.lock
    
    * Reset project.pbxproj
    
    * Update tab icons
    
    * Update handleChangeRequestPinForTransaction function and remove unused variable
    
    * Assign default value to requestPinForTransaction
    
    * Fix mapping issue
    
    * Update settings
    
    * Remove unused dependencies
    
    * Revert navigationConstants.js
    
    * Small update
    
    * Change assets path
    
    * Change the color for not selected bottom tab's icon
    
    * Fix 'Request Pin for transaction' button
    
    * Fix padding of the bottom tabs

---
+++ src/screens/Profile/ProfileImage.js
@@ -0,0 +1,50 @@
+// @flow
+import * as React from 'react';
+import styled from 'styled-components/native';
+import { baseColors, fontSizes, fontWeights } from 'utils/variables';
+
+const CircleImage = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+`;
+
+const ImageTouchable = styled.TouchableOpacity`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #d8d8d8;
+`;
+
+const AvatarText = styled.Text`
+  font-size: ${fontSizes.large};
+  font-weight: ${fontWeights.bold};
+  color: ${baseColors.white};
+`;
+
+type Props = {
+  uri?: string,
+  userName: string,
+}
+
+const ProfileImage = (props: Props) => {
+  const { uri, userName } = props;
+
+  const initials = userName
+    .split(' ')
+    .map(name => name.substring(0, 1))
+    .join('')
+    .toUpperCase();
+
+  return (
+    <ImageTouchable>
+      {!uri && <AvatarText>{initials}</AvatarText>}
+      {!!uri && <CircleImage source={{ uri }} />}
+    </ImageTouchable>
+  );
+};
+
+export default ProfileImage;
