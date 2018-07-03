d9923468a0ea33fe4feb796d428a462e2f963769
Lewis Flude <lewis.flude@gmail.com>
2018-07-03 17:31:12 +0100

    Initialise design fro ContactCard

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -1,8 +1,17 @@
 // @flow
 import * as React from 'react';
+import { baseColors, fontSizes, fontWeights } from 'utils/variables';
 import styled from 'styled-components/native';
 
 const ContactCardWrapper = styled.View`
+  background: ${baseColors.white};
+  border: 1px solid ${baseColors.lightGray};
+  height: 75px;
+  padding: 14px;
+  border-radius: 4px;
+  justify-content: flex-start;
+  flex-direction: row;
+  align-items: center;
 
 `;
 
@@ -10,14 +19,30 @@ const ContactCardAvatar = styled.Image`
 
 `;
 
-const ContactCardName = styled.Text`
+const ContactCardAvatarWrapper = styled.View`
+  height: 44px;
+  width: 44px;
+  border: 2px solid ${baseColors.white};
+  background: ${baseColors.mediumGray};
+  border-radius: 22px;
+  margin-right: 14px;
+  shadow-color: ${baseColors.black};
+  shadow-offset: 0 0;
+  shadow-radius: 4px     ;
+  shadow-opacity: 0.1;
+`;
 
+const ContactCardName = styled.Text`
+  font-size: ${fontSizes.medium};
+  font-weight: ${fontWeights.bold};
 `;
 
 const ContactCard = () => {
   return (
     <ContactCardWrapper>
-      <ContactCardAvatar />
+      <ContactCardAvatarWrapper>
+        <ContactCardAvatar />
+      </ContactCardAvatarWrapper>
       <ContactCardName>John Doe</ContactCardName>
     </ContactCardWrapper>
   );
--- src/screens/People/People.js
+++ src/screens/People/People.js
@@ -1,11 +1,20 @@
 // @flow
 import * as React from 'react';
 import { Container, Wrapper } from 'components/Layout';
+import Title from 'components/Title';
+import ContactCard from 'components/ContactCard';
 
 const People = () => {
   return (
     <Container>
-      <Wrapper regularPadding />
+      <Wrapper regularPadding>
+        <Title title="people" />
+        <ContactCard />
+        <ContactCard />
+        <ContactCard />
+        <ContactCard />
+
+      </Wrapper>
     </Container>
   );
 };
