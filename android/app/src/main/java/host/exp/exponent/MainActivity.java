abeb355927b5bde29b8e57facbc405f0c2df8116
Deimantas <deimantas@imas.lt>
2018-07-03 22:26:00 +0300

    force lottie native lib to use lower version due Java 8 issue, prodRelease SDK 19->21 due multidex issue

--- android/app/src/main/java/host/exp/exponent/MainActivity.java
+++ android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends DetachActivity {
 
   @Override
   public String publishedUrl() {
-    return "exp://exp.host/@deimantas/pillar-wallet";
+    return "exp://exp.host/@pillarbot/pillar-wallet";
   }
 
   @Override
