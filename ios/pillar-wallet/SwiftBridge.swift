4031ec6c263389639ad86a02dd99262fddc65efa
Mantas <9mantas@gmail.com>
2018-07-10 22:15:37 +0200

    added objc - swift bridging file

---
+++ ios/pillar-wallet/SwiftBridge.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftBridge.swift
+//  pillar-wallet
+//
+//  Created by Mantas on 10/07/2018.
+//  Copyright © 2018 650 Industries, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SwiftBridge: NSObject {
+
+}
