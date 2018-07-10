bb285e253ccf58f81a30bf40b364004bd4efee93
Mantas <9mantas@gmail.com>
2018-07-10 23:22:29 +0200

    Added comments on dependent files

--- ios/pillar-wallet/SwiftBridge.swift
+++ ios/pillar-wallet/SwiftBridge.swift
@@ -1,13 +1,7 @@
-//
-//  SwiftBridge.swift
-//  pillar-wallet
-//
-//  Created by Mantas on 10/07/2018.
-//  Copyright © 2018 650 Industries, Inc. All rights reserved.
-//
 
-import UIKit
-
-class SwiftBridge: NSObject {
-
-}
+/*
+ 
+ This file is needed to support Swift native module.
+ Do not remove
+ 
+ */
