6c0ebe0f13cb98e85219ab4a1b5c3b2b7a85d01f
Jegor Sidorenko <jegor@nfm.lt>
2018-07-16 16:39:50 +0300

    Update text on the Marketplace screen

--- src/screens/MarketplaceComingSoon/MarketplaceComingSoon.js
+++ src/screens/MarketplaceComingSoon/MarketplaceComingSoon.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import styled from 'styled-components/native';
 import { Container, Wrapper } from 'components/Layout';
+import { Paragraph } from 'components/Typography';
 import Title from 'components/Title';
 import { baseColors, fontSizes, fontWeights } from 'utils/variables';
 
@@ -16,7 +17,6 @@ const ComingSoonBannerText = styled.Text`
   font-weight: ${fontWeights.bold};
 `;
 
-
 const MarketplaceComingSoon = () => {
   return (
     <Container>
@@ -26,7 +26,22 @@ const MarketplaceComingSoon = () => {
       <ComingSoonBanner>
         <ComingSoonBannerText>Coming Soon</ComingSoonBannerText>
       </ComingSoonBanner>
-      <Wrapper center regularPadding />
+      <Wrapper regularPadding>
+        <Paragraph style={{ marginTop: 10 }}>
+          The marketplace will contain many services.
+        </Paragraph>
+        <Paragraph>
+          20|30 will build the first service, an ICO platform, delivered through the Pillar wallet.
+        </Paragraph>
+        <Paragraph>
+          As a participant in the UK Financial Control Authority sandbox,
+          20|30 aims to conduct the UK’s first primary issuance of an equity token later this year.
+        </Paragraph>
+        <Paragraph>
+          The London Stock Exchange Group (LSEG) and the Financial Conduct Authority are working with 20|30 and Nivaura
+          to build a platform for corporate equity issuance.
+        </Paragraph>
+      </Wrapper>
     </Container>
   );
 };
