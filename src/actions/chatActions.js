7b85c90dc8b45c411bd5883592ce346be1a89212
Rostislav Chuklin <RostislavCn@gmail.com>
2018-07-16 13:31:17 +0100

    Feature/people chat (#283)
    * Add Chat navigation
    
    * Add ChatScreen styles (beta)
    
    * added rn-signal-messaging-module
    
    * rn-signal-messaging module: init, register
    
    * .env signal host added and rn mdule yarn.lock update
    
    * rn-signal-messaging-module version up
    
    * Update chat styles
    
    * Show chat in modal, add load more button
    
    * Pass the real username to the chat
    
    * Add custom avatar to chat
    
    * Add missing types
    
    * profile clear reset messaging, chat integration to module
    
    * Update Android status bar style
    
    * Update chat styles
    
    * Update chat list item style, add fake connect with user button and action
    
    * chat fixes
    
    * added objc - swift bridging file
    
    * Added comments on dependent files
    
    * Change send massage icon
    
    * chat front-end to back-end fixes
    
    * Apply light status bar for Android (so that icon colours wouldn't change on modal open)
    
    * ios method fix
    
    * updated rn-signal-protocol-messaging version
    
    * add missing embed binaries for signal
    
    * update native base due to missing fonts
    
    * chat actions, reducer, constants
    
    * Toast removed form invitationsActions
    
    * app navigation updates
    
    * Chat UI, service cleanup,
    
    * mocks & linting
    
    * Catch Signals errors
    
    * Missed semicolons :)
    
    * Call addChatContactAction on Send Invitation
    
    * recovered: android signal init host param
    
    * added: correct google services json file
    
    * Move Signals addContact method to getChatByContactAction
    
    * Typo
    
    * fixed: android push notifications onReceive crash
    
    * hide showEarlier btn, fixed signal chat flow
    
    * rn-signal-messaging-module v1.0.40

---
+++ src/actions/chatActions.js
@@ -0,0 +1,48 @@
+// @flow
+import ChatService from 'services/chat';
+import { UPDATE_CHATS, ADD_MESSAGE, UPDATE_MESSAGES } from 'constants/chatConstants';
+
+const chat = new ChatService();
+
+export const getExistingChatsAction = () => {
+  return async (dispatch: Function) => {
+    const chats = await chat.client.getExistingChats().then(JSON.parse).catch(() => null);
+    dispatch({
+      type: UPDATE_CHATS,
+      payload: chats,
+    });
+  };
+};
+
+export const sendMessageByContactAction = (username: string, message: Object) => {
+  return async (dispatch: Function) => {
+    await chat.client.sendMessageByContact(username, message.text).catch(() => null);
+    const timestamp = new Date(message.createdAt).getTime() / 1000;
+    const msg = {
+      content: message.text,
+      savedTimestamp: timestamp,
+      serverTimestamp: timestamp,
+      username: message.user._id,
+    };
+
+    dispatch({
+      type: ADD_MESSAGE,
+      payload: { message: msg, username },
+    });
+  };
+};
+
+export const getChatByContactAction = (username: string, loadEarlier: boolean = false) => {
+  return async (dispatch: Function) => {
+    await chat.client.addContact(username).catch(() => null);
+    if (loadEarlier) {
+      // TODO: split message loading in bunches and load earlier on lick
+    }
+    await chat.client.receiveNewMessagesByContact(username);
+    const receivedMessages = await chat.client.getChatByContact(username).then(JSON.parse).catch(() => null);
+    dispatch({
+      type: UPDATE_MESSAGES,
+      payload: { messages: receivedMessages, username },
+    });
+  };
+};
---
+++ src/constants/chatConstants.js
@@ -0,0 +1,4 @@
+// @flow
+export const UPDATE_MESSAGES = 'UPDATE_MESSAGES';
+export const ADD_MESSAGE = 'ADD_MESSAGE';
+export const UPDATE_CHATS = 'UPDATE_CHATS';
