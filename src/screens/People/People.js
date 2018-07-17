133493162c6114bfffe623766c99cb487a5f4c9a
Jegor Sidorenko <jegor@nfm.lt>
2018-07-17 19:07:45 +0300

    Feature/people section part 2 (#305)
    * Update avatar background color
    
    * Action button fix
    
    * Remove sent connection requests
    
    * Change the border's color of focused search box
    
    * Update colors
    
    * Fix visual bug when we cancel the search
    
    * Restyle people section
    
    * Bugfix
    
    * Replace search icon
    
    * Fix tx fee price
    
    * Fix linting
    
    * Fix Keyboard overlaps bottom tabs menu on android

--- src/screens/People/People.js
+++ src/screens/People/People.js
@@ -9,6 +9,7 @@ import { Icon } from 'native-base';
 import { searchContactsAction, resetSearchContactsStateAction } from 'actions/contactsActions';
 import { fetchInviteNotificationsAction } from 'actions/invitationsActions';
 import { CONTACT, CONNECTION_REQUESTS } from 'constants/navigationConstants';
+import { TYPE_RECEIVED } from 'constants/invitationsConstants';
 import { FETCHING, FETCHED } from 'constants/contactsConstants';
 import { baseColors, UIColors, fontSizes } from 'utils/variables';
 import { Container, Wrapper } from 'components/Layout';
@@ -160,6 +161,7 @@ class PeopleScreen extends React.Component<Props, State> {
     } = this.props;
     const inSearchMode = (query.length >= MIN_QUERY_LENGTH && !!contactState);
     const usersFound = searchResults.apiUsers.length || searchResults.localContacts.length;
+    const pendingConnectionRequests = invitations.filter(({ type }) => type === TYPE_RECEIVED).length;
 
     return (
       <Container>
@@ -176,7 +178,7 @@ class PeopleScreen extends React.Component<Props, State> {
           />
         </Wrapper>
 
-        {!inSearchMode && !!invitations.length &&
+        {!inSearchMode && !!pendingConnectionRequests &&
           <ConnectionRequestBanner
             onPress={this.handleConnectionsRequestBannerPress}
             underlayColor={baseColors.lightGray}
@@ -186,7 +188,7 @@ class PeopleScreen extends React.Component<Props, State> {
                 Connection requests
               </ConnectionRequestBannerText>
               <ConnectionRequestNotificationCircle>
-                {invitations.length}
+                {pendingConnectionRequests}
               </ConnectionRequestNotificationCircle>
               <ConnectionRequestBannerIcon type="Feather" name="chevron-right" />
             </React.Fragment>
