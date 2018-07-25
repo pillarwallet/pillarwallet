82ec6608eda7151f4b0fe20e3e7e4bf6fa15ca96
Lewis Flude <lewis.flude@gmail.com>
2018-07-25 11:25:39 +0100

    Update DetachBuildConstants.java

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp38a4bbfce2ec45389ca2f4a5178939d6://10.26.111.17:19000";
+  public static final String DEVELOPMENT_URL = "exp38a4bbfce2ec45389ca2f4a5178939d6://10.103.210.157:19000";
 
 }
