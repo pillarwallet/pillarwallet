bfc8426e6042d9e572cf3b4516f508f382ed9951
Jegor Sidorenko <jegor@nfm.lt>
2018-07-31 12:29:59 +0100

    Remove unused code

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -33,7 +33,7 @@ import RetryApiRegistration from 'components/RetryApiRegistration';
 import AndroidTabBarComponent from 'components/AndroidTabBarComponent';
 
 // actions
-import { initAppAndRedirectAction, fetchUserAction } from 'actions/appActions';
+import { initAppAndRedirectAction } from 'actions/appActions';
 import {
   stopListeningNotificationsAction,
   startListeningNotificationsAction,
@@ -71,7 +71,7 @@ import {
   CHAT_LIST,
   CHAT,
 } from 'constants/navigationConstants';
-import { PENDING, REGISTERED } from 'constants/userConstants';
+import { PENDING } from 'constants/userConstants';
 
 // models
 import type { Assets } from 'models/Asset';
@@ -313,7 +313,6 @@ const AppFlowNavigation = createStackNavigator(
 type Props = {
   userState: ?string,
   fetchAppSettingsAndRedirect: Function,
-  fetchUser: Function,
   startListeningNotifications: Function,
   stopListeningNotifications: Function,
   startListeningIntercomNotifications: Function,
@@ -332,8 +331,6 @@ class AppFlow extends React.Component<Props, {}> {
 
   componentDidMount() {
     const {
-      fetchUser,
-      userState,
       startListeningNotifications,
       startListeningIntercomNotifications,
       fetchTransactionsHistory,
@@ -343,9 +340,6 @@ class AppFlow extends React.Component<Props, {}> {
       assets,
       wallet,
     } = this.props;
-    if (userState !== REGISTERED) {
-      fetchUser();
-    }
     startListeningNotifications();
     startListeningIntercomNotifications();
     fetchTransactionsHistory(wallet.address);
@@ -410,7 +404,6 @@ const mapStateToProps = ({
 
 const mapDispatchToProps = (dispatch) => ({
   fetchAppSettingsAndRedirect: () => dispatch(initAppAndRedirectAction()),
-  fetchUser: () => dispatch(fetchUserAction()),
   stopListeningNotifications: () => dispatch(stopListeningNotificationsAction()),
   startListeningNotifications: () => dispatch(startListeningNotificationsAction()),
   stopListeningIntercomNotifications: () => dispatch(stopListeningIntercomNotificationsAction()),
