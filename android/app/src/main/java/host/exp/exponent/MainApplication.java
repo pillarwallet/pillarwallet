f984ea330a92d678b5691b80b74019f8a6000b5c
Egidijus <egidijus@imas.lt>
2018-08-01 11:55:52 +0300

    added react-native-background-timer, fixed lock screen sleep timeout issue

--- android/app/src/main/java/host/exp/exponent/MainApplication.java
+++ android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -11,6 +11,7 @@ import expolib_v1.okhttp3.OkHttpClient;
 
 // Needed for `react-native link`
 // import com.facebook.react.ReactApplication;
+import com.ocetnik.timer.BackgroundTimerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import lt.imas.react_native_signal.RNSignalClientPackage;
 import com.tradle.react.UdpSocketsModule;
@@ -59,6 +60,7 @@ public class MainApplication extends ExpoApplication {
 
         // Needed for `react-native link`
         // new MainReactPackage(),
+            new BackgroundTimerPackage(),
             new VectorIconsPackage(),
             new RNSignalClientPackage(),
             new IntercomPackage(),
