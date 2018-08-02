037fabab2fd6e7d82ac6b7d48cef6b601a0ac5f7
Lewis Flude <lewis.flude@gmail.com>
2018-08-02 15:20:13 +0100

    Improve text in Add Token header to follow proper grammar rules
    As seen here https://ux.stackexchange.com/questions/84624/should-text-in-button-be-capitalized-in-english

--- src/screens/Assets/Assets.js
+++ src/screens/Assets/Assets.js
@@ -175,7 +175,7 @@ class AssetsScreen extends React.Component<Props> {
         <Header
           title="assets"
           onNextPress={this.goToAddTokenPage}
-          nextText="manage tokens"
+          nextText="Manage Tokens"
           headerRightFlex="2"
         />
         <ScrollWrapper
