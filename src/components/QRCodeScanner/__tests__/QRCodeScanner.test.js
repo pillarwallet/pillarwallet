da994bd06c1e14da29b6a652e5041ab35095b9d1
Rostislav Chuklin <rostislavcn@gmail.com>
2018-08-07 23:09:46 +0100

    Jest, Flow & minor linking fixes

--- src/components/QRCodeScanner/__tests__/QRCodeScanner.test.js
+++ src/components/QRCodeScanner/__tests__/QRCodeScanner.test.js
@@ -1,33 +1,8 @@
 // @flow
 import * as React from 'react';
-import { View as mockView } from 'react-native';
 import Permissions from 'react-native-permissions';
 import renderer from 'react-test-renderer';
-import 'jest-styled-components';
 import QRCodeScanner from '../QRCodeScanner';
-
-jest.mock('expo', () => {
-  const mockCamera = () => <mockView />;
-  const constants = {
-    Aspect: {},
-    BarCodeType: {},
-    Type: {},
-    CaptureMode: {},
-    CaptureTarget: {},
-    CaptureQuality: {},
-    Orientation: {},
-    FlashMode: {},
-    TorchMode: {},
-  };
-  mockCamera.Constants = constants;
-  return {
-    Camera: mockCamera,
-    Permissions: {
-      askAsync: jest.fn(() => Promise.resolve({ status: 'GRANTED' })),
-      CAMERA: 'camera',
-    },
-  };
-});
 
 describe('QR code scanner', () => {
   it('should ask for permissions on component mount', () => {
