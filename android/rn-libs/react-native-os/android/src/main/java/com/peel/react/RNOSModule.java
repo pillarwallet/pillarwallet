444a04c24a69797ff8d372bc157d6c2f74f49689
Rostislav Chuklin <rostislavcn@gmail.com>
2018-08-08 17:53:53 +0100

    missing rn-libs

---
+++ android/rn-libs/react-native-os/android/src/main/java/com/peel/react/RNOSModule.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2015-present, Peel Technologies, Inc.
+ * All rights reserved.
+ */
+
+package com.peel.react.rnos;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public final class RNOSModule implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<NativeModule>();
+
+        modules.add(new RNOS(reactContext));
+
+        return modules;
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(
+            ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
