b1ef9d0a07a05ccbf0c54ad3087bb701b05a66fa
Rostislav Chuklin <rostislavcn@gmail.com>
2018-08-08 16:12:23 +0100

    missing android files

---
+++ android/rn-libs/react-native-fetch-blob/polyfill/index.js
@@ -0,0 +1,11 @@
+import Blob from './Blob.js'
+import File from './File.js'
+import XMLHttpRequest from './XMLHttpRequest.js'
+import ProgressEvent from './ProgressEvent'
+import Event from './Event'
+import FileReader from './FileReader'
+import Fetch from './Fetch'
+
+export default {
+  Blob, File, XMLHttpRequest, ProgressEvent, Event, FileReader, Fetch
+}
