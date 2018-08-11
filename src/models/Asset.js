99cb3a56ac61bb447e80f5f6b120a47a258dbd35
Jegor Sidorenko <jegor@nfm.lt>
2018-08-11 12:52:47 +0100

    Fix portfolkio balance and transaction send method (#419)

--- src/models/Asset.js
+++ src/models/Asset.js
@@ -2,7 +2,6 @@
 export type Asset = {
   symbol: string,
   name: string,
-  balance: number,
   address: string,
   description: string,
   iconUrl: string,
@@ -12,4 +11,17 @@ export type Asset = {
 
 export type Assets = {
   [string]: Asset,
+};
+
+export type Balances = {
+  [string]: {
+    balance: string,
+    symbol: string,
+  },
+};
+
+export type Rates = {
+  [string]: {
+    [string]: number,
+  },
 };
