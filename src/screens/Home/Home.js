b20905a518f1573a2c20e04f5e60e5116f4b0499
Lewis Flude <lewis.flude@gmail.com>
2018-08-14 17:37:55 +0100

    Make home screen header more consistent with main header

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -11,7 +11,7 @@ import Intercom from 'react-native-intercom';
 import { BaseText } from 'components/Typography';
 import Title from 'components/Title';
 import PortfolioBalance from 'components/PortfolioBalance';
-import { uniqBy } from 'utils/common';
+import { uniqBy, isIphoneX } from 'utils/common';
 import { getUserName } from 'utils/contacts';
 import {
   fetchTransactionsHistoryNotificationsAction,
@@ -70,7 +70,7 @@ const ALL = 'ALL';
 
 const HomeHeader = styled.View`
   padding: 0 ${spacing.rhythm}px;
-  margin-top: ${Platform.OS === 'android' ? '20px' : 0};
+  margin-top: ${isIphoneX() ? '0' : '20px'};
 `;
 
 const HomeHeaderRow = styled.View`
