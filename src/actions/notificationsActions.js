248e39cd496ff5fce9cede35264137664782c9f4
Rostislav Chuklin <rostislavcn@gmail.com>
2018-08-17 15:56:03 +0100

    return notification listener to an old one

--- src/actions/notificationsActions.js
+++ src/actions/notificationsActions.js
@@ -90,7 +90,7 @@ export const startListeningNotificationsAction = () => {
     }
 
     if (notificationsListener) return;
-    notificationsListener = firebase.notifications().onNotification(message => {
+    notificationsListener = firebase.messaging().onMessage(message => {
       if (!message._data || !Object.keys(message._data).length) return;
       const notification = processNotification(message._data, wallet.address.toUpperCase());
       if (!notification) return;
