71cfeaa0f89f348c9e8f1f2aef58b9764367c5c4
Lewis Flude <lewis.flude@gmail.com>
2018-08-28 15:07:30 +0100

    Refactor checkbox logic to disallow people getting past without havin… (#479)
    * Refactor checkbox logic to disallow people getting past without having checked all boxes appropriately
    
    * Make legalterms checking ULTRA SECURE
    
    * Minor changes

--- src/screens/LegalTerms/LegalTerms.js
+++ src/screens/LegalTerms/LegalTerms.js
@@ -48,6 +48,27 @@ class LegalTerms extends React.Component<Props, State> {
     this.setState({ showPrivacyPolicyModal: !this.state.showPrivacyPolicyModal });
   };
 
+  toggleCheckbox = (field: string) => {
+    const { userCheck1, userCheck2, userCheck3 } = this.state;
+    if (field === 'userCheck1') {
+      this.setState({
+        userCheck1: !userCheck1,
+        userCheck3: false,
+      });
+    }
+    if (field === 'userCheck2') {
+      this.setState({
+        userCheck2: !userCheck2,
+        userCheck3: false,
+      });
+    }
+    if (field === 'userCheck3') {
+      this.setState({
+        userCheck3: !userCheck3,
+      });
+    }
+  };
+
   render() {
     const {
       userCheck1,
@@ -66,12 +87,12 @@ class LegalTerms extends React.Component<Props, State> {
           <Paragraph style={{ marginBottom: 20 }}>By using the Pillar Wallet I understand that:</Paragraph>
           <Checkbox
             text="Pillar does not have access to my private keys."
-            onPress={() => this.setState({ userCheck1: !userCheck1 })}
+            onPress={() => this.toggleCheckbox('userCheck1')}
           />
 
           <Checkbox
             text="The only way to recover my assets is to use the 12-word backup phrase."
-            onPress={() => this.setState({ userCheck2: !userCheck2 })}
+            onPress={() => this.toggleCheckbox('userCheck2')}
           />
 
         </Wrapper>
@@ -79,7 +100,7 @@ class LegalTerms extends React.Component<Props, State> {
 
           <Checkbox
             text="I have read, understand, and agree to the Terms of Use."
-            onPress={() => this.setState({ userCheck3: !userCheck3 })}
+            onPress={() => this.toggleCheckbox('userCheck3')}
             disabled={userCanAcceptCheck3}
           />
 
