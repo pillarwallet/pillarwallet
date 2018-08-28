e1589e07d512ce260919f8551ddf27c11a1fc4f8
Vytaute <vytaute@imas.lt>
2018-08-28 17:48:10 +0300

    Update error message

--- src/screens/SendToken/SendTokenAmount.js
+++ src/screens/SendToken/SendTokenAmount.js
@@ -46,7 +46,7 @@ const getFormStructure = (maxAmount: number, minAmount: number, enoughForFee: bo
     if (amount >= maxAmount) {
       return 'Amount should not exceed the sum of total balance and est. network fee';
     } else if (amount < minAmount) {
-      return 'Amount should be greater than 1 Wei (1e-18)';
+      return 'Amount should be greater than 1 Wei (0.000000000000000001)';
     } else if (!enoughForFee) {
       return 'Not enough ETH to process the transaction fee';
     }
