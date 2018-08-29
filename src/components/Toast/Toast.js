02756cdbc2e0d3fdbf0e7a9606463b19c0187fdc
Rostislav Chuklin <rostislavcn@gmail.com>
2018-08-29 13:17:02 +0100

    Toast component

---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,197 @@
+
+// @flow
+import * as React from 'react';
+import { Animated } from 'react-native';
+import merge from 'lodash.merge';
+import styled from 'styled-components/native';
+import IconButton from 'components/IconButton';
+import Icon from 'components/Icon';
+import { baseColors, fontSizes } from 'utils/variables';
+import { BoldText, BaseText } from 'components/Typography';
+
+type ToastOptions = {
+  autoClose?: boolean,
+  type: string,
+  message: string,
+  title: string,
+}
+
+type State = {
+  isVisible: boolean,
+  animSlide: Object,
+  toastOptions: ToastOptions,
+}
+
+const toastInitialOptions = {
+  autoClose: true,
+  type: 'info',
+  message: '',
+  title: '',
+};
+
+
+const typeColors = {
+  warning: baseColors.vividOrange,
+  info: baseColors.brightBlue,
+  success: baseColors.limeGreen,
+};
+
+const typeIcons = {
+  warning: 'warning-circle',
+  info: 'info-circle',
+  success: 'tick-circle',
+};
+
+const ToastHolder = styled.View`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TextHolder = styled.View`
+  flex: 9;
+`;
+
+const IconHolder = styled.View`
+  display: flex;
+  flex: 2;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default class Toast extends React.Component<*, State> {
+  state = {
+    isVisible: false,
+    animSlide: new Animated.Value(0),
+    toastOptions: toastInitialOptions,
+  };
+
+  static toastInstances: Object[] = [];
+
+  static show(toastOptions: ToastOptions) {
+    const toast = this.toastInstances[this.toastInstances.length - 1];
+    if (toast) {
+      toast.handleOpen(toastOptions);
+    }
+  }
+
+  static close() {
+    const toast = this.toastInstances[this.toastInstances.length - 1];
+    if (toast) {
+      toast.handleClose();
+    }
+  }
+
+  componentWillUnmount() {
+    Toast.toastInstances.splice(Toast.toastInstances.length - 1);
+  }
+
+  handleOpen = (toastOptions: ToastOptions) => {
+    if (this.state.isVisible) return;
+    const { options } = merge(
+      {},
+      { options: this.state.toastOptions },
+      { options: toastOptions },
+    );
+
+    this.setState({
+      isVisible: true,
+      toastOptions: options,
+    });
+
+    Animated.timing(
+      this.state.animSlide,
+      {
+        toValue: 1,
+        duration: 400,
+        useNativeDriver: true,
+      })
+      .start(() => {
+        if (!this.state.toastOptions.autoClose) return;
+        const timeout = setTimeout(() => {
+          this.handleClose();
+          clearTimeout(timeout);
+        }, 2000);
+      });
+  }
+
+  handleClose = () => {
+    this.setState({ isVisible: false });
+    Animated.timing(
+      this.state.animSlide,
+      {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start(() => this.setState({ toastOptions: toastInitialOptions }));
+  }
+
+  render() {
+    const { toastOptions } = this.state;
+    const animation = this.state.animSlide.interpolate({
+      inputRange: [0, 0.3, 1],
+      outputRange: [-125, -10, 0],
+    });
+    return (
+      <Animated.View
+        style={{
+          transform: [{ translateY: animation }],
+          opacity: !!this.state.toastOptions.message,
+          height: 125,
+          backgroundColor: '#fff',
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          paddingTop: 50,
+          paddingBottom: 20,
+          borderLeftWidth: 8,
+          borderStyle: 'solid',
+          borderColor: typeColors[toastOptions.type],
+          shadowColor: '#333',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 10,
+          elevation: 9,
+          zIndex: 1000,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ToastHolder>
+          <IconHolder>
+            <Icon
+              name={typeIcons[toastOptions.type]}
+              style={{
+                color: typeColors[toastOptions.type],
+                fontSize: fontSizes.extraLarge,
+              }}
+            />
+          </IconHolder>
+          <TextHolder>
+            <BoldText>{toastOptions.title}</BoldText>
+            <BaseText style={{ marginTop: 5, marginBottom: 10, color: baseColors.darkGray }}>
+              {toastOptions.message}
+            </BaseText>
+          </TextHolder>
+          <IconButton
+            onPress={this.handleClose}
+            icon="close"
+            color={baseColors.mediumGray}
+            style={{
+              flex: 3,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            iconStyle={{
+              borderWidth: 2,
+              padding: 6,
+              paddingTop: 6,
+              borderRadius: 16,
+              borderColor: baseColors.mediumGray,
+            }}
+          />
+        </ToastHolder>
+      </Animated.View>
+    );
+  }
+}
