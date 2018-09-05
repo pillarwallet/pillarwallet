b7790d964d97ec230279f24e19bf25f81ea8cd60
Jegor Sidorenko <jegor@nfm.lt>
2018-09-05 16:13:21 +0100

    Improve email validator

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -38,7 +38,8 @@ export function isValidFullname(fullName: string): boolean {
 }
 
 export function isValidEmail(email: string) {
-  const re = /\S+@\S+\.\S+/;
+  // eslint-disable-next-line
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
 
