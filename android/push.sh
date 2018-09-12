53f45b0ecf82983913f617df5d640775a25720ab
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2018-09-12 15:05:40 +0100

    tidy for pull request

--- android/push.sh
+++
@@ -1,4 +0,0 @@
-export buildNumber=1
-export GOOGLE_JSON_DATA=$(echo "$GOOGLE_JSON_BASE64_ENCODED" | base64 --decode)
-bundle exec fastlane supply init --json_key_data="$GOOGLE_JSON_DATA"
-bundle exec fastlane deploy_internal --verbose
