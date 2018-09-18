a2cd4593c9a7b9d94cf9149ff072fcd60e745275
Vytaute Vilutyte <v.vilutytei@gmail.com>
2018-09-18 16:32:15 +0300

    Feature/remove asset from list (#573)
    * Add simplified asset layout
    
    * Add remaining (M and S) assets' layouts
    
    * Adjust card view for smaller screens, update shadows
    
    * minor props cleanup
    
    * linting fix
    
    * Init swipe to remove asset for Expanded and Simplified asset lists
    
    * Update Hide asset button and disable it for ETH
    
    * Update asset list and its item styles so the 'Hide asset' button would not be covered (or be just slightly covered due to the shadow padding) on asset card slide
    
    * Add grid asset removal animation and toast for ETH
    
    * Hide 'Hide' buttons if user leaves the screen
    
    * Updating Asset screen padding so there won't be any resizing in transition animation
    
    * minor tweaks

--- src/screens/Asset/Asset.js
+++ src/screens/Asset/Asset.js
@@ -17,7 +17,7 @@ import AssetButtons from 'components/AssetButtons';
 import ActivityFeed from 'components/ActivityFeed';
 
 import Header from 'components/Header';
-import { Container, Wrapper, ScrollWrapper } from 'components/Layout';
+import { Container, ScrollWrapper } from 'components/Layout';
 import { Paragraph, BaseText } from 'components/Typography';
 import { SEND_TOKEN_FROM_ASSET_FLOW } from 'constants/navigationConstants';
 import { defaultFiatCurrency } from 'constants/assetsConstants';
@@ -55,6 +55,7 @@ type Props = {
   navigation: NavigationScreenProp<*>,
   baseFiatCurrency: ?string,
   contacts: Object,
+  resetHideRemoval: Function,
 };
 
 type State = {
@@ -70,8 +71,10 @@ type State = {
   },
 };
 
-const AssetCardWrapper = styled(Wrapper)`
+const AssetCardWrapper = styled.View`
   flex: 1;
+  padding-left: 0;
+  padding-right: ${spacing.rhythm - 2}px;
 `;
 
 const AssetDescriptionWrapper = styled.View`
@@ -115,8 +118,9 @@ class AssetScreen extends React.Component<Props, State> {
 
   componentDidMount() {
     const { fetchTransactionsHistory, wallet, navigation } = this.props;
-    const { assetData } = navigation.state.params;
+    const { assetData, resetHideRemoval } = navigation.state.params;
     fetchTransactionsHistory(wallet.address, assetData.token);
+    resetHideRemoval();
   }
 
   handleCardTap = () => {
@@ -228,6 +232,7 @@ class AssetScreen extends React.Component<Props, State> {
                 wallpaper={assetData.wallpaper}
                 isListed={isListed}
                 disclaimer={disclaimer}
+                horizontalPadding
               />
             </Transition>
             <AssetButtons
