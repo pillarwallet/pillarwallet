fc34b9482c4503ffb6e00fd84cce53b93e4c1356
Jegor Sidorenko <jegor@nfm.lt>
2018-10-01 17:37:20 +0100

    Feature/import wallet tweaks (#591)
    * Reset import wallet error when user clicks on the Back button
    
    * Disable auto-capitalization for 12 words phrase
    
    * Improve wording
    
    * Add missed change

--- src/components/RetryApiRegistration/RetryApiRegistration.js
+++ src/components/RetryApiRegistration/RetryApiRegistration.js
@@ -7,7 +7,7 @@ import { registerOnBackendAction } from 'actions/onboardingActions';
 import { Container } from 'components/Layout';
 import Button from 'components/Button';
 import Spinner from 'components/Spinner';
-import { API_REGISTRATION_FAILED, USERNAME_EXISTS, USERNAME_OK, CHECKING_USERNAME } from 'constants/walletConstants';
+import { REGISTRATION_FAILED, USERNAME_EXISTS, USERNAME_OK, CHECKING_USERNAME } from 'constants/walletConstants';
 
 type Props = {
   wallet: Object,
@@ -30,10 +30,10 @@ class RetryApiRegistration extends React.Component<Props> {
     return (
       <Container center>
         <BaseText style={{ marginBottom: 20 }}>Registering on backend</BaseText>
-        {walletState !== API_REGISTRATION_FAILED && (
+        {walletState !== REGISTRATION_FAILED && (
           <Spinner />
         )}
-        {walletState === API_REGISTRATION_FAILED && (
+        {walletState === REGISTRATION_FAILED && (
           <Button title="Try again" onPress={registerOnBackend} />
         )}
       </Container>
