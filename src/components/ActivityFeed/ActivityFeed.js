2809271e833c210fdf2f731a04b2ef1fa6f6dc00
Jegor Sidorenko <jegor@nfm.lt>
2018-10-26 12:28:34 +0100

    Update ActivityFeed.js

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -162,7 +162,7 @@ type Props = {
   additionalFiltering?: Function,
   feedTitle?: string,
   showEmptyState?: boolean,
-  onlyArrows?: boolean,
+  showArrowsOnly?: boolean,
 };
 
 type State = {
@@ -274,7 +274,7 @@ class ActivityFeed extends React.Component<Props, State> {
       navigation,
       assets,
       contacts,
-      onlyArrows,
+      showArrowsOnly,
     } = this.props;
 
     const walletAddress = wallet.address;
@@ -299,7 +299,7 @@ class ActivityFeed extends React.Component<Props, State> {
         .find(({ ethAddress }) => address.toUpperCase() === ethAddress.toUpperCase()) || {};
 
       let image;
-      if (contact && Object.keys(contact).length !== 0 && !onlyArrows) {
+      if (contact && Object.keys(contact).length !== 0 && !showArrowsOnly) {
         image = (
           <ProfileImage
             uri={contact.profileImage}
