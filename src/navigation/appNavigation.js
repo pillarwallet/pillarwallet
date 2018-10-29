2a5443dfed0c1843f7aa83a74861456ec740fdd1
Egidijus <egidijus@imas.lt>
2018-10-29 12:20:27 +0200

    Changed navbar shadow

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -288,7 +288,7 @@ const tabNavigation = createBottomTabNavigator(
         elevation: 14,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: -2 },
-        shadowOpacity: 0.0,
+        shadowOpacity: 0.05,
         shadowRadius: 2,
         borderTopColor: 'transparent',
         paddingTop: 5,
