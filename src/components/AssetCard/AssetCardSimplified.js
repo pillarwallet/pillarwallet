d87015698e8a8731a54ab2e371cdbab9f17c2b70
Vytaute Vilutyte <v.vilutytei@gmail.com>
2018-10-31 16:12:41 +0200

    Visual fix/chat list item (#709)
    * Update list item label size, update chat list item layout
    
    * Update ListItemWithImage and AssetCardSimplified labels
    
    * Change font weight for labels
    
    * Update alignment of AssetCard label

--- src/components/AssetCard/AssetCardSimplified.js
+++ src/components/AssetCard/AssetCardSimplified.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Platform, Dimensions } from 'react-native';
 import isEqual from 'lodash.isequal';
 import styled from 'styled-components/native';
-import { LightText, BoldText } from 'components/Typography';
+import { LightText, BaseText, BoldText } from 'components/Typography';
 import { Shadow } from 'components/Shadow';
 import { CachedImage } from 'react-native-cached-image';
 import { getCurrencySymbol } from 'utils/common';
@@ -124,12 +124,14 @@ const IconCircle = styled.View`
   justify-content: center;
 `;
 
-const Name = styled(BoldText)`
-  font-size: ${fontSizes.small};
-  letter-spacing: ${fontTrackings.medium};
+const Name = styled(BaseText)`
+  font-size: ${fontSizes.medium};
+  letter-spacing: ${fontTrackings.tiny};
   line-height: ${fontSizes.medium};
   color: ${baseColors.slateBlack};
   flex: 1;
+  margin-top: 2px;
+  text-align-vertical: center;
 `;
 
 const { width } = Dimensions.get('window');
