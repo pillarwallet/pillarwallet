2ccd5e133ef49ea530940946cd713a6f401fc348
Konstantin Gerasimov <konstantin@pillarproject.io>
2018-11-02 15:38:39 +0000

    Updated the Erc20TokensSearch service. Removed the temp erc20Tokens.json.

--- src/utils/__tests__/erc20TokensSearch.test.js
+++ src/utils/__tests__/erc20TokensSearch.test.js
@@ -1,10 +1,12 @@
 // @flow
-import erc20TokensSearch from 'utils/erc20TokensSearch';
+import erc20Tokens from 'testUtils/erc20TokensMock';
+import {
+  includes,
+  findList,
+} from 'utils/erc20TokensSearch';
 
 describe('ERC20TokensSearch utils', () => {
   describe('includes', () => {
-    const { includes } = erc20TokensSearch;
-
     describe('when value and/or searchValue are/is NOT empty line', () => {
       it('should return false', () => {
         expect(includes('', '')).toBe(false);
@@ -31,37 +33,40 @@ describe('ERC20TokensSearch utils', () => {
   });
 
   describe('findList', () => {
-    const { findList } = erc20TokensSearch;
-
     describe('when search value is empty string', () => {
       it('should return an empty array', () => {
-        expect(findList('')).toEqual([]);
+        expect(findList([], '')).toEqual([]);
       });
     });
 
     describe('when search value is set', () => {
       describe('and the token can NOT be found', () => {
         it('should return the empty array', () => {
-          expect(findList('lorem')).toEqual([]);
+          expect(findList([], 'lorem')).toEqual([]);
         });
       });
 
       describe('and the token(s) can be found by name or symbol', () => {
         it('should return the array with token(s)', () => {
-          expect(findList('PLR').length).toBe(1);
-          expect(findList('Pillar').length).toBe(1);
+          expect(findList(erc20Tokens, 'PLR').length).toBe(1);
+          expect(findList(erc20Tokens, 'Pillar').length).toBe(1);
         });
       });
 
       describe('and more then 20 token(s) can be found by address, name or symbol', () => {
         it('should return the array with 20 elements', () => {
-          expect(findList('a').length).toBe(20);
+          const tokens = [];
+          for (let i = 0; i < 30; i++) {
+            tokens.push(erc20Tokens[0]);
+          }
+
+          expect(findList(tokens, 'p').length).toBe(20);
         });
 
         describe('and the size is set', () => {
           it('should return the array with N elements', () => {
             const size = 3;
-            expect(findList('a', size).length).toBe(size);
+            expect(findList(erc20Tokens, 't', size).length).toBe(size);
           });
         });
       });
--- src/utils/erc20TokensSearch.js
+++ src/utils/erc20TokensSearch.js
@@ -1,21 +1,15 @@
 // @flow
-// This json file is taken from: https://github.com/dexlab-io/tokens-info-api
-import erc20Tokens from 'utils/erc20Tokens.json';
+import type { Asset } from 'models/Asset';
 
-const includes = (value: string, searchValue: string): boolean => {
+export const includes = (value: string, searchValue: string): boolean => {
   if (!value || !searchValue) return false;
   return value.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
 };
 
-const findList = (searchValue: string, size: number = 20): Array<Object> => {
-  if (!searchValue) return [];
-  return erc20Tokens.filter(token => {
-    const value = token.name + token.symbol;
-    return includes(value, searchValue);
-  }).slice(0, size);
-};
 
-export default {
-  includes,
-  findList,
+export const findList = (erc20Tokens: Array<Asset>, searchValue: string, size: number = 20): Array<Asset> => {
+  if (!searchValue) return [];
+  return erc20Tokens.filter(({ name, symbol }) => {
+    return includes(name, searchValue) || includes(symbol, searchValue);
+  }).slice(0, size);
 };
