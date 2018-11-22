1da35e9d9fb1a38bdf5b8092045414ccc536b36d
Jegor Sidorenko <jegor@nfm.lt>
2018-11-22 14:53:08 +0000

    Update ListItemWithImage.js

--- src/components/ListItem/ListItemWithImage.js
+++ src/components/ListItem/ListItemWithImage.js
@@ -78,8 +78,7 @@ const Column = styled.View`
   align-items: ${props => props.rightColumn ? 'flex-end' : 'flex-start'};
   justify-content: ${props => props.type === CHAT_ITEM ? 'flex-start' : 'center'};
   margin-top: ${props => props.type === CHAT_ITEM ? '-2px' : 0};
-  ${props => props.rightColumn ? 'padding-left: 10px' : 'flex: 1'}
-  
+  ${props => props.rightColumn ? 'padding-left: 10px;' : 'flex: 1;'}
 `;
 
 const ItemTitle = styled(BoldText)`
