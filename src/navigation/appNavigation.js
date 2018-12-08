c88e4007f157d86d42b172a141bb3808e7b93a5c
diablo <jaime@diablourbano.com>
2018-12-07 22:34:22 -0300

    add eslint exception to toast message (1 char)

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -449,7 +449,7 @@ class AppFlow extends React.Component<Props, {}> {
 
     if (!isWalletBackedUp) {
       Toast.show({
-        message: 'Please go to settings to complete wallet backup. Pillar cannot help you retrieve your wallet if lost.',
+        message: 'Please go to settings to complete wallet backup. Pillar cannot help you retrieve your wallet if lost.', // eslint-disable-line max-len
         type: 'warning',
         title: 'WARNING - Your funds are currently at risk.',
         autoClose: false,
