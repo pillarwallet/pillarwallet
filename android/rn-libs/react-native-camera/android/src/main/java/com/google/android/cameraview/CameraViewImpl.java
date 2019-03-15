5dcb51cb69f1f15b6f89163a9467634dbf8fa1ba
Deimantas Špučys <as.deimis@gmail.com>
2019-03-15 16:22:07 +0200

    react-native-camera android updates for lib version 1.1.4

--- android/rn-libs/react-native-camera/android/src/main/java/com/google/android/cameraview/CameraViewImpl.java
+++ android/rn-libs/react-native-camera/android/src/main/java/com/google/android/cameraview/CameraViewImpl.java
@@ -20,7 +20,10 @@ import android.media.CamcorderProfile;
 import android.view.View;
 import android.graphics.SurfaceTexture;
 
+import com.facebook.react.bridge.ReadableMap;
+
 import java.util.Set;
+import java.util.SortedSet;
 
 abstract class CameraViewImpl {
 
@@ -51,6 +54,12 @@ abstract class CameraViewImpl {
     abstract int getFacing();
 
     abstract Set<AspectRatio> getSupportedAspectRatios();
+    
+    abstract SortedSet<Size> getAvailablePictureSizes(AspectRatio ratio);
+    
+    abstract void setPictureSize(Size size);
+    
+    abstract Size getPictureSize();
 
     /**
      * @return {@code true} if the aspect ratio was changed.
@@ -67,14 +76,18 @@ abstract class CameraViewImpl {
 
     abstract int getFlash();
 
-    abstract void takePicture();
+    abstract void takePicture(ReadableMap options);
 
     abstract boolean record(String path, int maxDuration, int maxFileSize,
-                            boolean recordAudio, CamcorderProfile profile);
+                            boolean recordAudio, CamcorderProfile profile, int orientation);
 
     abstract void stopRecording();
 
+    abstract int getCameraOrientation();
+
     abstract void setDisplayOrientation(int displayOrientation);
+
+    abstract void setDeviceOrientation(int deviceOrientation);
 
     abstract void setFocusDepth(float value);
 
@@ -91,6 +104,10 @@ abstract class CameraViewImpl {
     abstract void setScanning(boolean isScanning);
 
     abstract boolean getScanning();
+    
+    abstract public void resumePreview();
+    
+    abstract public void pausePreview();
 
     abstract public void setPreviewTexture(SurfaceTexture surfaceTexture);
 
@@ -102,9 +119,9 @@ abstract class CameraViewImpl {
 
         void onCameraClosed();
 
-        void onPictureTaken(byte[] data);
+        void onPictureTaken(byte[] data, int deviceOrientation);
 
-        void onVideoRecorded(String path);
+        void onVideoRecorded(String path, int videoOrientation, int deviceOrientation);
 
         void onFramePreview(byte[] data, int width, int height, int orientation);
 
