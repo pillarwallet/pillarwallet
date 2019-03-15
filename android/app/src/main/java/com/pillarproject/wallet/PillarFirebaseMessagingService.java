72947dc2d3d3cab4be9cb36e5945d58e74140493
Deimantas Špučys <as.deimis@gmail.com>
2019-03-15 15:27:57 +0200

    removed unused method in custom PN handler

--- android/app/src/main/java/com/pillarproject/wallet/PillarFirebaseMessagingService.java
+++ android/app/src/main/java/com/pillarproject/wallet/PillarFirebaseMessagingService.java
@@ -1,7 +1,5 @@
 package com.pillarproject.wallet;
 
-import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.firebase.messaging.RemoteMessage;
@@ -9,24 +7,15 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 
 import io.intercom.android.sdk.push.IntercomPushClient;
-import io.invertase.firebase.Utils;
-import io.invertase.firebase.messaging.RNFirebaseBackgroundMessagingService;
 import io.invertase.firebase.messaging.RNFirebaseMessagingService;
 
 public class PillarFirebaseMessagingService extends RNFirebaseMessagingService {
-    private static final String TAG = "PillarFirebaseMessaging";
     private final IntercomPushClient intercomPushClient = new IntercomPushClient();
-    
-    @Override public void onNewToken(String refreshedToken) {
-        intercomPushClient.sendTokenToIntercom(getApplication(), refreshedToken);
-    }
-
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Map message = remoteMessage.getData();
         if (intercomPushClient.isIntercomPush(message)) {
-            Log.d(TAG, "onMessageReceived intercom message");
             intercomPushClient.handlePush(getApplication(), message);
         } else {
             super.onMessageReceived(remoteMessage);
