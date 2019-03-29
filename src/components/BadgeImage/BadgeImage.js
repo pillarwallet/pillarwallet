97cf4193fb8a6076a6d64db632a2ba3c460d9996
Jegor Sidorenko <jegor@nfm.lt>
2019-03-29 09:26:29 +0100

    Add size option for badge image

--- src/components/BadgeImage/BadgeImage.js
+++ src/components/BadgeImage/BadgeImage.js
@@ -5,19 +5,21 @@ import type { Badge } from 'models/Badge';
 
 type Props = {
   data: Badge,
+  size?: number,
 };
 
 const defaultBadge = require('assets/images/defaultBadge.png');
 
-const BadgeImage = ({ data: badge }: Props) => {
+const BadgeImage = ({ data: badge, size = 96 }: Props) => {
+  size = parseInt(size, 10);
   const badgeUri = badge.imageUrl ? `${badge.imageUrl}?t=${badge.updatedAt || 0}` : '';
   return (
     <CachedImage
       useQueryParamsInCacheKey
       key={badgeUri}
       style={{
-        height: 96,
-        width: 96,
+        height: size,
+        width: size,
       }}
       source={{ uri: badgeUri }}
       resizeMode="contain"
