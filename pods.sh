4ea3e2e94bfd5e1d81f11a0d750050824c784a6d
Antonio Nedanoski <antonio@tarmac.io>
2019-04-04 21:12:28 +0200

    Change cocoapods version

--- pods.sh
+++ pods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR
-#Y | sudo gem uninstall cocoapods
-#sudo gem install cocoapods -v 1.5.3
+Y | sudo gem uninstall cocoapods
+sudo gem install cocoapods -v 1.5.3
 cd ios && pod install --verbose
