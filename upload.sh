21dcf6aa9284cc33d85015f1bf79b71926b0d2e9
Antonio Nedanoski <antonio@tarmac.io>
2019-04-04 19:34:04 +0200

    Travis CI reconfig

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export buildNumber=$(cat ~/pillarwallet/buildNumber.txt)
-export APP_BUILD_NUMBER="$(cat /tmp/workspace/build-num/app_build_number.txt)"
+export APP_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
 cd ios && bundle exec fastlane deploy_staging APP_BUILD_NUMBER:$APP_BUILD_NUMBER build_number:$buildNumber APP_NAME:"Pillar Staging"
