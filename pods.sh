4a0d9926ef8440d505c406f6ae1e2160d1a78718
Antonio Nedanoski <antonio@tarmac.io>
2019-04-04 21:33:41 +0200

    Pipe yes answer to uninstall cocoapods command

--- pods.sh
+++ pods.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR
-Y | sudo gem uninstall cocoapods
+pod --version
+yes | sudo gem uninstall cocoapods
 sudo gem install cocoapods -v 1.5.3
 cd ios && pod install --verbose
