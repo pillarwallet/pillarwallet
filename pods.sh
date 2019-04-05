56d32c924a724c03ff74a071a05d1d65b7159572
Antonio Nedanoski <antonio@tarmac.io>
2019-04-05 10:48:56 +0200

    Remove cocoapods w/o sudo

--- pods.sh
+++ pods.sh
@@ -2,6 +2,6 @@
 
 cd $TRAVIS_BUILD_DIR
 pod --version
-sudo gem uninstall cocoapods -v 1.6.1
-sudo gem install cocoapods -v 1.5.3
+gem uninstall cocoapods
+gem install cocoapods -v 1.5.3
 cd ios && pod install --verbose
