2e24ba5cfc114b14979c1200534e4176c3623061
diablo <jaime@diablourbano.com>
2019-04-16 01:11:24 -0300

    trying jestSetup config process.env

--- src/testUtils/jestSetup.js
+++ src/testUtils/jestSetup.js
@@ -25,17 +25,9 @@ import { utils } from 'ethers';
 import StorageMock from './asyncStorageMock';
 import FirebaseMock from './firebaseMock';
 
-if (typeof process === 'undefined') {
-  global.process = require('process')
-} else {
-  const bProcess = require('process')
-  for (var p in bProcess) {
-    if (!(p in process)) {
-      process[p] = bProcess[p]
-    }
-  }
-}
-process.env['IS_TEST'] = 'TEST'; // eslint-disable-line dot-notation
+const testEnv = process.env || {};
+testEnv['IS_TEST'] = 'TEST'; // eslint-disable-line dot-notation
+process.env = testEnv;
 
 jest.mock('NativeAnimatedHelper');
 
