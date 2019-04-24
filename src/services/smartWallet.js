1e07dc87efc809f2da8726916faad9cb7e82bc51
Deimantas Špučys <as.deimis@gmail.com>
2019-04-24 16:01:01 +0300

    added wip: smart wallet sdk

---
+++ src/services/smartWallet.js
@@ -0,0 +1,56 @@
+// @flow
+/*
+    Pillar Wallet: the personal data locker
+    Copyright (C) 2019 Stiftung Pillar Project
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+import {
+  availableEnvironments,
+  IEnvironment,
+  ISdk,
+  createSdk,
+} from '@archanova/wallet-sdk';
+
+
+export default class SmartWallet {
+  sdk: ISdk;
+
+  constructor() {
+    const config: IEnvironment = availableEnvironments.staging;
+    try {
+      this.sdk = createSdk(config);
+    } catch (err) {
+      this.handleError(err);
+    }
+  }
+
+  async init() {
+    return this.sdk
+      .initialize()
+      .catch(this.handleError);
+  }
+
+  async createAccount() {
+    return this.sdk
+      .createAccount(null)
+      .then(account => console.log('SmartWallet account: ', account))
+      .catch(this.handleError);
+  }
+
+  handleError(error: any) {
+    console.log('SmartWallet handleError: ', error);
+  }
+}
