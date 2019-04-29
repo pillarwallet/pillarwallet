89cba19b6759c5c206e3001bed2b440bc6d4d825
Vytaute Vilutyte <v.vilutytei@gmail.com>
2019-04-29 11:59:56 +0300

    Add upgrade to Smart wallet information screen

--- src/screens/UpgradeToSmartWallet/UpgradeInfoScreen/UpgradeInfoScreen.js
+++ src/screens/UpgradeToSmartWallet/UpgradeInfoScreen/UpgradeInfoScreen.js
@@ -19,8 +19,13 @@
 */
 import * as React from 'react';
 import type { NavigationScreenProp } from 'react-navigation';
-import { Container } from 'components/Layout';
+import styled from 'styled-components/native';
+import Emoji from 'react-native-emoji';
+import { Container, Wrapper } from 'components/Layout';
 import Header from 'components/Header';
+import Button from 'components/Button';
+import { Paragraph, BoldText, BaseText } from 'components/Typography';
+import { baseColors, fontSizes, spacing } from 'utils/variables';
 
 type Props = {
   changePin: (newPin: string, currentPin: string) => Function,
@@ -33,16 +38,107 @@ type State = {
   pinError: string,
 };
 
+const WhiteWrapper = styled.View`
+  background-color: ${baseColors.white};
+  padding-bottom: 50px;
+`;
+
+const ExplanationRow = styled.View`
+  margin-top: 26px;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 100%;
+`;
+
+const TextWrapper = styled.View`
+  flex-direction: column;
+  flex: 1;
+`;
+
+const ExplanationTitle = styled(BoldText)`
+  font-size: ${fontSizes.medium}px;
+  color: ${baseColors.midnight};
+`;
+
+const BodyWrapper = styled(BaseText)`
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const ExplanationBody = styled(BaseText)`
+  font-size: ${fontSizes.small}px;
+  color: ${baseColors.midnight};
+  flex: 1;
+`;
+
+const ExplanationEmoji = styled(Emoji)`
+  font-size: 40px;
+  margin-right: 24px;
+  margin-left: 10px;
+  color: #000000;
+`;
+
+const Footer = styled.View`
+  background-color: ${baseColors.snowWhite};
+  border-top-width: 1px;
+  border-top-color: #ededed;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: ${spacing.large}px ${spacing.mediumLarge}px;
+  flex: 1;
+`;
+
 class UpgradeInfoScreen extends React.PureComponent<Props, State> {
   render() {
     const { navigation } = this.props;
     return (
       <Container>
-        <Header
-          title="what's next"
-          centerTitle
-          onBack={() => navigation.goBack(null)}
-        />
+        <WhiteWrapper>
+          <Header
+            title="what's next"
+            centerTitle
+            onBack={() => navigation.goBack(null)}
+          />
+          <Wrapper regularPadding>
+            <Paragraph small>
+              On the following screens you’ll have to choose your recovery agents and funds to transfer to your new
+              Smart Wallet.
+            </Paragraph>
+            <ExplanationRow>
+              <ExplanationEmoji name="raising_hand" />
+              <TextWrapper>
+                <ExplanationTitle>
+                  What’s recovery agents?
+                </ExplanationTitle>
+                <BodyWrapper>
+                  <ExplanationBody>
+                    It’s one or many of your contacts who posess a part of your key for restoring access to your
+                    Smart Wallet, etc.
+                  </ExplanationBody>
+                </BodyWrapper>
+              </TextWrapper>
+            </ExplanationRow>
+            <ExplanationRow>
+              <ExplanationEmoji name="rocket" />
+              <TextWrapper>
+                <ExplanationTitle>
+                  Why transfering funds?
+                </ExplanationTitle>
+                <BodyWrapper>
+                  <ExplanationBody>
+                    It’s more safe and it’s more convenient to store your funds in a contract rather than rely on
+                    a private key only, etc.
+                  </ExplanationBody>
+                </BodyWrapper>
+              </TextWrapper>
+            </ExplanationRow>
+          </Wrapper>
+        </WhiteWrapper>
+        <Footer>
+          <Button small title="Next" onPress={() => {}} />
+        </Footer>
       </Container>
     );
   }
