6a84043401b899ad45ab0077b6fbde0423b6d4f2
Jegor Sidorenko <jegor@nfm.lt>
2019-05-17 16:55:29 +0200

    Add own account to the contacts list

--- src/models/Account.js
+++ src/models/Account.js
@@ -3,9 +3,11 @@ import { ACCOUNT_TYPES } from '../constants/accountsConstants';
 
 export type AccountExtra = Object;
 
+export type AccountTypes = $Keys<typeof ACCOUNT_TYPES>;
+
 export type Account = {
   id: string,
-  type: $Keys<typeof ACCOUNT_TYPES>,
+  type: AccountTypes,
   extra?: AccountExtra,
   isActive: boolean,
 };
