5b13ef34bc8ab4536aee06247c3383359d5c5736
Jegor Sidorenko <jegor@nfm.lt>
2019-06-26 11:15:02 +0200

    Update badgesReducer.js

--- src/reducers/badgesReducer.js
+++ src/reducers/badgesReducer.js
@@ -1,4 +1,24 @@
 // @flow
+/*
+    Pillar Wallet: the personal data locker
+    Copyright (C) 2019 Stiftung Pillar Project
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+import merge from 'lodash.merge';
 import {
   UPDATE_BADGES,
   ADD_CONTACT_BADGES,
@@ -7,7 +27,6 @@ import {
   STOP_FETCHING_CONTACTS_BADGES,
 } from 'constants/badgesConstants';
 import type { Badges } from 'models/Badge';
-import merge from 'lodash.merge';
 
 export type BadgesReducerState = {
   data: Badges,
