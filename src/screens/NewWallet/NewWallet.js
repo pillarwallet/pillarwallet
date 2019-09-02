2ddf0825df7ef71928af5f9f3f279427e230fbf7
Vytaute Vilutyte <v.vilutytei@gmail.com>
2019-09-02 10:16:52 +0300

    Reuse loader component

--- src/screens/NewWallet/NewWallet.js
+++ src/screens/NewWallet/NewWallet.js
@@ -20,15 +20,13 @@
 import * as React from 'react';
 import type { NavigationScreenProp } from 'react-navigation';
 import { connect } from 'react-redux';
-import styled from 'styled-components/native';
 
 import { Container, Wrapper } from 'components/Layout';
-import { BaseText, BoldText } from 'components/Typography';
-import Spinner from 'components/Spinner';
+import { BaseText } from 'components/Typography';
+import Loader from 'components/Loader';
 import Button from 'components/Button';
 import { REGISTRATION_FAILED, USERNAME_EXISTS } from 'constants/walletConstants';
 import { APP_FLOW } from 'constants/navigationConstants';
-import { baseColors, fontSizes } from 'utils/variables';
 
 const API_FAILURES = [USERNAME_EXISTS, REGISTRATION_FAILED];
 
@@ -37,50 +35,10 @@ type Props = {
   wallet: Object,
 };
 
-type State = {
-  visibleMessageId: number,
-}
-
-const messages = [
-  'Yes, it takes time to load.',
-  'There are many things going on.',
-  'Decentralization isn’t easy.',
-];
-
-const MessageText = styled(BoldText)`
-  font-size: ${fontSizes.extraLarge}px;
-  line-height: 40px;
-  color: ${baseColors.slateBlack};
-  margin-top: 42px;
-  margin-right: 34px;
-`;
-
-class NewWallet extends React.Component<Props, State> {
-  timer: ?IntervalID;
-  state = {
-    visibleMessageId: 0,
-  };
-
-  componentDidMount() {
-    this.timer = setInterval(() => this.changeMessages(), 6000);
-  }
-
-  componentWillUnmount() {
-    if (this.timer) clearInterval(this.timer);
-  }
-
-  changeMessages = () => {
-    const { visibleMessageId } = this.state;
-    const lastMessageId = messages.length - 1;
-    const nextMessageId = visibleMessageId === lastMessageId ? 0 : visibleMessageId + 1;
-    this.setState({ visibleMessageId: nextMessageId });
-  };
-
+class NewWallet extends React.PureComponent<Props> {
   render() {
     const { wallet, navigation } = this.props;
-    const { visibleMessageId } = this.state;
     const { walletState } = wallet;
-    const message = messages[visibleMessageId] || '';
 
     const tryToReRegister = () => {
       navigation.navigate(APP_FLOW);
@@ -91,10 +49,7 @@ class NewWallet extends React.Component<Props, State> {
     return (
       <Container center={failedToRegister}>
         {!failedToRegister && (
-          <Wrapper fullscreen style={{ justifyContent: 'center', padding: 56, flex: 1 }}>
-            <Spinner />
-            <MessageText>{message}</MessageText>
-          </Wrapper>
+          <Loader />
         )}
         {failedToRegister && (
           <Wrapper fullScreen center flex={1}>
