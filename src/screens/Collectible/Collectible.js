db0cc5bae32ec2148740f49ce531e6872a669e41
Gonzalo Arreche <gonzaloarreche@gmail.com>
2019-10-21 10:57:50 -0300

    Fix collectible image closing on iPhone X (#1475)
    Fix collectible image closing on iPhone X
    
    Close icon on the iPhone X was not clickeable, this adds
    a margin to fix the issue.

--- src/screens/Collectible/Collectible.js
+++ src/screens/Collectible/Collectible.js
@@ -38,6 +38,7 @@ import { ScrollWrapper, Wrapper } from 'components/Layout';
 import { Paragraph } from 'components/Typography';
 import CircleButton from 'components/CircleButton';
 
+import { isIphoneX } from 'utils/common';
 import { baseColors, fontSizes, spacing } from 'utils/variables';
 import { mapOpenSeaAndBCXTransactionsHistory, mapTransactionsHistory } from 'utils/feedData';
 
@@ -98,6 +99,8 @@ const CollectibleImage = styled(CachedImage)`
 `;
 
 const IconWrapper = styled.View`
+  ${isIphoneX() && 'margin-top: 30px;'}
+  margin-right: 6px;
   padding: 6px;
   flex-direction: row;
   align-items: center;
