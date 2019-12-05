06a32ac9b978f6fd3c9c472bc440d308f2ff3cdf
Deimantas Špučys <as.deimis@gmail.com>
2019-12-05 11:42:28 +0200

    added ios and android missing pieces for successful builds

--- ios/pillarwallet/AppDelegate.m
+++ ios/pillarwallet/AppDelegate.m
@@ -1,5 +1,5 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
-#import "RNBranch.h"
+#import "RNBranch/RNBranch.h"
 #import "Firebase.h"
 #import "AppDelegate.h"
 #import "RNFirebaseNotifications.h"
