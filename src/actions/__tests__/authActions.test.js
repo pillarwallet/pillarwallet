a1fe807e1a41626885bf1ff7ea8d1f4d6c4edaa1
Deimantas Špučys <as.deimis@gmail.com>
2019-12-11 23:27:58 +0200

    test fix

--- src/actions/__tests__/authActions.test.js
+++ src/actions/__tests__/authActions.test.js
@@ -112,10 +112,10 @@ describe('Auth actions', () => {
       { type: UPDATE_WALLET_STATE, payload: DECRYPTING },
       { type: SET_FEATURE_FLAGS, payload: INITIAL_FEATURE_FLAGS },
       { type: UPDATE_APP_SETTINGS, payload: { firebaseAnalyticsConnectionEnabled: false } },
-      { type: UPDATE_SESSION, payload: { fcmToken: '12x2342x212' } },
       { type: UPDATE_USER, payload: { user: registeredMockUser, state: REGISTERED } },
-      { type: DECRYPT_WALLET, payload: mockWallet },
+      { type: DECRYPT_WALLET, payload: { ...mockWallet, privateKey: undefined } },
       { type: UPDATE_PIN_ATTEMPTS, payload: { lastPinAttempt: 0, pinAttemptsCount: 0 } },
+      { type: UPDATE_SESSION, payload: { fcmToken: '12x2342x212' } },
     ];
 
     const pin = '123456';
