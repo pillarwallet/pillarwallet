691167c382d7687a3d556ee33b327faffc21f37c
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-01-13 09:34:16 +0200

    Optimise style for HideButtonLabel

--- src/screens/Assets/HideAssetButton.js
+++ src/screens/Assets/HideAssetButton.js
@@ -40,9 +40,11 @@ const HideButtonWrapper = styled.TouchableOpacity`
 
 const HideButtonLabel = styled(BaseText)`
   color: ${themedColors.negative};
-  font-size: ${props => props.expanded ? fontSizes.regular : fontSizes.small}px;
+  ${({ expanded, disabled }) => `
+    font-size: ${expanded ? fontSizes.regular : fontSizes.small}px;
+    opacity: ${disabled ? 0.5 : 1}
+  `}
   margin-top: 8px;
-  opacity: ${props => props.disabled ? 0.5 : 1}
 `;
 
 const TurnOffIcon = styled(Icon)`
