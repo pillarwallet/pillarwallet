c5eed7e2429afd83f138ff2041c90d35e6d2dedc
Angelika Serwa <angelika.serwa@swmansion.com>
2020-02-11 15:48:29 +0100

    Add interaction testing

--- storybook/stories/Button/index.js
+++ storybook/stories/Button/index.js
@@ -3,8 +3,8 @@ import * as React from 'react';
 import { TouchableOpacity } from 'react-native';
 
 type Props = {
-  onPress: () => void,
-  children: React.Node,
+  onPress?: () => void,
+  children?: React.Node,
 }
 
 export default function Button({ onPress, children }: Props) {
