0e9d8560247737d6e2dfd61e2757cc774fd2b09a
alex <aleksandar.lazarevski@tarmac.io>
2020-03-06 18:30:01 +0100

    Removed unused var

--- src/utils/keyPairGenerator.js
+++ src/utils/keyPairGenerator.js
@@ -17,7 +17,6 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-import { Platform } from 'react-native';
 import { utils } from 'ethers';
 import { Thread } from 'react-native-threads';
 import { PRE_KEY_THRESHOLD } from 'configs/connectionKeysConfig';
