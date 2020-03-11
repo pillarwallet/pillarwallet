2d6b488d1bdab6bb9c64bb158c4a35053f81ad81
Deimantas Špučys <as.deimis@gmail.com>
2020-03-11 09:00:31 -0700

    rn 0.61.5

--- android/app/src/main/java/com/pillarproject/wallet/MainApplication.java
+++ android/app/src/main/java/com/pillarproject/wallet/MainApplication.java
@@ -1,5 +1,6 @@
 package com.pillarproject.wallet;
 
+import android.content.Context;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -8,7 +9,6 @@ import com.crypho.scrypt.RNScryptPackage;
 import com.facebook.react.PackageList;
 import androidx.multidex.MultiDexApplication;
 import com.facebook.react.ReactApplication;
-
 import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 import cl.json.RNSharePackage;
 import io.expo.appearance.RNCAppearancePackage;
@@ -18,7 +18,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.wix.reactnativenotifications.RNNotificationsPackage;
-
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
@@ -61,8 +61,34 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
       Intercom.initialize(this, "android_sdk-b989462efb366f8046f5ca1a12c75d67ecb7592c", "s70dqvb2");
     }
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
     // TODO: remove this call?
 //    long storageSizeMax = 60L * 1024L * 1024L; // 60 MB
 //    com.facebook.react.modules.storage.ReactDatabaseSupplier.getInstance(getApplicationContext()).setMaximumSize(storageSizeMax);
   }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
 }
