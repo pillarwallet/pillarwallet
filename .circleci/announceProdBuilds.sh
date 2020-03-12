514059f528a5cf272293fcd946f81176ebb20876
Antonio Nedanoski <antonio@tarmac.io>
2020-03-12 11:53:49 +0100

    Small change to prod builds script

--- .circleci/announceProdBuilds.sh
+++ .circleci/announceProdBuilds.sh
@@ -14,9 +14,9 @@ cat <<EOM
             "fallback": "<!here> - $applicationName deployed to $targetEnvironment.",
             "color": "#33CC66",
             "pretext": "<!here> - $applicationName deployed to $targetEnvironment.",
-            "title": "$CIRCLE_PROJECT_REPONAME",
+            "title": "build number: $appBuildNumber",
             "title_link": "https://circleci.com/workflow-run/$CIRCLE_WORKFLOW_WORKSPACE_ID",
-            "text": "build number: $appBuildNumber\n \n $pr_description",
+            "text": "What's new: \n \n $pr_description",
             "ts": $(date '+%s')
         }
     ]
