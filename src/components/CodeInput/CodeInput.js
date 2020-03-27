2cac9818e4bbeeca7724197af1ae0fbfc7ddd982
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-03-27 11:38:33 +0200

    Add CodeInput component

---
+++ src/components/CodeInput/CodeInput.js
@@ -0,0 +1,200 @@
+// @flow
+/*
+    Pillar Wallet: the personal data locker
+    Copyright (C) 2019 Stiftung Pillar Project
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+import React from 'react';
+import { TouchableWithoutFeedback } from 'react-native';
+import styled from 'styled-components/native';
+
+import { MediumText } from 'components/Typography';
+
+import { themedColors } from 'utils/themes';
+import { fontStyles } from 'utils/variables';
+
+import type { Event } from 'react-native';
+
+
+type Position = {
+  x: number,
+  y: number,
+};
+
+type Props = {
+  codeLength: number,
+  inputProps?: Object,
+};
+
+type State = {
+  value: string,
+  focused: boolean,
+  inputPositions: Position[],
+  focusLastOne: boolean,
+};
+
+
+const INPUT_SIDE = 56;
+
+const Wrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Row = styled.View`
+  position: relative;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FakeInput = styled.View`
+  width: ${INPUT_SIDE}px;
+  height: ${INPUT_SIDE}px;
+  border-radius: 4px;
+  margin: 7px;
+  background-color: ${themedColors.tertiary};
+  align-items: center;
+  justify-content: center;
+  border-width: 2px;
+  border-color: ${({ focus }) => focus ? themedColors.primary : themedColors.tertiary};
+`;
+
+const InputValue = styled(MediumText)`
+  color: ${themedColors.text};
+  ${fontStyles.giant};
+`;
+
+const StyledTextInput = styled.TextInput`
+  position: absolute;
+  color: ${themedColors.text};
+  ${fontStyles.giant};
+  text-align: center;
+  width: ${INPUT_SIDE}px;
+  height: ${INPUT_SIDE}px;
+  padding: 0;
+`;
+
+
+export default class CodeInput extends React.Component<Props, State> {
+  static defaultProps = {
+    codeLength: 1,
+  };
+
+  input = React.createRef();
+
+  state = {
+    value: '',
+    focused: false,
+    inputPositions: [],
+    focusLastOne: false,
+  };
+
+  handleClick = () => {
+    const { current } = this.input;
+    if (!current) return;
+    current.focus();
+  };
+
+  onFocus = () => {
+    const { codeLength } = this.props;
+    const { value } = this.state;
+    this.setState({ focused: true, focusLastOne: codeLength === value.length });
+  };
+
+  onBlur = () => {
+    this.setState({ focused: false });
+  };
+
+  onKeyPress = (e: Event) => {
+    const { value: currentValue } = this.state;
+    if (e.nativeEvent.key === 'Backspace') {
+      this.setState({ value: currentValue.slice(0, currentValue.length - 1), focusLastOne: false });
+    }
+  };
+
+  onChange = (value: string) => {
+    const { codeLength } = this.props;
+    const { value: currentValue } = this.state;
+    if (currentValue.length < codeLength) {
+      this.setState({ value: (currentValue + value).slice(0, codeLength) });
+    }
+  };
+
+  setInputPosition = (pos: Position) => {
+    const { inputPositions } = this.state;
+    const updatedInputPositions = [...inputPositions, pos];
+    this.setState({ inputPositions: updatedInputPositions });
+  };
+
+  render() {
+    const {
+      value,
+      focused,
+      inputPositions,
+      focusLastOne,
+    } = this.state;
+    const { codeLength, inputProps = {} } = this.props;
+    const codeArray = new Array(codeLength).fill(0);
+    const values = value.replace(/\s/g, '').split('');
+
+    const selectedIndex = values.length < codeLength ? values.length : codeLength - 1;
+    const hideInput = !(values.length < codeLength);
+    const { x, y } = inputPositions[selectedIndex] || {};
+
+    return (
+      <Wrapper>
+        <TouchableWithoutFeedback onPress={this.handleClick}>
+          <Row>
+            {codeArray.map((val, index) => {
+              const selected = !focusLastOne ? values.length === index : values.length - 1 === index;
+              return (
+                <FakeInput
+                  onLayout={({ nativeEvent }) => {
+                    this.setInputPosition({
+                      x: nativeEvent.layout.x,
+                      y: nativeEvent.layout.y,
+                    });
+                  }}
+                  focus={focused && selected}
+                >
+                  <InputValue>{values[index] || ''}</InputValue>
+                </FakeInput>
+              );
+            })}
+            <StyledTextInput
+              {...inputProps}
+              value=""
+              innerRef={this.input}
+              onChangeText={this.onChange}
+              onKeyPress={this.onKeyPress}
+              onFocus={this.onFocus}
+              onBlur={this.onBlur}
+              style={{
+                left: x,
+                top: y,
+                opacity: hideInput ? 0 : 1,
+              }}
+            />
+          </Row>
+        </TouchableWithoutFeedback>
+      </Wrapper>
+    );
+  }
+}
