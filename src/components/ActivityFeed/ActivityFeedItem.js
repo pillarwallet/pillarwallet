ff5a231691062d8527ee25eac0b243b61f63500d
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-04-28 10:08:08 +0300

    Adjsut LitstItemWithImage secondary button (#1868)

--- src/components/ActivityFeed/ActivityFeedItem.js
+++ src/components/ActivityFeed/ActivityFeedItem.js
@@ -136,6 +136,8 @@ type EventData = {
   iconBackgroundColor?: string,
   iconBorder?: boolean,
   fallbackToGenericToken?: boolean,
+  secondaryButton?: boolean,
+  buttonActionLabel?: string,
 };
 
 const NAMES = {
@@ -469,8 +471,6 @@ export class ActivityFeedItem extends React.Component<Props> {
     let actionLabel;
     if (type === TYPE_ACCEPTED) {
       actionLabel = STATUSES.CONNECTED;
-    } else if (type === TYPE_SENT) {
-      actionLabel = STATUSES.REQUESTED;
     } else {
       actionLabel = null;
     }
@@ -479,8 +479,12 @@ export class ActivityFeedItem extends React.Component<Props> {
       label: username,
       actionLabel,
       avatarUrl: profileImage,
-      labelAsButton: type === TYPE_SENT,
     };
+
+    if (type === TYPE_SENT) {
+      data.buttonActionLabel = STATUSES.REQUESTED;
+      data.secondaryButton = true;
+    }
 
     if (type === TYPE_RECEIVED) {
       data.subtext = 'Connection request';
