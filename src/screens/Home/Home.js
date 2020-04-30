467ab92979cf68ea8c77351c96e56066bfe6752a
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-04-30 18:27:34 +0300

    Fix/ppn settle (#1902)
    * Disable Settle transaction button in Smart Wallet is not deployed

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -404,7 +404,7 @@ class HomeScreen extends React.Component<Props, State> {
                 ),
               },
             ],
-            sideFlex: '20px',
+            sideFlex: '25px',
           }}
           inset={{ bottom: 0 }}
           tab
