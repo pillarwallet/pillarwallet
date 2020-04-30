6e9811d6564c29259f8c46d6a13e68f72de579ba
Jegor Sidorenko <jegor@nfm.lt>
2020-04-30 17:04:03 +0200

    Fix relayer's detection in Exchange

--- src/services/smartWallet.js
+++ src/services/smartWallet.js
@@ -97,6 +97,7 @@ type ParsedEstimate = {
   totalCost: ?BigNumber,
   gasToken: ?sdkInterfaces.IGasToken,
   gasTokenCost: ?BigNumber,
+  relayerFeatures: ?sdkInterfaces.RelayerFeatures;
 };
 
 let subscribedToEvents = false;
@@ -106,12 +107,14 @@ export const parseEstimatePayload = (estimatePayload: EstimatePayload): ParsedEs
   const gasToken = get(estimatePayload, 'gasToken');
   const gasPrice = get(estimatePayload, 'signedGasPrice.gasPrice');
   const gasTokenCost = get(estimatePayload, 'signedGasTokenCost.gasTokenCost');
+  const relayerFeatures = get(estimatePayload, 'relayerFeatures');
   return {
     gasAmount,
     gasPrice,
     totalCost: gasAmount && gasPrice && gasPrice.mul(gasAmount),
     gasToken,
     gasTokenCost,
+    relayerFeatures,
   };
 };
 
@@ -132,9 +135,10 @@ const calculateEstimate = (
     : defaultGasAmount,
   );
 
+  const hasGasTokenSupport = get(parsedPayload, 'relayerFeatures.gasTokenSupported', false);
   const parsedGasTokenAddress = get(parsedPayload, 'gasToken.address');
   const gasTokenAddress = get(gasToken, 'address');
-  const isGasTokenAvailable = !isEmpty(gasToken)
+  const isGasTokenAvailable = hasGasTokenSupport && !isEmpty(gasToken)
     && addressesEqual(parsedGasTokenAddress, gasTokenAddress);
 
   gasTokenCost = new BigNumber(isGasTokenAvailable && gasTokenCost
