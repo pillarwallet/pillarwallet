d18d8d6a4c122840a723294900f7aac8bb045f43
Jegor Sidorenko <jegor@nfm.lt>
2020-05-06 17:12:41 +0200

    Random fixes part II (#1929)
    * Code refactoring
    
    * Ensure the fcm token is a string

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -123,18 +123,17 @@ export const loginAction = (
       session: { data: { isOnline } },
     } = getState();
 
-    const { wallet: encryptedWallet } = await storage.get('wallet');
-
     dispatch({
       type: UPDATE_WALLET_STATE,
       payload: DECRYPTING,
     });
-    await delay(100);
 
     try {
       let wallet;
 
       if (pin) {
+        const { wallet: encryptedWallet } = await storage.get('wallet');
+        await delay(100);
         const saltedPin = await getSaltedPin(pin, dispatch);
         wallet = await decryptWallet(encryptedWallet, saltedPin, { mnemonic: true });
         // no further code will be executed if pin is wrong
