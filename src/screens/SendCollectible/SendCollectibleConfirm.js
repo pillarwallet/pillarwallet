2e9e4c00ea42c9ec43698d3f5b1d3e30498a2f0a
Deimantas Špučys <as.deimis@gmail.com>
2020-05-08 12:56:31 -0700

    collectible transaction gas fixes

--- src/screens/SendCollectible/SendCollectibleConfirm.js
+++ src/screens/SendCollectible/SendCollectibleConfirm.js
@@ -6,34 +6,50 @@ import type { NavigationScreenProp } from 'react-navigation';
 import { connect } from 'react-redux';
 import { utils } from 'ethers';
 import { createStructuredSelector } from 'reselect';
-import { NETWORK_PROVIDER } from 'react-native-dotenv';
+import { COLLECTIBLES_NETWORK, GAS_TOKEN_ADDRESS, NETWORK_PROVIDER } from 'react-native-dotenv';
+import get from 'lodash.get';
+import isEmpty from 'lodash.isempty';
+import { BigNumber } from 'bignumber.js';
 
-import type { CollectibleTransactionPayload } from 'models/Transaction';
-import type { GasInfo } from 'models/GasInfo';
-import type { Accounts } from 'models/Account';
-import type { Dispatch, RootReducerState } from 'reducers/rootReducer';
+// actions
+import { fetchGasInfoAction } from 'actions/historyActions';
 
-import {
-  SEND_COLLECTIBLE_CONTACTS,
-  SEND_TOKEN_ASSETS,
-  SEND_TOKEN_PIN_CONFIRM,
-} from 'constants/navigationConstants';
+// components
 import { ScrollWrapper } from 'components/Layout';
-import { Label, MediumText } from 'components/Typography';
+import { BaseText, Label, MediumText } from 'components/Typography';
 import Button from 'components/Button';
 import ContainerWithHeader from 'components/Layout/ContainerWithHeader';
 import TextInput from 'components/TextInput';
 import Spinner from 'components/Spinner';
-import { fetchGasInfoAction } from 'actions/historyActions';
+
+// constants
+import { SEND_COLLECTIBLE_CONTACTS, SEND_TOKEN_ASSETS, SEND_TOKEN_PIN_CONFIRM } from 'constants/navigationConstants';
+import { ETH, SPEED_TYPES } from 'constants/assetsConstants';
+
+// utils
 import { fontSizes, spacing } from 'utils/variables';
-import { getUserName } from 'utils/contacts';
-import { addressesEqual } from 'utils/assets';
-import { getAccountName } from 'utils/accounts';
-import { calculateGasEstimate, fetchRinkebyETHBalance } from 'services/assets';
-import { activeAccountAddressSelector } from 'selectors';
+import { findMatchingContact, getUserName } from 'utils/contacts';
+import { addressesEqual, getAssetDataByAddress, getAssetsAsList, isEnoughBalanceForTransactionFee } from 'utils/assets';
+import { checkIfSmartWalletAccount, getAccountName } from 'utils/accounts';
+import { formatTransactionFee, getEthereumProvider } from 'utils/common';
 
+// services
+import smartWalletService from 'services/smartWallet';
+import { buildERC721TransactionData, calculateGasEstimate, fetchRinkebyETHBalance } from 'services/assets';
 
-const NORMAL = 'avg';
+// selectors
+import { activeAccountAddressSelector, activeAccountSelector } from 'selectors';
+import { accountBalancesSelector } from 'selectors/balances';
+import { accountAssetsSelector } from 'selectors/assets';
+
+// types
+import type { CollectibleTransactionPayload, GasToken } from 'models/Transaction';
+import type { GasInfo } from 'models/GasInfo';
+import type { Account, Accounts } from 'models/Account';
+import type { Dispatch, RootReducerState } from 'reducers/rootReducer';
+import type { Asset, Assets, Balances } from 'models/Asset';
+import type { ContactSmartAddressData } from 'models/Contacts';
+
 
 type Props = {
   navigation: NavigationScreenProp<*>,
@@ -44,16 +60,25 @@ type Props = {
   wallet: Object,
   activeAccountAddress: string,
   accounts: Accounts,
+  activeAccount: ?Account,
+  accountAssets: Assets,
+  supportedAssets: Asset[],
+  smartWalletAccountSupportsGasToken: boolean,
+  balances: Balances,
+  contactsSmartAddresses: ContactSmartAddressData[],
 };
 
 type State = {
   note: ?string,
   rinkebyETH: string,
+  gettingFee: boolean,
   gasLimit: number,
+  feeByGasToken: boolean,
+  txFeeInWei: BigNumber,
 };
 
 const FooterWrapper = styled.View`
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: ${spacing.large}px;
@@ -73,6 +98,7 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
   receiver: string;
   receiverEnsName: string;
   source: string;
+  gasToken: ?GasToken;
 
   constructor(props) {
     super(props);
@@ -80,34 +106,40 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
     this.receiver = this.props.navigation.getParam('receiver', '');
     this.source = this.props.navigation.getParam('source', '');
     this.receiverEnsName = this.props.navigation.getParam('receiverEnsName');
+    let feeByGasToken = false;
+
+    const {
+      activeAccount,
+      accountAssets,
+      supportedAssets,
+      smartWalletAccountSupportsGasToken,
+    } = props;
+    const gasTokenData = getAssetDataByAddress(getAssetsAsList(accountAssets), supportedAssets, GAS_TOKEN_ADDRESS);
+    const isSmartAccount = activeAccount && checkIfSmartWalletAccount(activeAccount);
+    if (isSmartAccount
+      && smartWalletAccountSupportsGasToken
+      && !isEmpty(gasTokenData)) {
+      const { decimals, address, symbol } = gasTokenData;
+      this.gasToken = { decimals, address, symbol };
+      feeByGasToken = true;
+    }
 
     this.state = {
       note: null,
       rinkebyETH: '',
       gasLimit: 0,
+      gettingFee: true,
+      feeByGasToken,
+      txFeeInWei: new BigNumber(0),
     };
   }
 
   componentDidMount() {
-    const {
-      activeAccountAddress,
-      fetchGasInfo,
-    } = this.props;
+    const { fetchGasInfo } = this.props;
     if (Platform.OS === 'android') BackHandler.addEventListener('hardwareBackPress', this.handleBackAction);
     fetchGasInfo();
     this.fetchETHBalanceInRinkeby();
-    const {
-      id: tokenId,
-      contractAddress,
-    } = this.assetData;
-    calculateGasEstimate({
-      from: activeAccountAddress,
-      to: this.receiver,
-      contractAddress,
-      tokenId,
-    })
-      .then(gasLimit => this.setState({ gasLimit }))
-      .catch(() => null);
+    this.calculateTransactionFee();
   }
 
   componentDidUpdate(prevProps: Props) {
@@ -119,6 +151,51 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
   componentWillUnmount() {
     if (Platform.OS === 'android') BackHandler.removeEventListener('hardwareBackPress', this.handleBackAction);
   }
+
+  getTransactionPayload = (): CollectibleTransactionPayload => {
+    const { activeAccountAddress } = this.props;
+    const { txFeeInWei, note } = this.state;
+    const {
+      name,
+      tokenType,
+      id: tokenId,
+      contractAddress,
+    } = this.assetData;
+
+    return {
+      from: activeAccountAddress,
+      to: this.receiver,
+      receiverEnsName: this.receiverEnsName,
+      name,
+      contractAddress,
+      tokenType,
+      tokenId,
+      txFeeInWei,
+      note,
+    };
+  };
+
+  calculateTransactionFee = async () => {
+    const { activeAccount, gasInfo } = this.props;
+    let { gasLimit } = this.state;
+    const transactionPayload = this.getTransactionPayload();
+    let txFeeInWei;
+
+    if (activeAccount && checkIfSmartWalletAccount(activeAccount)) {
+      txFeeInWei = await this.getSmartWalletTxFeeInWei(transactionPayload);
+    } else {
+      const gasPrice = gasInfo.gasPrice[SPEED_TYPES.NORMAL] || 0;
+      const gasPriceWei = utils.parseUnits(gasPrice.toString(), 'gwei');
+      gasLimit = await calculateGasEstimate(transactionPayload);
+      txFeeInWei = gasPriceWei.mul(gasLimit);
+    }
+
+    this.setState({
+      gettingFee: false,
+      txFeeInWei,
+      gasLimit,
+    });
+  };
 
   handleBackAction = () => {
     const { navigation, contacts } = this.props;
@@ -147,32 +224,54 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
     this.setState({ rinkebyETH: rinkebyETHBlanace });
   };
 
+  getSmartWalletTxFeeInWei = async (transaction: CollectibleTransactionPayload): BigNumber => {
+    const { feeByGasToken } = this.state;
+    const { gasInfo } = this.props;
+
+    const provider = getEthereumProvider(COLLECTIBLES_NETWORK);
+    const data = await buildERC721TransactionData(transaction, provider);
+
+    const estimateTransaction = {
+      data,
+      recipient: transaction.contractAddress || '',
+      gasToken: this.gasToken,
+      value: 0,
+    };
+
+    const { gasTokenCost, cost: ethCost } = await smartWalletService
+      .estimateAccountTransaction(estimateTransaction, gasInfo)
+      .catch(() => ({}));
+
+    if (gasTokenCost && gasTokenCost.gt(0)) {
+      if (!feeByGasToken) this.setState({ feeByGasToken: true });
+
+      return gasTokenCost;
+    }
+
+    if (feeByGasToken) this.setState({ feeByGasToken: false });
+
+    return ethCost || new BigNumber(0);
+  };
+
   handleFormSubmit = () => {
     Keyboard.dismiss();
-    const { navigation } = this.props;
-    const { note, gasLimit } = this.state;
-    const {
-      name,
-      tokenType,
-      id: tokenId,
-      contractAddress,
-    } = this.assetData;
+    const { navigation, activeAccount } = this.props;
+    const { txFeeInWei, gasLimit, feeByGasToken } = this.state;
 
-    const gasPrice = this.getGasPriceInWei().toNumber();
-    const txFeeInWei = this.getTxFeeInWei();
+    let transactionPayload = this.getTransactionPayload();
 
-    const transactionPayload: CollectibleTransactionPayload = {
-      to: this.receiver,
-      receiverEnsName: this.receiverEnsName,
-      name,
-      contractAddress,
-      tokenType,
-      tokenId,
-      note,
-      gasLimit,
-      gasPrice,
-      txFeeInWei,
-    };
+    if (!activeAccount || !checkIfSmartWalletAccount(activeAccount)) {
+      const gasPrice = gasLimit ? txFeeInWei.div(gasLimit).toNumber() : 0;
+      transactionPayload = {
+        ...transactionPayload,
+        gasLimit,
+        gasPrice,
+      };
+    }
+
+    if (feeByGasToken) {
+      transactionPayload = { ...transactionPayload, gasToken: this.gasToken };
+    }
 
     navigation.navigate(SEND_TOKEN_PIN_CONFIRM, {
       transactionPayload,
@@ -184,34 +283,50 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
     this.setState({ note: text });
   }
 
-  getGasPriceInWei = () => {
-    const { gasInfo } = this.props;
-    const gasPrice = gasInfo.gasPrice[NORMAL] || 0;
-    return utils.parseUnits(gasPrice.toString(), 'gwei');
-  };
-
-  getTxFeeInWei = () => {
-    const { gasLimit } = this.state;
-    const gasPriceWei = this.getGasPriceInWei();
-    return gasPriceWei.mul(gasLimit);
-  };
-
   render() {
     const {
       contacts,
       session,
       gasInfo,
       accounts,
+      contactsSmartAddresses,
+      balances,
     } = this.props;
+    const {
+      rinkebyETH,
+      gettingFee,
+      txFeeInWei,
+      feeByGasToken,
+    } = this.state;
     const { name } = this.assetData;
-    const { rinkebyETH, gasLimit } = this.state;
+
+    // recipient
     const to = this.receiver;
-    const txFeeInWei = this.getTxFeeInWei();
-    const txFee = utils.formatEther(txFeeInWei.toString());
-    const contact = contacts.find(({ ethAddress }) => to.toUpperCase() === ethAddress.toUpperCase());
+    const contact = findMatchingContact(to, contacts, contactsSmartAddresses);
     const recipientUsername = getUserName(contact);
-    const canProceedTesting = parseFloat(rinkebyETH) > parseFloat(txFee) || NETWORK_PROVIDER !== 'ropsten';
     const userAccount = !recipientUsername ? accounts.find(({ id }) => addressesEqual(id, to)) : null;
+
+    // rinkeby testing check
+    const txFee = utils.formatEther(txFeeInWei.toString());
+    const canProceedTesting = parseFloat(rinkebyETH) > parseFloat(txFee);
+
+    // fee
+    const parsedGasToken = feeByGasToken && !isEmpty(this.gasToken) ? this.gasToken : null;
+    const balanceCheckTransaction = {
+      txFeeInWei,
+      amount: 0,
+      gasToken: parsedGasToken,
+    };
+    const isEnoughForFee = canProceedTesting || isEnoughBalanceForTransactionFee(balances, balanceCheckTransaction);
+    const feeDisplayValue = formatTransactionFee(txFeeInWei, parsedGasToken);
+    const feeSymbol = get(parsedGasToken, 'symbol', ETH);
+    const errorMessage = !isEnoughForFee && `Not enough ${feeSymbol} for transaction fee`;
+
+    // confirm button
+    const isConfirmDisabled = gettingFee || !session.isOnline || !gasInfo.isFetched || !isEnoughForFee;
+    const confirmButtonTitle = gettingFee
+      ? 'Getting the fee..'
+      : 'Confirm Transaction';
 
     return (
       <ContainerWithHeader
@@ -221,10 +336,15 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
         }}
         footer={(
           <FooterWrapper>
+            {!!errorMessage &&
+            <BaseText negative regular center style={{ marginBottom: 15 }}>
+              {errorMessage}
+            </BaseText>
+            }
             <Button
-              disabled={!gasLimit || !session.isOnline || !gasInfo.isFetched || !canProceedTesting}
+              disabled={isConfirmDisabled}
               onPress={this.handleFormSubmit}
-              title="Confirm Transaction"
+              title={confirmButtonTitle}
             />
           </FooterWrapper>
         )}
@@ -261,10 +381,8 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
           </LabeledRow>
           <LabeledRow>
             <Label>Est. Network Fee</Label>
-            {
-              (!!gasLimit && <Value>{txFee} ETH</Value>)
-              || <Spinner style={{ marginTop: 5 }} width={20} height={20} />
-            }
+            {!gettingFee && <Value>{feeDisplayValue}</Value>}
+            {gettingFee && <Spinner style={{ marginTop: 5 }} width={20} height={20} />}
           </LabeledRow>
           {NETWORK_PROVIDER === 'ropsten' &&
           <LabeledRow>
@@ -292,21 +410,32 @@ class SendCollectibleConfirm extends React.Component<Props, State> {
 }
 
 const mapStateToProps = ({
-  contacts: { data: contacts },
+  contacts: {
+    data: contacts,
+    contactsSmartAddresses: { addresses: contactsSmartAddresses },
+  },
   session: { data: session },
   history: { gasInfo },
   wallet: { data: wallet },
   accounts: { data: accounts },
+  assets: { supportedAssets },
+  smartWallet: { connectedAccount: { gasTokenSupported: smartWalletAccountSupportsGasToken } },
 }: RootReducerState): $Shape<Props> => ({
   contacts,
   session,
   gasInfo,
   wallet,
   accounts,
+  supportedAssets,
+  smartWalletAccountSupportsGasToken,
+  contactsSmartAddresses,
 });
 
 const structuredSelector = createStructuredSelector({
+  balances: accountBalancesSelector,
   activeAccountAddress: activeAccountAddressSelector,
+  activeAccount: activeAccountSelector,
+  accountAssets: accountAssetsSelector,
 });
 
 const combinedMapStateToProps = (state: RootReducerState): $Shape<Props> => ({
