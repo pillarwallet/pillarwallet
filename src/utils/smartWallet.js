48d5df9b1d4c30b4982a881d26d7ddd6bd5107a3
Jegor Sidorenko <jegor@nfm.lt>
2020-05-15 12:09:59 +0200

    Chore

--- src/utils/smartWallet.js
+++ src/utils/smartWallet.js
@@ -147,7 +147,7 @@ export const parseSmartWalletTransactions = (
   smartWalletTransactions: IAccountTransaction[],
   supportedAssets: Asset[],
   assets: Asset[],
-  accountHasGasTokenSupport: boolean,
+  isGasTokenSupportedForAccount: boolean,
 ): Transaction[] => smartWalletTransactions
   .reduce((mapped, smartWalletTransaction) => {
     const {
@@ -267,7 +267,7 @@ export const parseSmartWalletTransactions = (
       };
     } else if (transactionType === AccountTransactionTypes.AddDevice) {
       const addedDeviceAddress = get(smartWalletTransaction, 'extra.address');
-      if (!isEmpty(addedDeviceAddress) && accountHasGasTokenSupport) {
+      if (!isEmpty(addedDeviceAddress) && isGasTokenSupportedForAccount) {
         transaction = {
           ...transaction,
           tag: SMART_WALLET_SWITCH_TO_GAS_TOKEN_RELAYER,
