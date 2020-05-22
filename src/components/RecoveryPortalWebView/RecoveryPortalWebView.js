ce6dcf5b6d3d55fdbaabbb4ce93dbccfae92d9fc
Deimantas Špučys <as.deimis@gmail.com>
2020-05-22 14:40:34 -0700

    recovery portal webview as component

---
+++ src/components/RecoveryPortalWebView/RecoveryPortalWebView.js
@@ -0,0 +1,100 @@
+// @flow
+/*
+    Pillar Wallet: the personal data locker
+    Copyright (C) 2019 Stiftung Pillar Project
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { RECOVERY_PORTAL_URL } from 'react-native-dotenv';
+import { useBackHandler } from '@react-native-community/hooks';
+import type { NavigationScreenProp } from 'react-navigation';
+
+// components
+import { Wrapper } from 'components/Layout';
+import ContainerWithHeader from 'components/Layout/ContainerWithHeader';
+import Spinner from 'components/Spinner';
+
+
+type Props = {
+  onWebViewMessage?: Function,
+  navigation: NavigationScreenProp,
+  onWebViewNavigate?: Function,
+  onRef?: Function,
+  title?: string,
+  isVisible?: boolean,
+  webViewRef?: WebView,
+};
+
+const renderLoading = () => <Spinner style={{ alignSelf: 'center', position: 'absolute', top: '50%' }} />;
+
+const RecoveryPortalWebView = ({
+  onWebViewMessage,
+  title,
+  onWebViewNavigate,
+  isVisible = true,
+  navigation,
+  onRef,
+}: Props) => {
+  let webViewRef;
+  let canWebViewNavigateBack = false;
+
+  const handleNavigationBack = () => {
+    if (!webViewRef || !canWebViewNavigateBack) {
+      navigation.goBack();
+      return;
+    }
+    webViewRef.goBack();
+  };
+
+  useBackHandler(() => {
+    handleNavigationBack();
+    return true;
+  });
+
+  const onNavigationStateChange = (webViewNavigationState) => {
+    canWebViewNavigateBack = !!webViewNavigationState.canGoBack;
+    if (onWebViewNavigate) onWebViewNavigate(webViewNavigationState);
+  };
+
+  return (
+    <ContainerWithHeader
+      headerProps={{
+        centerItems: [{ title: title || 'Recovery Portal' }],
+        customOnBack: handleNavigationBack,
+      }}
+    >
+      <Wrapper style={{ flex: 1 }}>
+        {!isVisible && renderLoading()}
+        {isVisible &&
+          <WebView
+            ref={(ref) => { webViewRef = ref; if (onRef) onRef(webViewRef); }}
+            source={{ uri: RECOVERY_PORTAL_URL }}
+            onNavigationStateChange={onNavigationStateChange}
+            allowsBackForwardNavigationGestures={false}
+            onMessage={onWebViewMessage}
+            renderLoading={renderLoading}
+            originWhitelist={['*']}
+            cacheEnabled={false}
+            thirdPartyCookiesEnabled={false}
+            hideKeyboardAccessoryView
+            startInLoadingState
+            incognito
+          />
+        }
+      </Wrapper>
+    </ContainerWithHeader>
+  );
+};
+
+export default RecoveryPortalWebView;
--- src/screens/RecoveryPortal/RecoveryPortalWalletRecovery.js
+++ src/screens/RecoveryPortal/RecoveryPortalWalletRecovery.js
@@ -14,21 +14,15 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-import * as React from 'react';
-import { BackHandler, Platform } from 'react-native';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { WebView } from 'react-native-webview';
-import get from 'lodash.get';
-import { RECOVERY_PORTAL_URL } from 'react-native-dotenv';
 import type { NavigationScreenProp } from 'react-navigation';
 
 // actions
 import { initRecoveryPortalWalletRecoverAction } from 'actions/recoveryPortalActions';
 
 // components
-import { Wrapper } from 'components/Layout';
-import ContainerWithHeader from 'components/Layout/ContainerWithHeader';
-import Spinner from 'components/Spinner';
+import RecoveryPortalWebView from 'components/RecoveryPortalWebView';
 
 // types
 import type { Dispatch, RootReducerState } from 'reducers/rootReducer';
@@ -42,40 +36,22 @@ type Props = {
   initRecoveryPortalWalletRecover: () => void,
 };
 
-class RecoveryPortalWalletRecovery extends React.Component<Props> {
-  webViewRef: WebView;
-  canWebviewNavigateBack: boolean = false;
+const RecoveryPortalWalletRecovery = ({
+  navigation,
+  temporaryWallet,
+  isRecoveryPending,
+  initRecoveryPortalWalletRecover,
+}: Props) => {
+  useEffect(() => {
+    initRecoveryPortalWalletRecover();
+  }, []);
 
-  componentDidMount() {
-    this.props.initRecoveryPortalWalletRecover();
-    if (Platform.OS !== 'android') return;
-    BackHandler.addEventListener('hardwareBackPress', this.handleNavigationBack);
-  }
+  let webViewRef;
+  const showWebView = !!temporaryWallet || isRecoveryPending;
 
-  componentWillUnmount() {
-    if (Platform.OS !== 'android') return;
-    BackHandler.removeEventListener('hardwareBackPress', this.handleNavigationBack);
-  }
-
-  renderLoading = () => <Spinner style={{ alignSelf: 'center', position: 'absolute', top: '50%' }} />;
-
-  handleNavigationBack = () => {
-    if (!this.webViewRef || !this.canWebviewNavigateBack) {
-      this.props.navigation.goBack();
-      return;
-    }
-    this.webViewRef.goBack();
-  };
-
-  onNavigationStateChange = (webViewNavigationState) => {
-    this.canWebviewNavigateBack = !!webViewNavigationState.canGoBack;
-  };
-
-  onWebViewMessage = (message) => {
-    const messageType = get(message, 'nativeEvent.data');
-    const { temporaryWallet } = this.props;
-    if (!this.webViewRef || messageType !== 'getRecoveryDeviceAddress' || !temporaryWallet) return;
-    this.webViewRef.injectJavaScript(`
+  const onWebViewMessage = (message) => {
+    if (!webViewRef || message?.nativeEvent?.data !== 'getRecoveryDeviceAddress' || !temporaryWallet) return;
+    webViewRef.injectJavaScript(`
       var event = new CustomEvent("recoveryDeviceAddressAdded", {
         detail: { address: "${temporaryWallet.address}" }
       });
@@ -83,41 +59,15 @@ class RecoveryPortalWalletRecovery extends React.Component<Props> {
     `);
   };
 
-  render() {
-    const { temporaryWallet, isRecoveryPending } = this.props;
-
-    const showWebView = !!temporaryWallet || isRecoveryPending;
-
-    return (
-      <ContainerWithHeader
-        headerProps={{
-          centerItems: [{ title: 'Recovery Portal' }],
-          customOnBack: this.handleNavigationBack,
-        }}
-      >
-        <Wrapper style={{ flex: 1 }}>
-          {!showWebView && this.renderLoading()}
-          {showWebView &&
-            <WebView
-              ref={(ref) => { this.webViewRef = ref; }}
-              source={{ uri: RECOVERY_PORTAL_URL }}
-              onNavigationStateChange={this.onNavigationStateChange}
-              onMessage={this.onWebViewMessage}
-              renderLoading={this.renderLoading}
-              originWhitelist={['*']}
-              cacheEnabled={false}
-              sharedCookiesEnabled={false}
-              thirdPartyCookiesEnabled={false}
-              hideKeyboardAccessoryView
-              startInLoadingState
-              incognito
-            />
-          }
-        </Wrapper>
-      </ContainerWithHeader>
-    );
-  }
-}
+  return (
+    <RecoveryPortalWebView
+      onRef={(ref) => { webViewRef = ref; }}
+      isVisible={showWebView}
+      onWebViewMessage={onWebViewMessage}
+      navigation={navigation}
+    />
+  );
+};
 
 const mapStateToProps = ({
   wallet: { backupStatus: { isRecoveryPending } },
