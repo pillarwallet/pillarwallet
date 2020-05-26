dee736ae258b3c11ed5c6822e1d492eeb1fb8049
alazarevski <aleksandar.lazarevski@tarmac.io>
2020-05-26 10:52:40 +0200

    Fix PortfolioBalances

--- src/components/PortfolioBalance/PortfolioBalance.js
+++ src/components/PortfolioBalance/PortfolioBalance.js
@@ -22,7 +22,6 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import styled from 'styled-components/native';
 
-import type { BitcoinBalance } from 'models/Bitcoin';
 import type { RootReducerState } from 'reducers/rootReducer';
 import type {
   Balances,
@@ -33,7 +32,6 @@ import BalanceView from 'components/PortfolioBalance/BalanceView';
 import { BaseText, MediumText } from 'components/Typography';
 import Icon from 'components/Icon';
 import { calculateBalanceInFiat } from 'utils/assets';
-import { calculateBitcoinBalanceInFiat } from 'utils/bitcoin';
 import { fontSizes, fontStyles, spacing } from 'utils/variables';
 import { themedColors } from 'utils/themes';
 import { allBalancesSelector } from 'selectors/balances';
@@ -42,7 +40,6 @@ import { allBalancesSelector } from 'selectors/balances';
 type Props = {
   rates: Rates,
   balances: Balances,
-  bitcoinBalances: BitcoinBalance,
   fiatCurrency: string,
   style: Object,
   showBalance: boolean,
@@ -85,18 +82,6 @@ const LabelText = styled(BaseText)`
   margin-bottom: 8px;
 `;
 
-const getCombinedBalances = (props: Props): number => {
-  const {
-    balances,
-    fiatCurrency,
-    rates,
-    bitcoinBalances,
-  } = props;
-
-  return calculateBitcoinBalanceInFiat(rates, bitcoinBalances, fiatCurrency)
-    + calculateBalanceInFiat(rates, balances, fiatCurrency);
-};
-
 class PortfolioBalance extends React.PureComponent<Props> {
   render() {
     const {
@@ -104,9 +89,11 @@ class PortfolioBalance extends React.PureComponent<Props> {
       fiatCurrency,
       showBalance,
       toggleBalanceVisibility,
+      rates,
+      balances,
     } = this.props;
 
-    const combinedBalance = getCombinedBalances(this.props);
+    const combinedBalance = calculateBalanceInFiat(rates, balances, fiatCurrency);
 
     return (
       <BalanceWrapper>
@@ -134,10 +121,8 @@ class PortfolioBalance extends React.PureComponent<Props> {
 
 const mapStateToProps = ({
   rates: { data: rates },
-  bitcoin: { data: { balances: bitcoinBalances } },
 }: RootReducerState): $Shape<Props> => ({
   rates,
-  bitcoinBalances,
 });
 
 const structuredSelector = createStructuredSelector({
