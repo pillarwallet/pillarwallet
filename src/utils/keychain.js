7dd04aa2c26904b7fdcc4bc8b5a1f6dec54c9bec
Jegor Sidorenko <jegor@nfm.lt>
2020-06-02 14:09:22 +0200

    Use proper keychain id for staging builds

--- src/utils/keychain.js
+++ src/utils/keychain.js
@@ -21,8 +21,9 @@ import { Platform } from 'react-native';
 import * as Keychain from 'react-native-keychain';
 import get from 'lodash.get';
 import isEmpty from 'lodash.isempty';
+import { BUILD_TYPE } from 'react-native-dotenv';
 
-const KEYCHAIN_SERVICE = 'com.pillarproject.wallet';
+const KEYCHAIN_SERVICE = `com.pillarproject.wallet${BUILD_TYPE === 'staging' ? '.staging' : ''}`;
 const KEYCHAIN_DATA_KEY = 'data';
 const BIOMETRICS_PROMPT_MESSAGE = 'Continue';
 
