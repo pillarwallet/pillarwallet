3724444ab29b65109a9fc08656140124844f8652
Angelika Serwa <angelika.serwa@swmansion.com>
2020-06-02 13:40:00 +0200

    Update verified modal copy

--- src/screens/Users/VerifiedModal.js
+++ src/screens/Users/VerifiedModal.js
@@ -18,6 +18,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 import * as React from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components/native';
 import { MediumText, BaseText } from 'components/Typography';
 import Button from 'components/Button';
@@ -25,6 +26,9 @@ import SlideModal from 'components/Modals/SlideModal';
 import Icon from 'components/Icon';
 import { Spacing } from 'components/Layout';
 import { themedColors } from 'utils/themes';
+import { getCampaignRewardText } from 'utils/referrals';
+import type { RootReducerState } from 'reducers/rootReducer';
+import type { RewardsByCompany } from 'reducers/referralsReducer';
 
 
 type Props = {
@@ -32,6 +36,8 @@ type Props = {
   onModalHide: () => void,
   verifiedField: ?string,
   onButtonPress: () => void,
+  isPillarRewardCampaignActive: boolean,
+  rewardsByCampaign: RewardsByCompany,
 };
 
 const Wrapper = styled.View`
@@ -48,9 +54,16 @@ const LikeIcon = styled(Icon)`
 
 const VerifiedModal = (props: Props) => {
   const {
-    isVisible, onModalHide, verifiedField, onButtonPress,
+    isVisible, onModalHide, verifiedField, onButtonPress, isPillarRewardCampaignActive, rewardsByCampaign,
   } = props;
   const referralMethod = verifiedField === 'phone' ? 'text message' : 'email';
+  const rewardText = getCampaignRewardText(rewardsByCampaign.pillar);
+
+  const text = (isPillarRewardCampaignActive && rewardText) ?
+    `You are now able to invite friends via ${referralMethod}. You will receive ${rewardText} and a badge 
+     for each friend installed the app with your referral link.` :
+    `You are now able to invite friends via ${referralMethod}. Start receiving badges for friends 
+     installed Pillar with your referral link!`;
 
   return (
     <SlideModal
@@ -65,10 +78,7 @@ const VerifiedModal = (props: Props) => {
         <Spacing h={40} />
         <MediumText center large >Your {verifiedField} has been verified</MediumText>
         <Spacing h={15} />
-        <BaseText center medium>
-          You are now able to invite friends via {referralMethod}. You will receive 25 PLR
-          and a badge for each friend installed the app with your referral link.
-        </BaseText>
+        <BaseText center medium>{text}</BaseText>
         <Spacing h={32} />
         <Button title="Invite friends" onPress={onButtonPress} />
       </Wrapper>
@@ -76,4 +86,11 @@ const VerifiedModal = (props: Props) => {
   );
 };
 
-export default VerifiedModal;
+const mapStateToProps = ({
+  referrals: { isPillarRewardCampaignActive, rewardsByCampaign },
+}: RootReducerState): $Shape<Props> => ({
+  isPillarRewardCampaignActive,
+  rewardsByCampaign,
+});
+
+export default connect(mapStateToProps)(VerifiedModal);
