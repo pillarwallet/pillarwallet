fe7526a144c9167a9bcbc90240d3f6faf0458711
Angelika Serwa <angelika.serwa@swmansion.com>
2020-06-09 15:48:51 +0200

    remove nulls checks

--- src/components/ActivityFeed/ActivityFeedItem.js
+++ src/components/ActivityFeed/ActivityFeedItem.js
@@ -443,7 +443,7 @@ export class ActivityFeedItem extends React.Component<Props> {
               label: usernameOrAddress,
               avatarUrl,
               isReceived,
-              username: contact && contact.username,
+              username: contact?.username,
             };
 
             if (event.extra) {
@@ -527,7 +527,7 @@ export class ActivityFeedItem extends React.Component<Props> {
             label: itemLabel,
             subtext,
             avatarUrl,
-            username: contact && contact.username,
+            username: contact?.username,
             itemValue: `${directionSymbol} ${formattedValue} ${event.asset}`,
             valueColor: isReceived && formattedValue !== '0' ? 'positive' : 'text',
             ...additionalInfo,
