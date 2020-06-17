56a3f42c89c0665041b5edecd8f3aa2bec18b3bd
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-17 14:39:35 +0200

    add connext service

---
+++ src/services/connext.js
@@ -0,0 +1,9 @@
+// @flow
+import * as connext from '@connext/client';
+import {getAsyncStore} from '@connext/store';
+
+export async function connectChannel(signer: string) {
+  const store = getAsyncStore();
+  const network = 'rinkeby';
+  return connext.connect('rinkeby', {signer, store});
+}
