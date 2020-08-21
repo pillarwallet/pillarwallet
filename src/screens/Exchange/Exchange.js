3b99d982675fb2059b3bb39638069e933b2d5649
jdnmr <donimirski5@gmail.com>
2020-08-21 18:55:57 +0200

    missing translations

--- src/screens/Exchange/Exchange.js
+++ src/screens/Exchange/Exchange.js
@@ -467,7 +467,7 @@ class ExchangeScreen extends React.Component<Props, State> {
         <SelectorOptions
           isVisible={showBuyOptions || showSellOptions}
           onHide={() => this.setState({ showBuyOptions: false, showSellOptions: false })}
-          title={showSellOptions ? 'Sell' : 'Buy'}
+          title={t(`label.${showSellOptions ? 'sell' : 'buy'}`)}
           options={showSellOptions ? fromOptions : toOptions}
           searchPlaceholder={t('form.selector.searchPlaceholder')}
           onOptionSelect={this.handleSelectorOptionSelect}
