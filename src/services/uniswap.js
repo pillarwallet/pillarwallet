bbf089afabd2654294356ea136a350cabe6cbc72
alazarevski <37015091+alazarevski@users.noreply.github.com>
2020-08-25 11:09:59 +0200

    Update src/services/uniswap.js
    Co-authored-by: Deimantas <as.deimis@gmail.com>

--- src/services/uniswap.js
+++ src/services/uniswap.js
@@ -64,9 +64,7 @@ import { ETH } from 'constants/assetsConstants';
 // assets
 import ERC20_CONTRACT_ABI from 'abi/erc20.json';
 
-const ethProvider = () => {
-  return getEthereumProvider(getEnv('NETWORK_PROVIDER'));
-};
+const ethProvider = () => getEthereumProvider(getEnv('NETWORK_PROVIDER'));
 
 const getBackupRoute = async (
   fromAssetAddress: string,
