6c3a822a759454dae4941e29824809efbee4a07a
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-09-15 15:17:52 +0300

    Small fix

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -446,7 +446,7 @@ export const logoutAction = () => {
     if (themeType === DARK_THEME) await dispatch(setAppThemeAction(DARK_THEME)); // to persist dark theme after storage
     // is cleaned up so we would not blind users after they delete wallet :)
 
-    // leave translation initialisated flag in place
+    // leave translation initialised flag in place
     if (getState()?.session?.data?.translationsInitialised) dispatch(setSessionTranslationBundleInitialisedAction());
     navigate(NavigationActions.navigate({ routeName: ONBOARDING_FLOW }));
   };
