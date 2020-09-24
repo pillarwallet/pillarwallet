0a92d30eb9f5eb6fa115ac99242b4aae208077a5
Weronika Grzybowska <werag2105@gmail.com>
2020-09-24 17:13:27 +0200

    Add type definitions for react-redux hooks

---
+++ flow-typed/npm/react-redux_v7.x.x.js
@@ -0,0 +1,30 @@
+// Partial definitons of types for react-redux from flow-typed repo
+
+declare module "react-redux" {
+  declare export function useDispatch<D>(): D;
+
+  declare export function useSelector<S, SS>(
+    selector: (state: S) => SS,
+    equalityFn?: (a: SS, b: SS) => boolean,
+  ): SS;
+
+  declare export function useStore<Store>(): Store;
+
+  declare export var Provider: any;
+  declare export var createProvider: any;
+  declare export var connect: any;
+  declare export var connectAdvanced: any;
+  declare export var batch: any;
+
+  declare export default {
+    Provider: typeof Provider,
+    createProvider: typeof createProvider,
+    connect: typeof connect,
+    connectAdvanced: typeof connectAdvanced,
+    useDispatch: typeof useDispatch,
+    useSelector: typeof useSelector,
+    useStore: typeof useStore,
+    batch: typeof batch,
+    ...
+  };
+}
