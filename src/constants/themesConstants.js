bf848b8b4e8f6454202f0df7d115b0dd58baa852
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-09-29 16:38:30 +0300

    Fix constants

--- src/constants/themesConstants.js
+++ src/constants/themesConstants.js
@@ -1,11 +1,12 @@
+/* eslint-disable quote-props */
 // @flow
 /*
     Pillar Wallet: the personal data locker
-    Copyright (C) 2019 Stiftung Pillar Project
+    Copyright (C) '2019 Stiftung Pillar Project
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    the Free Software Foundation; either version '2 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
@@ -33,59 +34,59 @@ export const BASIC = {
 };
 
 export const PRIMARY_ACCENT = {
-  100: 'primaryAccent100',
-  110: 'primaryAccent110',
-  120: 'primaryAccent120',
-  130: 'primaryAccent130',
-  140: 'primaryAccent140',
-  150: 'primaryAccent150',
-  160: 'primaryAccent160',
-  170: 'primaryAccent170',
-  180: 'primaryAccent180',
-  190: 'primaryAccent190',
-  200: 'primaryAccent200',
-  210: 'primaryAccent210',
-  220: 'primaryAccent220',
-  230: 'primaryAccent230',
-  240: 'primaryAccent240',
-  250: 'primaryAccent250',
-  260: 'primaryAccent260',
-  270: 'primaryAccent270',
-  280: 'primaryAccent280',
-  290: 'primaryAccent290',
+  '100': 'primaryAccent100',
+  '110': 'primaryAccent110',
+  '120': 'primaryAccent120',
+  '130': 'primaryAccent130',
+  '140': 'primaryAccent140',
+  '150': 'primaryAccent150',
+  '160': 'primaryAccent160',
+  '170': 'primaryAccent170',
+  '180': 'primaryAccent180',
+  '190': 'primaryAccent190',
+  '200': 'primaryAccent200',
+  '210': 'primaryAccent210',
+  '220': 'primaryAccent220',
+  '230': 'primaryAccent230',
+  '240': 'primaryAccent240',
+  '250': 'primaryAccent250',
+  '260': 'primaryAccent260',
+  '270': 'primaryAccent270',
+  '280': 'primaryAccent280',
+  '290': 'primaryAccent290',
 };
 
 export const SECONDARY_ACCENT = {
-  100: 'secondaryAccent100',
-  110: 'secondaryAccent110',
-  120: 'secondaryAccent120',
-  130: 'secondaryAccent130',
-  140: 'secondaryAccent140',
-  150: 'secondaryAccent150',
-  160: 'secondaryAccent160',
-  170: 'secondaryAccent170',
-  180: 'secondaryAccent180',
-  190: 'secondaryAccent190',
-  200: 'secondaryAccent200',
-  210: 'secondaryAccent210',
-  220: 'secondaryAccent220',
-  230: 'secondaryAccent230',
-  240: 'secondaryAccent240',
-  250: 'secondaryAccent250',
-  260: 'secondaryAccent260',
-  270: 'secondaryAccent270',
-  280: 'secondaryAccent280',
-  290: 'secondaryAccent290',
+  '100': 'secondaryAccent100',
+  '110': 'secondaryAccent110',
+  '120': 'secondaryAccent120',
+  '130': 'secondaryAccent130',
+  '140': 'secondaryAccent140',
+  '150': 'secondaryAccent150',
+  '160': 'secondaryAccent160',
+  '170': 'secondaryAccent170',
+  '180': 'secondaryAccent180',
+  '190': 'secondaryAccent190',
+  '200': 'secondaryAccent200',
+  '210': 'secondaryAccent210',
+  '220': 'secondaryAccent220',
+  '230': 'secondaryAccent230',
+  '240': 'secondaryAccent240',
+  '250': 'secondaryAccent250',
+  '260': 'secondaryAccent260',
+  '270': 'secondaryAccent270',
+  '280': 'secondaryAccent280',
+  '290': 'secondaryAccent290',
 };
 
 export const SYNTHETIC = {
-  100: 'synthetic100',
-  110: 'synthetic110',
-  120: 'synthetic120',
-  130: 'synthetic130',
-  140: 'synthetic140',
-  150: 'synthetic150',
-  160: 'synthetic160',
-  170: 'synthetic170',
-  180: 'synthetic180',
+  '100': 'synthetic100',
+  '110': 'synthetic110',
+  '120': 'synthetic120',
+  '130': 'synthetic130',
+  '140': 'synthetic140',
+  '150': 'synthetic150',
+  '160': 'synthetic160',
+  '170': 'synthetic170',
+  '180': 'synthetic180',
 };
