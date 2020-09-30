00b2901fade9ed0826d04488f8d33c06b7ee3a80
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-09-30 12:06:28 +0300

    Fix spacing

--- src/models/Theme.js
+++ src/models/Theme.js
@@ -17,8 +17,10 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 import lightThemeColors from 'utils/themes/lightTheme';
 import darkThemeColors from 'utils/themes/darkTheme';
+
 
 export type ThemeColors = {
   [colorName: string]: string,
