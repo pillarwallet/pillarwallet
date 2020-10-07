6b404dc24f5e13340c177b0b6a58d770d1a1ebf9
Angelika Serwa <angelika.serwa@swmansion.com>
2020-10-07 15:44:41 +0200

    Add ReviewSummary component

---
+++ src/components/ReviewSummary/ReviewSummary.js
@@ -0,0 +1,104 @@
+// @flow
+/*
+    Pillar Wallet: the personal data locker
+    Copyright (C) 2019 Stiftung Pillar Project
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components/native';
+import { getEnv } from 'configs/envConfig';
+import { CachedImage } from 'react-native-cached-image';
+import { BaseText, MediumText } from 'components/Typography';
+import { Spacing } from 'components/Layout';
+import CollectibleImage from 'components/CollectibleImage';
+import { formatAmount } from 'utils/common';
+import { getFormattedRate } from 'utils/assets';
+import { defaultFiatCurrency } from 'constants/assetsConstants';
+import type { Rates, Asset } from 'models/Asset';
+import type { Collectible } from 'models/Collectible';
+import type { RootReducerState } from 'reducers/rootReducer';
+
+
+type Props = {
+  amount?: number,
+  asset?: Asset,
+  collectible?: Collectible,
+  text: string,
+  rates: Rates,
+  baseFiatCurrency: ?string,
+};
+
+const Container = styled.View`
+  align-items: center;
+`;
+
+const TokenImage = styled(CachedImage)`
+  width: 64px;
+  height: 64px;
+`;
+
+const StyledCollectibleImage = styled(CollectibleImage)`
+  width: 192px;
+  height: 192px;
+`;
+
+const ReviewSummary = ({
+  asset, collectible, text, amount, rates, baseFiatCurrency,
+}: Props) => {
+  if (asset && amount) {
+    const assetIcon = `${getEnv().SDK_PROVIDER}/${asset.iconUrl}?size=3`;
+    const formattedAmount = formatAmount(amount);
+    const fiatAmount = getFormattedRate(rates, amount, asset.symbol, baseFiatCurrency || defaultFiatCurrency);
+
+    return (
+      <Container>
+        <TokenImage source={{ uri: assetIcon }} />
+        <Spacing h={16} />
+        <BaseText regular>{text}</BaseText>
+        <Spacing h={16} />
+        <MediumText giant>{formattedAmount} <MediumText secondary fontSize={20}>{asset.symbol}</MediumText></MediumText>
+        <Spacing h={7} />
+        <BaseText secondary small>{fiatAmount}</BaseText>
+      </Container>
+    );
+  }
+
+  if (collectible) {
+    return (
+      <Container>
+        <StyledCollectibleImage source={{ uri: collectible.icon }} />
+        <Spacing h={16} />
+        <BaseText regular>{text}</BaseText>
+        <Spacing h={16} />
+        <MediumText medium>{collectible.name}</MediumText>
+      </Container>
+    );
+  }
+
+  return null;
+};
+
+const mapStateToProps = ({
+  rates: { data: rates },
+  appSettings: { data: { baseFiatCurrency } },
+}: RootReducerState): $Shape<Props> => ({
+  rates,
+  baseFiatCurrency,
+});
+
+export default connect(mapStateToProps)(ReviewSummary);
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -8,6 +8,7 @@ function loadStories() {
 	require('../src/components/ActivityFeed/ActivityFeedItem.stories');
 	require('../src/components/EventDetails/EventDetails.stories');
 	require('../src/components/InsightWithButton/InsightWithButton.stories');
+	require('../src/components/ReviewSummary/ReviewSummary.stories');
 	require('../src/components/SWActivationCard/SWActivationCard.stories');
 	require('../src/components/Selector/Selector.stories');
 	require('../src/components/Toast/ToastCard.stories');
@@ -20,6 +21,7 @@ const stories = [
 	'../src/components/ActivityFeed/ActivityFeedItem.stories',
 	'../src/components/EventDetails/EventDetails.stories',
 	'../src/components/InsightWithButton/InsightWithButton.stories',
+	'../src/components/ReviewSummary/ReviewSummary.stories',
 	'../src/components/SWActivationCard/SWActivationCard.stories',
 	'../src/components/Selector/Selector.stories',
 	'../src/components/Toast/ToastCard.stories',
