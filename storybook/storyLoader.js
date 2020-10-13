23cba89cb911805a745c882aaf2babbe4285ec8c
Angelika Serwa <angelika.serwa@gmail.com>
2020-10-13 11:05:53 +0200

    Update fee toggle component (#2264)

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,6 +7,7 @@ function loadStories() {
 	require('../src/components/ActionModal/ActionModal.stories');
 	require('../src/components/ActivityFeed/ActivityFeedItem.stories');
 	require('../src/components/EventDetails/EventDetails.stories');
+	require('../src/components/FeeLabelToggle/FeeLabelToggle.stories');
 	require('../src/components/InsightWithButton/InsightWithButton.stories');
 	require('../src/components/SWActivationCard/SWActivationCard.stories');
 	require('../src/components/Selector/Selector.stories');
@@ -19,6 +20,7 @@ const stories = [
 	'../src/components/ActionModal/ActionModal.stories',
 	'../src/components/ActivityFeed/ActivityFeedItem.stories',
 	'../src/components/EventDetails/EventDetails.stories',
+	'../src/components/FeeLabelToggle/FeeLabelToggle.stories',
 	'../src/components/InsightWithButton/InsightWithButton.stories',
 	'../src/components/SWActivationCard/SWActivationCard.stories',
 	'../src/components/Selector/Selector.stories',
