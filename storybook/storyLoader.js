ba822dd5da8fc8c389ba1f9cf5e33868b63aa616
Vytaute Vilutyte <v.vilutytei@gmail.com>
2020-10-16 15:53:40 +0300

    Update ListCard component, add it to Storybook

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -9,6 +9,7 @@ function loadStories() {
 	require('../src/components/EventDetails/EventDetails.stories');
 	require('../src/components/HeaderBlock/HeaderBlock.stories');
 	require('../src/components/InsightWithButton/InsightWithButton.stories');
+	require('../src/components/ListItem/ListCard.stories');
 	require('../src/components/SWActivationCard/SWActivationCard.stories');
 	require('../src/components/Selector/Selector.stories');
 	require('../src/components/Toast/ToastCard.stories');
@@ -21,6 +22,7 @@ const stories = [
 	'../src/components/EventDetails/EventDetails.stories',
 	'../src/components/HeaderBlock/HeaderBlock.stories',
 	'../src/components/InsightWithButton/InsightWithButton.stories',
+	'../src/components/ListItem/ListCard.stories',
 	'../src/components/SWActivationCard/SWActivationCard.stories',
 	'../src/components/Selector/Selector.stories',
 	'../src/components/Toast/ToastCard.stories',
