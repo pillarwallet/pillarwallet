e752ceabba63d6e68a005e17058dad49c1ca9217
Deimantas <as.deimis@gmail.com>
2020-11-25 14:40:47 +0200

    PIL-914: 1inch API update, other exchange fixes (#2346)
    * updated 1inch new api version request
    
    * fixed exchange rate display, reduced exchange obj, 1inch api updates, etc

--- src/utils/uniswap.js
+++ src/utils/uniswap.js
@@ -66,12 +66,11 @@ export const getAskRate = (trade: Trade): string => {
   return excutionPriceBN.toFixed();
 };
 
-export const parseAssets = (assets: Asset[]) => {
-  assets.forEach(asset => {
-    asset.address = toChecksumAddress(asset.symbol === ETH ? WETH[chainId]?.address : asset.address);
-    asset.code = asset.symbol;
-  });
-};
+export const parseAssets = (assets: Asset[]): Asset[] => assets.map((asset) => ({
+  ...asset,
+  address: toChecksumAddress(asset.symbol === ETH ? WETH[chainId]?.address : asset.address),
+  code: asset.symbol,
+}));
 
 export const getExpectedOutput = (
   trade: Trade,
@@ -152,18 +151,11 @@ export const swapExactEthToTokens = (
 };
 
 export const generateTxObject = (
-  txCount: string,
   to: string,
   value: string,
   txData: string,
-): Object => {
-  return {
-    to,
-    value,
-    nonce: txCount,
-    gasLimit: '0',
-    gasPrice: '0',
-    chainId,
-    data: txData,
-  };
-};
+): Object => ({
+  to,
+  value,
+  data: txData,
+});
