bfa19d79828231ac7ecbdee9492ac693cef8b0bd
Deimantas Špučys <as.deimis@gmail.com>
2020-11-26 19:15:19 +0200

    chore

--- App.js
+++ App.js
@@ -370,7 +370,13 @@ const mapDispatchToProps = (dispatch: Dispatch): $Shape<Props> => ({
 const AppWithNavigationState = withTranslation()(connect(mapStateToProps, mapDispatchToProps)(App));
 
 const AppRoot = () => (
-  <Suspense fallback={<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}><Spinner /></View>}>
+  <Suspense
+    fallback={(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Spinner theme={defaultTheme} />
+      </View>
+    )}
+  >
     <Provider store={store}>
       <PersistGate
         loading={<Container defaultTheme={defaultTheme}><LoadingSpinner theme={defaultTheme} /></Container>}
