ecfb67337e08a30aad0e60dc5cd667606dd3106b
Deimantas Špučys <as.deimis@gmail.com>
2020-12-02 17:07:51 +0200

    chore

--- src/screens/Exchange/ExchangeOffers.js
+++ src/screens/Exchange/ExchangeOffers.js
@@ -309,6 +309,7 @@ class ExchangeOffers extends React.Component<Props, State> {
       takeOffer,
       setExecutingTransaction,
       fromAmount,
+      resetEstimateTransaction,
     } = this.props;
 
     const {
@@ -324,7 +325,7 @@ class ExchangeOffers extends React.Component<Props, State> {
 
     this.setState({ pressedOfferId: _id }, () => {
       takeOffer(fromAsset, toAsset, amountToSell, provider, trackId, order => {
-        this.props.resetEstimateTransaction();
+        resetEstimateTransaction();
         this.setState({ pressedOfferId: '' }); // reset offer card button loading spinner
         if (isEmpty(order)) return;
         setExecutingTransaction();
