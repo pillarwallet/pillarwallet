ed96342a6ee9c9df8d4a5d0466ff84da1a60d8b9
jdnmr <42654568+jdnmr@users.noreply.github.com>
2021-01-18 16:58:40 +0100

    PIL-922 Fix/fetching exchange supported assets (#2425)
    * use one service's assets in case the other service's call fails
    
    * add tests
    
    * fix tests
    
    * chore

--- src/actions/exchangeActions.js
+++ src/actions/exchangeActions.js
@@ -361,6 +361,7 @@ export const getExchangeSupportedAssetsAction = (callback?: () => void) => {
   return async (dispatch: Dispatch, getState: GetState) => {
     const {
       assets: { supportedAssets },
+      exchange: { exchangeSupportedAssets: _exchangeSupportedAssets },
     } = getState();
 
     dispatch({
@@ -373,7 +374,11 @@ export const getExchangeSupportedAssetsAction = (callback?: () => void) => {
 
     const assetsSymbols = await Promise.all([oneInchAssetsSymbols, uniswapAssetsSymbols]);
 
-    if (!assetsSymbols[1]) {
+    const fetchOneInchSuccess = Array.isArray(assetsSymbols[0]);
+    const fetchUniswapSuccess = Array.isArray(assetsSymbols[1]);
+    const fetchSuccess = fetchOneInchSuccess && fetchUniswapSuccess;
+
+    if (!fetchUniswapSuccess) {
       dispatch({
         type: SET_UNISWAP_TOKENS_QUERY_STATUS,
         payload: { status: UNISWAP_TOKENS_QUERY_STATUS.ERROR },
@@ -385,11 +390,24 @@ export const getExchangeSupportedAssetsAction = (callback?: () => void) => {
       });
     }
 
-    const fetchSuccess: boolean = Array.isArray(assetsSymbols[0]) && Array.isArray(assetsSymbols[1]);
+    // if fetching failed and user can fall back to valid assets, no need to do anything
+    if (_exchangeSupportedAssets?.length && !fetchSuccess) return;
 
-    const fetchedAssetsSymbols: string[] = fetchSuccess ? uniq(assetsSymbols[0].concat(assetsSymbols[1])) : [];
+    let fetchedAssetsSymbols: string[] = [];
+    if (fetchSuccess) {
+      fetchedAssetsSymbols = uniq(assetsSymbols[0].concat(assetsSymbols[1]));
+    } else if (fetchOneInchSuccess) {
+      fetchedAssetsSymbols = uniq(assetsSymbols[0]);
+    } else if (fetchUniswapSuccess) {
+      fetchedAssetsSymbols = uniq(assetsSymbols[1]);
+    }
 
-    const exchangeSupportedAssets = fetchSuccess
+    if (!fetchedAssetsSymbols.length) {
+      reportErrorLog('Failed to fetch exchange supported assets', null);
+      return;
+    }
+
+    const exchangeSupportedAssets = fetchOneInchSuccess || fetchUniswapSuccess
       ? supportedAssets.filter(({ symbol, isSynthetixAsset }) =>
         isSynthetixAsset || fetchedAssetsSymbols.includes(symbol))
       : [];
