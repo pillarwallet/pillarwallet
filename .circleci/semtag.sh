96c0f747c8b00d566f895523812e80f9c89542f3
Antonio Nedanoski <antonio@tarmac.io>
2021-01-19 11:33:11 +0100

    Prettify semtag.sh

--- .circleci/semtag.sh
+++ .circleci/semtag.sh
@@ -6,13 +6,7 @@ chmod +x semtag
 SEMVER_TAG=$(git log -1  --pretty='%s' | awk 'NF>1{print $NF}' | cut -d'#' -f2 | tr '[:upper:]' '[:lower:]')
 
 case "$SEMVER_TAG" in
-	patch)
-		bash semtag final -s $SEMVER_TAG
-		;;
-	minor)
-		bash semtag final -s $SEMVER_TAG
-		;;
-	major)
+	patch | minor | major)
 		bash semtag final -s $SEMVER_TAG
 		;;
 	*)
