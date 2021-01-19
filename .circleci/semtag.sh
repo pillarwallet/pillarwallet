7fe3ee0836ae02a5fd48ee8377b8f77a3a10fc39
Antonio Nedanoski <antonio@tarmac.io>
2021-01-19 10:44:12 +0100

    Clean up files

--- .circleci/semtag.sh
+++ .circleci/semtag.sh
@@ -3,20 +3,17 @@ set -euo pipefail
 
 curl -o semtag https://raw.githubusercontent.com/pnikosis/semtag/v0.0.9/semtag
 chmod +x semtag
-SEMVER_TAG='pillarwallet/develop'
+SEMVER_TAG=$(git log -1  --pretty='%s' | awk 'NF>1{print $NF}' | cut -d'#' -f2 | tr '[:upper:]' '[:lower:]')
 
 case "$SEMVER_TAG" in
 	patch)
-		#bash semtag final -s $SEMVER_TAG
-        echo "All good - patch"
+		bash semtag final -s $SEMVER_TAG
 		;;
 	minor)
-		#bash semtag final -s $SEMVER_TAG
-        echo "All good - minor"
+		bash semtag final -s $SEMVER_TAG
 		;;
 	major)
-		#bash semtag final -s $SEMVER_TAG
-        echo "All good - major"
+		bash semtag final -s $SEMVER_TAG
 		;;
 	*)
 		exit 1
