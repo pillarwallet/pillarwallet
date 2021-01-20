b6eefcbededb995a50131fb40f2bdc4042fca47c
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-01-20 12:00:06 +0100

    fix: jest mocks for react navigation

--- src/testUtils/jestSetup.js
+++ src/testUtils/jestSetup.js
@@ -19,6 +19,7 @@
 */
 
 // This script runs at the beginning of all unit tests
+import 'react-native-gesture-handler/jestSetup';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { JSDOM } from 'jsdom';
@@ -80,6 +81,21 @@ const storageCache = {};
 const MockAsyncStorage = new StorageMock(storageCache);
 
 jest.mock('@react-native-community/async-storage', () => MockAsyncStorage);
+
+// Source: https://reactnavigation.org/docs/testing/#mocking-native-modules
+jest.mock('react-native-reanimated', () => {
+  // eslint-disable-next-line global-require
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+  jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
 
 jest.setMock('@react-native-firebase/crashlytics');
 jest.setMock('@react-native-firebase/app/lib/internal/registry/nativeModule', {});
