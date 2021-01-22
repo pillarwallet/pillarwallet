bb27e9411a81972c935cada084f508be2ce3a0f2
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-01-22 17:40:54 +0100

    refactor: improve code

--- src/components/Layout/ContainerWithHeader.js
+++ src/components/Layout/ContainerWithHeader.js
@@ -223,7 +223,7 @@ class ContainerWithHeader extends React.Component<Props, State> {
             {...headerProps}
             navigation={navigation}
             bottomBorderAnimationValue={bottomBorderAnimationValue}
-            forceInsetTop={Platform.select({ android: 'never' })}
+            noPaddingTop
           />}
         <StyledSafeAreaView
           forceInset={{ top: topInset, bottom: bottomInset, ...inset }}
