58ccc252ddbe1f26eee5ead944862ed06320793d
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-02-26 15:21:58 +0100

    reverse svg icons

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,3 @@
-
 // @flow
 /**
  * Metro configuration for React Native
@@ -7,30 +6,14 @@
  * @format
  */
 
-/* eslint-disable i18next/no-literal-string */
-/* eslint-disable import/no-extraneous-dependencies */
-
-const { getDefaultConfig } = require('metro-config');
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    maxWorkers: 4,
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      // $FlowFixMe: flow issues
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+module.exports = {
+  maxWorkers: 4,
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
