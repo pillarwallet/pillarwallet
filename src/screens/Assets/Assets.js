4eff907a0b37f6503222509f469888aadb01bcac
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-04-21 17:06:15 +0200

    refactor: migrate redux to hooks

--- src/screens/Assets/Assets.js
+++ src/screens/Assets/Assets.js
@@ -17,105 +17,76 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 import * as React from 'react';
 import { useNavigation } from 'react-navigation-hooks';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { useDispatch } from 'react-redux';
 import t from 'translations/translate';
 
-// components
+// Components
+import { Container } from 'components/Layout';
 import { BaseText } from 'components/Typography';
 import ContainerWithHeader from 'components/Layout/ContainerWithHeader';
 import Spinner from 'components/Spinner';
 import Button from 'components/Button';
-import { Container } from 'components/Layout';
 
 // Selectors
+import { useRootSelector, activeAccountSelector } from 'selectors';
+import { accountAssetsSelector } from 'selectors/assets';
 import { availableStakeSelector, PPNIncomingTransactionsSelector } from 'selectors/paymentNetwork';
 
-// types
-import type { Assets } from 'models/Asset';
-import type { Collectible } from 'models/Collectible';
-import type { Badges } from 'models/Badge';
-import type { SmartWalletStatus } from 'models/SmartWalletStatus';
-import type { Accounts, Account } from 'models/Account';
-import type { Transaction } from 'models/Transaction';
-import type { Dispatch, RootReducerState } from 'reducers/rootReducer';
-
-// actions
+// Actions
 import { fetchInitialAssetsAction } from 'actions/assetsActions';
 import { fetchAllCollectiblesDataAction } from 'actions/collectiblesActions';
 
-// constants
-import {
-  FETCH_INITIAL_FAILED,
-  FETCHED,
-} from 'constants/assetsConstants';
+// Constants
+import { FETCH_INITIAL_FAILED, FETCHED } from 'constants/assetsConstants';
 import { PAYMENT_COMPLETED, SMART_WALLET_UPGRADE_STATUSES } from 'constants/smartWalletConstants';
 import { BLOCKCHAIN_NETWORK_TYPES } from 'constants/blockchainNetworkConstants';
 import { ACCOUNTS } from 'constants/navigationConstants';
 
-// utils
+// Utils
 import { getAccountName } from 'utils/accounts';
 import { getSmartWalletStatus, isDeployingSmartWallet, getDeploymentHash } from 'utils/smartWallet';
 import { useTheme, useThemeColors, getColorByThemeOutsideStyled } from 'utils/themes';
 
-// selectors
-import { accountCollectiblesSelector } from 'selectors/collectibles';
-import { accountAssetsSelector } from 'selectors/assets';
-import { activeAccountSelector } from 'selectors';
-
-// local components
+// Local
 import PPNView from 'screens/Assets/PPNView';
 import WalletView from 'screens/Assets/WalletView';
 import WalletActivation from 'screens/Assets/WalletActivation';
-
-type Props = {
-  fetchInitialAssets: () => void,
-  assets: Assets,
-  collectibles: Collectible[],
-  assetsState: ?string,
-  badges: Badges,
-  accounts: Accounts,
-  smartWalletState: Object,
-  blockchainNetworks: Object[],
-  activeAccount: ?Account,
-  fetchAllCollectiblesData: () => void,
-  availableStake: number,
-  PPNTransactions: Transaction[],
-};
 
 const VIEWS = {
   SMART_WALLET_VIEW: 'SMART_WALLET_VIEW',
   PPN_VIEW: 'PPN_VIEW',
 };
 
-function AssetsScreen({
-  fetchInitialAssets,
-  fetchAllCollectiblesData,
-  assets,
-  blockchainNetworks,
-  activeAccount,
-  availableStake,
-  PPNTransactions,
-  assetsState,
-  accounts,
-  smartWalletState,
-}: Props) {
+function AssetsScreen() {
   const navigation = useNavigation();
 
   const [showSmartWalletInsight, setShowSmartWalletInsight] = React.useState(false);
+
+  const accounts = useRootSelector((root) => root.accounts.data);
+  const assets = useRootSelector(accountAssetsSelector);
+  const assetsState = useRootSelector((root) => root.assets.assetsState);
+  const smartWalletState = useRootSelector(root => root.smartWallet);
+  const blockchainNetworks = useRootSelector((root) => root.blockchainNetwork.data);
+  const activeAccount = useRootSelector(activeAccountSelector);
+  const availableStake = useRootSelector(availableStakeSelector);
+  const PPNTransactions = useRootSelector(PPNIncomingTransactionsSelector);
+
+  const dispatch = useDispatch();
 
   const theme = useTheme();
   const colors = useThemeColors();
 
   React.useEffect(() => {
     if (!Object.keys(assets).length) {
-      fetchInitialAssets();
+      dispatch(fetchInitialAssetsAction());
     }
 
-    fetchAllCollectiblesData();
-  }, []);
+    dispatch(fetchAllCollectiblesDataAction());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
 
   // shouldComponentUpdate(nextProps: Props, nextState: State) {
   //   const { navigation } = this.props;
@@ -181,8 +152,7 @@ function AssetsScreen({
   };
 
   const renderView = (viewType: string, onScroll: (Object) => void) => {
-    const smartWalletStatus: SmartWalletStatus = getSmartWalletStatus(accounts, smartWalletState);
-
+    const smartWalletStatus = getSmartWalletStatus(accounts, smartWalletState);
     const isDeploying = isDeployingSmartWallet(smartWalletState, accounts);
 
     if (!Object.keys(assets).length && assetsState === FETCHED) {
@@ -191,7 +161,7 @@ function AssetsScreen({
           <BaseText style={{ marginBottom: 20 }}>{t('label.loadingDefaultAssets')}</BaseText>
           {assetsState !== FETCH_INITIAL_FAILED && <Spinner />}
           {assetsState === FETCH_INITIAL_FAILED && (
-            <Button title={t('button.tryAgain')} onPress={() => fetchInitialAssets()} />
+            <Button title={t('button.tryAgain')} onPress={() => dispatch(fetchInitialAssetsAction())} />
           )}
         </Container>
       );
@@ -206,8 +176,6 @@ function AssetsScreen({
     }
 
     switch (viewType) {
-      case VIEWS.PPN_VIEW:
-        return <PPNView onScroll={onScroll} />;
       case VIEWS.SMART_WALLET_VIEW:
         return (
           <WalletView
@@ -217,6 +185,8 @@ function AssetsScreen({
             onScroll={onScroll}
           />
         );
+      case VIEWS.PPN_VIEW:
+        return <PPNView onScroll={onScroll} />;
       default:
         return null;
     }
@@ -258,36 +228,4 @@ function AssetsScreen({
   );
 }
 
-const mapStateToProps = ({
-  accounts: { data: accounts },
-  assets: { assetsState },
-  badges: { data: badges },
-  smartWallet: smartWalletState,
-  blockchainNetwork: { data: blockchainNetworks },
-}: RootReducerState): $Shape<Props> => ({
-  accounts,
-  assetsState,
-  badges,
-  smartWalletState,
-  blockchainNetworks,
-});
-
-const structuredSelector = createStructuredSelector({
-  collectibles: accountCollectiblesSelector,
-  assets: accountAssetsSelector,
-  activeAccount: activeAccountSelector,
-  availableStake: availableStakeSelector,
-  PPNTransactions: PPNIncomingTransactionsSelector,
-});
-
-const combinedMapStateToProps = (state) => ({
-  ...structuredSelector(state),
-  ...mapStateToProps(state),
-});
-
-const mapDispatchToProps = (dispatch: Dispatch): $Shape<Props> => ({
-  fetchInitialAssets: () => dispatch(fetchInitialAssetsAction()),
-  fetchAllCollectiblesData: () => dispatch(fetchAllCollectiblesDataAction()),
-});
-
-export default connect(combinedMapStateToProps, mapDispatchToProps)(AssetsScreen);
+export default AssetsScreen;
