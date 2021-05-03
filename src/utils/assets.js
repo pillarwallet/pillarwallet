4c04515be476e19189ccd7231041577e576e4295
divyaBacancy <divya.mantri@bacancy.com>
2021-05-03 15:15:58 +0530

    Revert: Fiat Value Display K, M, B

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -140,11 +140,10 @@ export const getFormattedRate = (
   amount: number,
   token: string,
   fiatCurrency: string,
-  shouldCommify?: boolean,
 ): string => {
   const amountInFiat = amount * getRate(rates, token, fiatCurrency);
 
-  return formatFiat(amountInFiat, fiatCurrency, { shouldCommify });
+  return formatFiat(amountInFiat, fiatCurrency);
 };
 
 export const calculateMaxAmount = (
