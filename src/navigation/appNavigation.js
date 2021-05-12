86fe32122b78b71df2127cfddc1d515ee659d66c
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-05-12 10:31:29 +0200

    self code review

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -50,7 +50,6 @@ import BackupPhraseScreen from 'screens/BackupPhrase';
 import BackupPhraseValidateScreen from 'screens/BackupPhraseValidate';
 import CollectibleScreen from 'screens/Collectible';
 import WalletConnectHomeScreen from 'screens/WalletConnect/Home';
-import WalletConnectScreen from 'screens/LegacyWalletConnect';
 import WalletConnectConnectorRequestScreen from 'screens/LegacyWalletConnect/WalletConnectConnectorRequest';
 import WalletConnectCallRequest from 'screens/LegacyWalletConnect/WalletConnectCallRequest';
 import WalletConnectPinConfirm from 'screens/LegacyWalletConnect/WalletConnectPinConfirm';
