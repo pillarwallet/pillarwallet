0d4244a4f62bebeccff51708a8148fb9aa84951b
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-05-13 16:40:25 +0200

    revert unused Tabs changes

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,17 +2,14 @@
 /*
     Pillar Wallet: the personal data locker
     Copyright (C) 2019 Stiftung Pillar Project
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -28,25 +25,33 @@ type Tab = {
   onPress: () => void,
 };
 
+type TabProps = Tab & {
+  active: boolean,
+};
+
 type Props = {
   tabs: Tab[],
   activeTab: string,
   wrapperStyle?: Object,
 };
 
-const Tabs = ({ tabs, activeTab, wrapperStyle }: Props) => {
-  return (
-    <TabsContainer style={wrapperStyle}>
-      {tabs.map(tab => <TabComponent {...tab} active={activeTab === tab.id} key={tab.id} />)}
-    </TabsContainer>
-  );
-};
+const TabsContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+`;
 
-export default Tabs;
+const Underline = styled.View`
+  margin-top: 3px;
+  border-top-width: 6px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border-color: ${({ theme }) => theme.colors.primaryAccent130};
+`;
 
-type TabProps = Tab & {
-  active: boolean,
-};
+const TabContainer = styled.TouchableOpacity`
+  flex: 1;
+  align-items: center;
+`;
 
 const TabComponent = ({ name, onPress, active }: TabProps) => {
   if (active) {
@@ -68,20 +73,12 @@ const TabComponent = ({ name, onPress, active }: TabProps) => {
   );
 };
 
-const TabsContainer = styled.View`
-  flex-direction: row;
-  width: 100%;
-`;
+const Tabs = ({ tabs, activeTab, wrapperStyle }: Props) => {
+  return (
+    <TabsContainer style={wrapperStyle}>
+      {tabs.map(tab => <TabComponent {...tab} active={activeTab === tab.id} key={tab.id} />)}
+    </TabsContainer>
+  );
+};
 
-const Underline = styled.View`
-  margin-top: 3px;
-  border-top-width: 6px;
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-  border-color: ${({ theme }) => theme.colors.primaryAccent130};
-`;
-
-const TabContainer = styled.TouchableOpacity`
-  flex: 1;
-  align-items: center;
-`;
+export default Tabs;
