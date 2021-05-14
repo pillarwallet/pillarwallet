bf71a50b4d39916d02a890c2a22cbc23209fed0b
Deimantas <as.deimis@gmail.com>
2021-05-14 12:06:24 +0300

    remove etherspot accounts log

--- src/actions/etherspotActions.js
+++ src/actions/etherspotActions.js
@@ -149,7 +149,6 @@ export const importEtherspotAccountsAction = () => {
     }
 
     const etherspotAccounts = await etherspotService.getAccounts();
-    reportLog('Fetched Etherspot Accounts', { etherspotAccounts });
     if (!etherspotAccounts) {
       // Note: there should be always at least one account, it syncs on Etherspot SDK init, otherwise it's failure
       reportErrorLog('importEtherspotAccountsAction failed: no accounts', { etherspotAccounts });
