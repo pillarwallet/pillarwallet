38bcaee6728e2315e5f53012661b129466559b0c
divyaBacancy <divya.mantri@bacancy.com>
2021-05-22 14:34:28 +0530

    refractor

--- src/constants/assetsConstants.js
+++ src/constants/assetsConstants.js
@@ -79,10 +79,8 @@ const tBTC = 'tBTC';
 const OBTC = 'OBTC';
 const wBTC = 'wBTC'; // testnet
 const testBTC = 'testBTC'; // testnet
-const MATIC = 'MATIC';
 const MATIC_DAI = 'MATIC_DAI';
 const MATIC_USDC = 'MATIC_USDC';
-const XDAI = 'XDAI';
 
 
 export const TOKENS = ('TOKENS': 'TOKENS');
