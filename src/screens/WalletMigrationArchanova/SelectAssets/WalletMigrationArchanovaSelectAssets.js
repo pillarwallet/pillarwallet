a1d5d72f2950e468c36339ebf05291096b26f668
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-08-03 15:33:05 +0200

    enable NFT migration UI

--- src/screens/WalletMigrationArchanova/SelectAssets/WalletMigrationArchanovaSelectAssets.js
+++ src/screens/WalletMigrationArchanova/SelectAssets/WalletMigrationArchanovaSelectAssets.js
@@ -39,6 +39,7 @@ import {
 // Selectors
 import { useRootSelector } from 'selectors';
 import { archanovaAccountSelector } from 'selectors/accounts';
+import { archanovaCollectiblesSelector } from 'selectors/archanova';
 
 // Actions
 import { switchToArchanovaAccountIfNeededAction } from 'actions/accountsActions';
@@ -72,8 +73,7 @@ const WalletMigrationArchanovaSelectAssets = () => {
 
   const tokens = useTokensWithBalances();
 
-  // TODO: disabled NTFs because archanova migrator contract does not handle all kinds of NTFs atm (e.g. cryptokitties)
-  const collectibles = []; // useRootSelector(archanovaCollectiblesSelector);
+  const collectibles = useRootSelector(archanovaCollectiblesSelector);
   const totalValueInFiat = useTotalMigrationValueInFiat();
 
   // Force active account to be archanova
