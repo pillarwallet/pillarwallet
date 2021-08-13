26ba726590a4673a55b59857f5aced451973a9dd
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-08-13 14:54:26 +0200

    revert formatting

--- src/selectors/wallets.js
+++ src/selectors/wallets.js
@@ -35,35 +35,43 @@ import type { RootReducerState } from 'reducers/rootReducer';
 // local
 import { accountsSelector, activeAccountSelector, activeBlockchainSelector } from './selectors';
 
+
 export const activeWalletSelector = createSelector(
   activeAccountSelector,
   activeBlockchainSelector,
   (activeAccount) => activeAccount,
 );
 
-export const availableWalletsSelector = createSelector(accountsSelector, activeBlockchainSelector, (accounts) => {
-  const availableWallets = [];
+export const availableWalletsSelector = createSelector(
+  accountsSelector,
+  activeBlockchainSelector,
+  (accounts) => {
+    const availableWallets = [];
 
-  const keyWallet = accounts.find(({ type }) => type === ACCOUNT_TYPES.KEY_BASED);
-  if (keyWallet) availableWallets.push(keyWallet);
+    const keyWallet = accounts.find(({ type }) => type === ACCOUNT_TYPES.KEY_BASED);
+    if (keyWallet) availableWallets.push(keyWallet);
 
-  accounts.filter(isSmartWalletAccount).forEach((smartWallet) => {
-    availableWallets.push({
-      ...smartWallet,
-      isActive: smartWallet.isActive,
+    accounts.filter(isSmartWalletAccount).forEach((smartWallet) => {
+      availableWallets.push({
+        ...smartWallet,
+        isActive: smartWallet.isActive,
+      });
     });
-  });
 
-  // etherspot account first
-  return sortBy(availableWallets, ({ type }) => (type === ACCOUNT_TYPES.ETHERSPOT_SMART_WALLET ? -1 : 1));
-});
+    // etherspot account first
+    return sortBy(
+      availableWallets,
+      ({ type }) => type === ACCOUNT_TYPES.ETHERSPOT_SMART_WALLET ? -1 : 1,
+    );
+  },
+);
 
 export const hasKeyBasedAssetsTransferInProgressSelector = createSelector(
   ({ keyBasedAssetTransfer }: RootReducerState) => keyBasedAssetTransfer.data,
   ({ keyBasedAssetTransfer }: RootReducerState) => keyBasedAssetTransfer.creatingTransactions,
-  (keyBasedAssetsTransfer, creatingTransactions) =>
-    creatingTransactions ||
-    keyBasedAssetsTransfer.some((keyBasedAssetTransfer) => !isEmpty(keyBasedAssetTransfer?.signedTransaction)),
+  (keyBasedAssetsTransfer, creatingTransactions) => creatingTransactions || keyBasedAssetsTransfer.some(
+    (keyBasedAssetTransfer) => !isEmpty(keyBasedAssetTransfer?.signedTransaction),
+  ),
 );
 
 export function useIsWalletBackedUp() {
