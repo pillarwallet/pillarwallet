93db253aec46356e59e2236956f128716ac6754c
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-08-19 12:55:39 +0200

    TokenIcon update

--- src/screens/Assets/rewards/RewardListItem.js
+++ src/screens/Assets/rewards/RewardListItem.js
@@ -26,7 +26,7 @@ import styled from 'styled-components/native';
 import Text from 'components/modern/Text';
 import FiatValueView from 'components/modern/FiatValueView';
 import TokenValueView from 'components/modern/TokenValueView';
-import TokenIcon from 'components/modern/TokenIcon';
+import TokenIcon from 'components/Icons/TokenIcon';
 
 // Selectors
 import { useFiatCurrency, useChainRates } from 'selectors';
