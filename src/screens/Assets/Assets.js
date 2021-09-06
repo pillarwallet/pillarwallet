508f4172bf40fb1ac78172ddfe8be1b919f53b84
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2021-09-06 12:22:28 +0200

    move modern TabView

--- src/screens/Assets/Assets.js
+++ src/screens/Assets/Assets.js
@@ -25,7 +25,7 @@ import { useTranslation } from 'translations/translate';
 // Components
 import { Container } from 'components/layout/Layout';
 import HeaderBlock from 'components/HeaderBlock';
-import TabView from 'components/modern/TabView';
+import TabView from 'components/layout/TabView';
 
 // Constants
 import { ASSET_CATEGORY as CATEGORY } from 'constants/assetsConstants';
