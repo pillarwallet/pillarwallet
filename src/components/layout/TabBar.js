b2af578b2c81c6d29a46ceef7edb3f7fbe89601c
vignesh.a <vignesh.a@tradala.in>
2021-11-10 19:16:47 +0530

    changed based on PR comments

--- src/components/layout/TabBar.js
+++ src/components/layout/TabBar.js
@@ -98,5 +98,5 @@ const Underline = styled.View`
   border-top-width: ${Platform.OS === 'android' ? '3px' : '6px'};
   border-top-left-radius: 3px;
   border-top-right-radius: 3px;
-  border-color: #A020F0;
+  border-color: ${({ theme }) => theme.colors.purple};
 `;
