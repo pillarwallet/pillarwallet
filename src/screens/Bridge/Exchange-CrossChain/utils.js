657336d3e7ff3e2e88b8a6c1adf526804a961fd5
hemadri-tank-bacancy <hemadri.tank@bacancy.com>
2023-01-19 20:37:00 +0530

    own token in popular tokenlist

--- src/screens/Bridge/Exchange-CrossChain/utils.js
+++ src/screens/Bridge/Exchange-CrossChain/utils.js
@@ -51,7 +51,7 @@ import { DAI, USDT, USDC, BUSD } from 'constants/assetsConstants';
 // Types
 import type { QueryResult } from 'utils/types/react-query';
 import type { Asset, AssetByAddress, AssetOption, AssetsPerChain } from 'models/Asset';
-import type { WalletAssetsBalances } from 'models/Balances';
+import type { WalletAssetsBalances, AssetBalance } from 'models/Balances';
 import type { ExchangeOffer } from 'models/Exchange';
 import type { Currency, RatesPerChain } from 'models/Rates';
 import type { Chain, ChainRecord } from 'models/Chain';
@@ -178,6 +178,7 @@ export function useToPopularAssets(chain: ?Chain, isCrossChain?: boolean): Asset
   const filteredSupportedList = supportedChains.filter((chainNm: Chain) => chainNm !== chain);
 
   const popularAssetsPerChain = usePopularAssetsPerChain();
+  const supportedAssetsPerChain = useSupportedAssetsPerChain();
   const walletBalancesPerChain = useRootSelector(accountWalletAssetsBalancesSelector);
   const ratesPerChain = useRatesPerChain();
   const currency = useFiatCurrency();
@@ -185,19 +186,63 @@ export function useToPopularAssets(chain: ?Chain, isCrossChain?: boolean): Asset
   return React.useMemo(() => {
     if (isCrossChain) {
       return filteredSupportedList.flatMap((chainNm) =>
-        getExchangeToAssetOptions(chainNm, popularAssetsPerChain, walletBalancesPerChain, ratesPerChain, currency),
+        getPopularToAssetOptions(
+          chainNm,
+          popularAssetsPerChain,
+          supportedAssetsPerChain,
+          walletBalancesPerChain,
+          ratesPerChain,
+          currency,
+        ),
       );
     }
-    return getExchangeToAssetOptions(chain, popularAssetsPerChain, walletBalancesPerChain, ratesPerChain, currency);
+    return getPopularToAssetOptions(
+      chain,
+      popularAssetsPerChain,
+      supportedAssetsPerChain,
+      walletBalancesPerChain,
+      ratesPerChain,
+      currency,
+    );
   }, [
     isCrossChain,
     chain,
     popularAssetsPerChain,
+    supportedAssetsPerChain,
     walletBalancesPerChain,
     ratesPerChain,
     currency,
     filteredSupportedList,
   ]);
+}
+
+function getPopularToAssetOptions(
+  chain: ?Chain,
+  popularAssetsPerChain: AssetsPerChain,
+  supportedAssetsPerChain: AssetsPerChain,
+  walletBalancesPerChain: ChainRecord<WalletAssetsBalances>,
+  ratesPerChain: RatesPerChain,
+  currency: Currency,
+): AssetOption[] {
+  if (!chain) return [];
+
+  const popularAssets: any = popularAssetsPerChain?.[chain] ?? [];
+  const supportedAssets: any = supportedAssetsPerChain?.[chain] ?? [];
+  const walletBalances = walletBalancesPerChain?.[chain] ?? {};
+  const rates = ratesPerChain?.[chain] ?? {};
+
+  Object.values(walletBalances)?.forEach((balanceInfo: AssetBalance | any) => {
+    const findAsset = popularAssets?.find((asset) => asset.address === balanceInfo.address);
+
+    if (!findAsset) {
+      const supportedAsset: AssetsPerChain | any = supportedAssets?.find(
+        (asset) => asset.address === balanceInfo.address,
+      );
+      popularAssets.push(supportedAsset);
+    }
+  });
+
+  return popularAssets.map((asset) => getAssetOption(asset, walletBalances, rates, currency, chain));
 }
 
 function getExchangeToAssetOptions(
