5674786f5a657a8ee12af84f9baad068315923ad
vnovacev <novacev@yahoo.com>
2023-02-27 15:08:23 +0100

    Test new announce url message

--- .circleci/announceURLs.sh
+++ .circleci/announceURLs.sh
@@ -25,4 +25,5 @@ cat <<EOM
 EOM
 )
 
-curl -X POST --data-urlencode payload="$payload" "$S3_URL_SLACK_WEBHOOK"
+# curl -X POST --data-urlencode payload="$payload" "$S3_URL_SLACK_WEBHOOK"
+curl -X POST --data-urlencode payload="$payload" "$S3_TEST_WEBHOOK"
