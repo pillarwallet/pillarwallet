10665b25355bcca3df517b8ac9f4ae134c75f3f8
lalitkumarrock <lalit.kumar@bacancy.com>
2023-11-03 13:23:05 +0530

    disabled set keychain pincode for biometric

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -292,9 +292,7 @@ export async function getDecryptedWallet(
   if (keychainData?.pin) {
     const { pin: keychainPin, privateKey: keychainPrivateKey, mnemonic: keychainMnemonic } = keychainData;
 
-    const mnemonicPhrase = typeof keychainMnemonic === 'string'
-      ? keychainMnemonic
-      : keychainMnemonic?.phrase; // needed for ethers v5 migration
+    const mnemonicPhrase = typeof keychainMnemonic === 'string' ? keychainMnemonic : keychainMnemonic?.phrase; // needed for ethers v5 migration
 
     if (pin && pin === keychainPin && keychainPrivateKey) {
       return withMnemonic && mnemonicPhrase
@@ -306,12 +304,15 @@ export async function getDecryptedWallet(
   const wallet = await decryptWalletFromStorage(pin, deviceUniqueId);
 
   // migrate older users for keychain access
-  const keychainDataObject = {
-    pin,
-    privateKey: wallet?.privateKey,
-    mnemonic: wallet?.mnemonic?.phrase,
-  };
-  await setKeychainDataObject(keychainDataObject, biometricsEnabled);
+  // Note: This takes more time when biometrics is enabled. So disable this for biometrics.
+  if (!biometricsEnabled) {
+    const keychainDataObject = {
+      pin,
+      privateKey: wallet?.privateKey,
+      mnemonic: wallet?.mnemonic?.phrase,
+    };
+    await setKeychainDataObject(keychainDataObject, biometricsEnabled);
+  }
 
   return wallet;
 }
