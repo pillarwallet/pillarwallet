db629f60d8c63eee2586bae873c7b8949c8a88ea
LeonMorrison-tech <Lalitpancholi1008@gmail.com>
2025-09-10 09:45:01 +0530

    Removed Global Components

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,30 +4,21 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('../src/components/ActionModal/ActionModal.stories');
 	require('../src/components/Banner/Banner.stories');
-	require('../src/components/CircleButton/CircleButton.stories');
 	require('../src/components/ContactSelector/ContactSelector.stories');
 	require('../src/components/DetailModal/DetailModal.stories');
 	require('../src/components/FeeLabelToggle/FeeLabelToggle.stories');
-	require('../src/components/FloatingBox/FloatingBox.stories');
 	require('../src/components/FloatingButtons/FloatingButtons.stories');
 	require('../src/components/Graph/ValueOverTimeGraph.stories');
 	require('../src/components/HeaderBlock/HeaderBlock.stories');
-	require('../src/components/HistoryList/HistoryList.stories');
-	require('../src/components/InsightWithButton/InsightWithButton.stories');
 	require('../src/components/Modals/SlideModal/SlideModal.stories');
 	require('../src/components/ProfileImage/ProfileImage.stories');
-	require('../src/components/RetryGraphQueryBox/RetryGraphQueryBox.stories');
 	require('../src/components/ReviewSummary/ReviewSummary.stories');
-	require('../src/components/SWActivationCard/SWActivationCard.stories');
 	require('../src/components/Toast/ToastCard.stories');
 	require('../src/components/Tooltip/Tooltip.stories');
 	require('../src/components/core/CheckBox/CheckBox.stories');
-	require('../src/components/legacy/ActivityFeed/ActivityFeedItem.stories');
 	require('../src/components/legacy/Button/Button.stories');
 	require('../src/components/legacy/Checkbox/Checkbox.stories');
-	require('../src/components/legacy/EventDetails/EventDetails.stories');
 	require('../src/components/legacy/ListItem/ListCard.stories');
 	require('../src/components/legacy/ListItem/ListItemChevron.stories');
 	require('../src/components/legacy/ListItem/SettingsItemCarded.stories');
@@ -38,30 +29,21 @@ function loadStories() {
 }
 
 const stories = [
-	'../src/components/ActionModal/ActionModal.stories',
 	'../src/components/Banner/Banner.stories',
-	'../src/components/CircleButton/CircleButton.stories',
 	'../src/components/ContactSelector/ContactSelector.stories',
 	'../src/components/DetailModal/DetailModal.stories',
 	'../src/components/FeeLabelToggle/FeeLabelToggle.stories',
-	'../src/components/FloatingBox/FloatingBox.stories',
 	'../src/components/FloatingButtons/FloatingButtons.stories',
 	'../src/components/Graph/ValueOverTimeGraph.stories',
 	'../src/components/HeaderBlock/HeaderBlock.stories',
-	'../src/components/HistoryList/HistoryList.stories',
-	'../src/components/InsightWithButton/InsightWithButton.stories',
 	'../src/components/Modals/SlideModal/SlideModal.stories',
 	'../src/components/ProfileImage/ProfileImage.stories',
-	'../src/components/RetryGraphQueryBox/RetryGraphQueryBox.stories',
 	'../src/components/ReviewSummary/ReviewSummary.stories',
-	'../src/components/SWActivationCard/SWActivationCard.stories',
 	'../src/components/Toast/ToastCard.stories',
 	'../src/components/Tooltip/Tooltip.stories',
 	'../src/components/core/CheckBox/CheckBox.stories',
-	'../src/components/legacy/ActivityFeed/ActivityFeedItem.stories',
 	'../src/components/legacy/Button/Button.stories',
 	'../src/components/legacy/Checkbox/Checkbox.stories',
-	'../src/components/legacy/EventDetails/EventDetails.stories',
 	'../src/components/legacy/ListItem/ListCard.stories',
 	'../src/components/legacy/ListItem/ListItemChevron.stories',
 	'../src/components/legacy/ListItem/SettingsItemCarded.stories',
